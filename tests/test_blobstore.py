cd2138d12b0e810e14833b1d0b7dcd97975ab34c
Tony Tung <ttung@chanzuckerberg.com>
2017-06-12 15:17:51 -0700

    Method to retrieve metadata for an object.
    Added test to retrieve metadata for known blobs.

---
+++ tests/test_blobstore.py
@@ -0,0 +1,15 @@
+class BlobStoreTests(object):
+    """
+    Common blobstore tests.  We want to avoid repeating ourselves, so if we
+    built the abstractions correctly, common operations can all be tested here.
+    """
+
+    def test_get_metadata(self):
+        """
+        Ensure that the ``get_metadata`` methods return sane data.
+        """
+        handle = self.handle  # type: BlobStore
+        metadata = handle.get_metadata(
+            self.test_bucket,
+            "sourcedata/test_good_source_data")
+        self.assertIn('hca-dss-content-type', metadata)
