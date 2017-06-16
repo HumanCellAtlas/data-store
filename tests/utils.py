9d01dd57ef625da66307737a0700b68bc2a49d4d
Tony Tung <ttung@chanzuckerberg.com>
2017-06-16 10:41:32 -0700

    Clean up the fetching of bucket env variables.

---
+++ tests/utils.py
@@ -0,0 +1,8 @@
+import os
+
+
+def get_env(varname):
+    if varname not in os.environ:
+        raise RuntimeError(
+            "Please set the {} environment variable".format(varname))
+    return os.environ[varname]
