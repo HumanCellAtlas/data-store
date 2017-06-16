f73b659fbb97c7ac520de870fd49ae7e9fc79b3c
Tony Tung <tonytung@merly.org>
2017-06-16 12:02:17 -0700

    unbreak tests. (#84)
    I foolishly removed the sourcedata/test_good_source_data from the hca-dss-test bucket on S3.  But that wasn't the right place for it anyway.  It belongs in the test source data bucket.
    
    I created the bucket on GS with the proper permissions and put the data there.

--- tests/test_blobstore.py
+++ tests/test_blobstore.py
@@ -10,6 +10,6 @@ class BlobStoreTests(object):
         """
         handle = self.handle  # type: BlobStore
         metadata = handle.get_metadata(
-            self.test_bucket,
-            "sourcedata/test_good_source_data")
+            self.test_src_data_bucket,
+            "test_good_source_data")
         self.assertIn('hca-dss-content-type', metadata)
