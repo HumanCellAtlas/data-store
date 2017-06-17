ecbd7202cfa1941a5293d541e9ac42f3b9aff283
Michael Baumann <mbaumann@ucsc.edu>
2017-06-16 17:06:36 -0700

    Incorporate review comments from Tony on pull request. Minor clean-up and corrections to the deploy_aws_elasticsearch.sh script.

--- daemons/deploy_aws_elasticsearch.sh
+++ daemons/deploy_aws_elasticsearch.sh
@@ -3,13 +3,14 @@
 set -euo pipefail
 
 function create_elasticsearch_domain() {
-    echo Creating Elasticsearch domain with name: $elasticsearch_domain_name
+    local elasticsearch_domain_name=$1
+    echo "Creating Elasticsearch domain with name: $elasticsearch_domain_name"
     # Just create a small ES cluster for now.
     # It is easy to increase the capacity as needed.
-    elasticsearch_version=5.3 # Latest version supported by AWS as of 6/2017
-    elasticsearch_cluster_config=InstanceType=t2.small.elasticsearch,InstanceCount=1
+    elasticsearch_version="5.3" # Latest version supported by AWS as of 6/2017
+    elasticsearch_cluster_config="InstanceType=t2.small.elasticsearch,InstanceCount=1"
     # 35GB is maximum allowed for a t2.small.elasticsearch
-    ebs_options=EBSEnabled=true,VolumeType=gp2,VolumeSize=35
+    ebs_options=EBSEnabled="true,VolumeType=gp2,VolumeSize=35"
 
     aws es create-elasticsearch-domain \
       --domain-name $elasticsearch_domain_name \
@@ -19,105 +20,115 @@ function create_elasticsearch_domain() {
 }
 
 function update_elasticsearch_domain_policy() {
-    echo Updating policy for Elasticsearch domain with name: $elasticsearch_domain_name
+    local elasticsearch_domain_name=$1
+    echo "Updating policy for Elasticsearch domain with name: $elasticsearch_domain_name"
     sed "s/ELASTICSEARCH_DOMAIN/$elasticsearch_domain_name/g" ./es_policy.json.template > ./es_policy.json
     aws es update-elasticsearch-domain-config --domain-name $elasticsearch_domain_name \
          --access-policies "$(< ./es_policy.json)"
 }
 
 function delete_elasticsearch_domain() {
-    echo Deleting Elasticsearch domain with name: $elasticsearch_domain_name
+    local elasticsearch_domain_name=$1
+    echo "Deleting Elasticsearch domain with name: $elasticsearch_domain_name"
     aws es delete-elasticsearch-domain --domain-name $elasticsearch_domain_name
 }
 
 function check_elasticsearch_domain_exists() {
-    set +e
+    local elasticsearch_domain_name=$1
+    set +e # Don't terminate the program if grep doesn't to find a match.
     aws es list-domain-names | grep $elasticsearch_domain_name > /dev/null
     result=$?
-    set -e
     return $result
 }
 
 function wait_for_elasticsearch_domain_removed() {
-	timeout_value=$1
-	start_time=`date +'%s'`
-	timeout_expiration=$(($start_time + $timeout_value))
+    local elasticsearch_domain_name=$1
+    local timeout_value=$2
+    local start_time=`date +'%s'`
+    local timeout_expiration=$(($start_time + $timeout_value))
 
-	while [[ `date +'%s'` -lt $timeout_expiration ]]; do
-	    check_elasticsearch_domain_exists
-	    if [[ $? -ne 0 ]]; then
-	        echo Elasticsearch domain has been removed
-	        return 0
-	    fi
-        echo Waiting for Elasticsearch domain to be removed ...
-		sleep 5
-	done
-	echo Timeout waiting for Elasticsearch domain to be removed
-	return 1
+    while [[ `date +'%s'` -lt $timeout_expiration ]]; do
+        check_elasticsearch_domain_exists $elasticsearch_domain_name
+        if [[ $? -ne 0 ]]; then
+            echo "Elasticsearch domain has been removed"
+            return 0
+        fi
+        echo "Waiting for Elasticsearch domain to be removed ..."
+        sleep 5
+    done
+    echo "Timeout waiting for Elasticsearch domain to be removed"
+    return 1
 }
 
 function wait_for_elasticsearch_endpoint() {
-	timeout_value=$1
-	start_time=`date +'%s'`
-	timeout_expiration=$(($start_time + $timeout_value))
+    local elasticsearch_domain_name=$1
+    local timeout_value=$2
+    local start_time=`date +'%s'`
+    local timeout_expiration=$(($start_time + $timeout_value))
 
-	while [[ `date +'%s'` -lt $timeout_expiration ]]; do
-	    if [[ `get_elasticsearch_endpoint $elasticsearch_domain_name` ]]; then
-	        echo Elasticsearch endpoint is available.
-	        return 0
-	    fi
-        echo Waiting for Elasticsearch endpoint to be become available ...
-		sleep 5
-	done
-	echo Timeout waiting for Elasticsearch endpoint
-	return 1
+    while [[ `date +'%s'` -lt $timeout_expiration ]]; do
+        get_elasticsearch_endpoint $elasticsearch_domain_name
+        if [[ "`get_elasticsearch_endpoint $elasticsearch_domain_name`" == "null" ]]; then
+            echo "Waiting for Elasticsearch endpoint to be become available ..."
+            sleep 5
+        else
+            echo "Elasticsearch endpoint is available."
+            return 0
+        fi
+    done
+    echo "Timeout waiting for Elasticsearch endpoint"
+    return 1
 }
 
 function get_elasticsearch_arn() {
-    check_elasticsearch_domain_exists &&
-    aws es describe-elasticsearch-domain --domain-name $elasticsearch_domain_name 2> /dev/null \
-        | jq -r '.DomainStatus.ARN'
+    local elasticsearch_domain_name=$1
+    check_elasticsearch_domain_exists $elasticsearch_domain_name &&
+        aws es describe-elasticsearch-domain --domain-name $elasticsearch_domain_name 2> /dev/null \
+            | jq -r '.DomainStatus.ARN'
 }
 
 function get_elasticsearch_endpoint() {
-    check_elasticsearch_domain_exists &&
-    aws es describe-elasticsearch-domain --domain-name $elasticsearch_domain_name 2> /dev/null \
-        | jq -r '.DomainStatus.Endpoint'
+    local elasticsearch_domain_name=$1
+    check_elasticsearch_domain_exists $elasticsearch_domain_name &&
+        aws es describe-elasticsearch-domain --domain-name $elasticsearch_domain_name 2> /dev/null \
+            | jq -r '.DomainStatus.Endpoint'
 }
 
 function display_elasticsearch_domain_info() {
+    local elasticsearch_domain_name=$1
     elasticsearch_arn=$(get_elasticsearch_arn $elasticsearch_domain_name)
-    echo "elasticsearch ARN: " $elasticsearch_arn
+    echo "elasticsearch ARN: $elasticsearch_arn"
 
     elasticsearch_endpoint=$(get_elasticsearch_endpoint $elasticsearch_domain_name)
-    echo "elasticsearch endpoint: " $elasticsearch_endpoint
+    echo "elasticsearch endpoint: $elasticsearch_endpoint"
 }
 
 function setup_elasticsearch_domain() {
-    if [[ $delete_existing_es_instance == True ]]; then
-        check_elasticsearch_domain_exists
+    local elasticsearch_domain_name=$1
+    local delete_existing_es_instance=$2
+    if "$delete_existing_es_instance" == "True"; then
+        check_elasticsearch_domain_exists $elasticsearch_domain_name
         if [[ $? -eq 0 ]] ; then
             # An elasticsearch domain with this name already exists
-
-            echo Elasticsearch domain already exists for $elasticsearch_domain_name
-            echo Deleting existing Elasticsearch domain
+            echo "Elasticsearch domain already exists for $elasticsearch_domain_name"
+            echo "Deleting existing Elasticsearch domain"
             delete_elasticsearch_domain $elasticsearch_domain_name
 
-            wait_for_elasticsearch_domain_removed 300
+            wait_for_elasticsearch_domain_removed $elasticsearch_domain_name 600
        fi
     fi
 
-    check_elasticsearch_domain_exists
+    check_elasticsearch_domain_exists $elasticsearch_domain_name
     if [[ $? -ne 0 ]]; then
-        echo Creating new Elasticsearch domain with name: $elasticsearch_domain_name
+        echo "Creating new Elasticsearch domain with name: $elasticsearch_domain_name"
         create_elasticsearch_domain $elasticsearch_domain_name
 
         # Should we only update the policy when the the elasticsearch domain is newly created
         # or always update the policy in case the policy has changed?
-        echo Setting/updating Elasticsearch policy
+        echo "Setting/updating Elasticsearch policy"
         update_elasticsearch_domain_policy $elasticsearch_domain_name
     else
-        echo Using existing Elasticsearch domain with name: $elasticsearch_domain_name
+        echo "Using existing Elasticsearch domain with name: $elasticsearch_domain_name"
     fi
 }
 
@@ -130,16 +141,15 @@ if [[ $# != 2 ]]; then
     exit 1
 fi
 daemon_name=$1 stage=$2
-elasticsearch_domain_name="${daemon_name}-${stage}"
+declare -r global_elasticsearch_domain_name="${daemon_name}-${stage}"
+declare -r global_delete_existing_es_instance="False"
 
-delete_existing_es_instance=False
+setup_elasticsearch_domain $global_elasticsearch_domain_name $global_delete_existing_es_instance
 
-setup_elasticsearch_domain $elasticsearch_domain_name
-
-wait_for_elasticsearch_endpoint 300
+wait_for_elasticsearch_endpoint $global_elasticsearch_domain_name 600
 
 # TODO Delete existing indexes here via Python script
 
-display_elasticsearch_domain_info
+display_elasticsearch_domain_info $global_elasticsearch_domain_name
 
-echo Completed Elasticsearch setup
+echo "Completed Elasticsearch setup"
