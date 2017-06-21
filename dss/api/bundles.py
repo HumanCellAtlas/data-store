736b280e1d6b9abfcb307d8e76324b9649400f39
Tony Tung <ttung@chanzuckerberg.com>
2017-06-21 12:30:03 -0700

    Implementation of GET /bundles

--- dss/api/bundles.py
+++ dss/api/bundles.py
@@ -12,8 +12,55 @@ from ..config import Config
 from ..hcablobstore import BundleFileMetadata, BundleMetadata, FileMetadata
 
 
-def get(uuid: str, version: str, replica: str):
-    return {}
+def get(
+        uuid: str,
+        version: str=None,
+        # TODO: (ttung) once we can run the endpoints from each cloud, we should default to the local cloud.
+        replica: str="AWS"):
+    uuid = uuid.lower()
+
+    handle, hca_handle, bucket = \
+        Config.get_cloud_specific_handles(replica)
+
+    if version is None:
+        # list the files and find the one that is the most recent.
+        prefix = "bundles/{}.".format(uuid)
+        for matching_file in handle.list(bucket, prefix):
+            matching_file = matching_file[len(prefix):]
+            if version is None or matching_file > version:
+                version = matching_file
+
+    if version is None:
+        # no matches!
+        return make_response("Cannot find file!", 404)
+
+    # retrieve the bundle metadata.
+    bundle_metadata = json.loads(
+        handle.get(
+            bucket,
+            "bundles/{}.{}".format(uuid, version)
+        ).decode("utf-8"))
+
+    return dict(
+        bundle=dict(
+            uuid=uuid,
+            version=version,
+            files=[
+                {
+                    'name': file[BundleFileMetadata.NAME],
+                    'content-type': file[BundleFileMetadata.CONTENT_TYPE],
+                    'uuid': file[BundleFileMetadata.UUID],
+                    'version': file[BundleFileMetadata.VERSION],
+                    'crc32c': file[BundleFileMetadata.CRC32C],
+                    's3_etag': file[BundleFileMetadata.S3_ETAG],
+                    'sha1': file[BundleFileMetadata.SHA1],
+                    'sha256': file[BundleFileMetadata.SHA256],
+                }
+                for file in bundle_metadata[BundleMetadata.FILES]
+            ],
+            creator_uid=bundle_metadata[BundleMetadata.CREATOR_UID],
+        )
+    )
 
 
 def list_versions(uuid: str):
--- tests/test_bundle.py
+++ tests/test_bundle.py
@@ -15,6 +15,7 @@ pkg_root = os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))
 sys.path.insert(0, pkg_root)
 
 import dss  # noqa
+from dss.config import override_s3_config  # noqa
 from tests.infra import DSSAsserts  # noqa
 
 
@@ -24,8 +25,8 @@ class TestDSS(unittest.TestCase, DSSAsserts):
         self.app = dss.create_app().app.test_client()
 
     def test_bundle_get(self):
-        bundle_uuid = "ce55fd51-7833-469b-be0b-5da88ebebfcd"
-        version = "2017-06-16T19:36:04.240704Z"
+        bundle_uuid = "011c7340-9b3c-4d62-bf49-090d79daf198"
+        version = "2017-06-20T21:45:06.766634Z"
 
         url = urllib.parse.urlunparse((
             "",
@@ -40,9 +41,23 @@ class TestDSS(unittest.TestCase, DSSAsserts):
             "",
         ))
 
-        self.assertGetResponse(
-            url,
-            requests.codes.ok)
+        with override_s3_config("hca-dss-test-src"):
+            response = self.assertGetResponse(
+                url,
+                requests.codes.ok)
+
+        self.assertEqual(response[2]['bundle']['uuid'], bundle_uuid)
+        self.assertEqual(response[2]['bundle']['version'], version)
+        self.assertEqual(response[2]['bundle']['creator_uid'], 12345)
+        self.assertEqual(response[2]['bundle']['files'][0]['content-type'], "text/plain")
+        self.assertEqual(response[2]['bundle']['files'][0]['crc32c'], "e16e07b9")
+        self.assertEqual(response[2]['bundle']['files'][0]['name'], "LICENSE")
+        self.assertEqual(response[2]['bundle']['files'][0]['s3_etag'], "3b83ef96387f14655fc854ddc3c6bd57")
+        self.assertEqual(response[2]['bundle']['files'][0]['sha1'], "2b8b815229aa8a61e483fb4ba0588b8b6c491890")
+        self.assertEqual(response[2]['bundle']['files'][0]['sha256'],
+                         "cfc7749b96f63bd31c3c42b5c471bf756814053e847c10f3eb003417bc523d30")
+        self.assertEqual(response[2]['bundle']['files'][0]['uuid'], "ce55fd51-7833-469b-be0b-5da88ebebfcd")
+        self.assertEqual(response[2]['bundle']['files'][0]['version'], "2017-06-16T19:36:04.240704Z")
 
     def test_bundle_put(self):
         file_uuid = uuid.uuid4()
