cccda9162c5e32b8102c50fa69c893c312d74e16
Andrey Kislyuk <kislyuk@gmail.com>
2017-06-21 14:17:55 -0700

    More fixups for Lambda daemon testing

--- daemons/invoke_lambda.sh
+++ daemons/invoke_lambda.sh
@@ -2,10 +2,16 @@
 
 set -euo pipefail
 
+if [[ $# != 3 ]]; then
+    echo "Usage: $(basename $0) daemon-name stage lambda-input-file"
+    exit 1
+fi
+
 lambda_name="$1-$2"
 lambda_input_file=$3
 
-lambda_output="$(aws lambda invoke --function-name $lambda_name --invocation-type RequestResponse --payload "$(cat $lambda_input_file)" --log-type Tail /dev/stdout)"
+lambda_payload="$(sed "s/testBucket/$DSS_S3_TEST_BUCKET/g" $lambda_input_file)"
+lambda_output="$(aws lambda invoke --function-name $lambda_name --invocation-type RequestResponse --payload "$lambda_payload" --log-type Tail /dev/stdout | sed 's/^null//')"
 
 echo "$lambda_output" | jq -r .LogResult | base64 --decode
 
