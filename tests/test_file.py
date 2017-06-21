830c0be733f42adf8af9f75e8a49c03c3e2d3a42
Tony Tung <tonytung@merly.org>
2017-06-21 11:59:40 -0700

    API to get files! (#101)
    Removed a bunch of the old tests that merely verified the API was alive and replaced them with somewhat meaningful tests.

--- tests/test_file.py
+++ tests/test_file.py
@@ -3,10 +3,12 @@
 
 from __future__ import absolute_import, division, print_function, unicode_literals
 
+import hashlib
 import os
 import sys
-import uuid
 import unittest
+import urllib
+import uuid
 
 import requests
 
@@ -14,6 +16,7 @@ pkg_root = os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))
 sys.path.insert(0, pkg_root)
 
 import dss  # noqa
+from dss.config import override_s3_config  # noqa
 from tests.infra import DSSAsserts  # noqa
 
 
@@ -21,20 +24,6 @@ class TestFileApi(unittest.TestCase, DSSAsserts):
     def setUp(self):
         DSSAsserts.setup(self)
         self.app = dss.create_app().app.test_client()
-
-    def test_file_api(self):
-        self.assertGetResponse("/v1/files", requests.codes.ok)
-
-        self.assertHeadResponse(
-            "/v1/files/91839244-66ab-408f-9be5-c82def201f26",
-            requests.codes.ok)
-
-        self.assertGetResponse(
-            "/v1/files/91839244-66ab-408f-9be5-c82def201f26",
-            requests.codes.bad_request)
-        self.assertGetResponse(
-            "/v1/files/91839244-66ab-408f-9be5-c82def201f26?replica=aws",
-            requests.codes.found)
 
     def test_file_put(self):
         file_uuid = uuid.uuid4()
@@ -55,6 +44,99 @@ class TestFileApi(unittest.TestCase, DSSAsserts):
             }
         )
         self.assertIn('timestamp', response[2])
+
+    def test_file_head(self):
+        file_uuid = "1245b18c-b36b-4ebb-8220-0e0c6f4a87a4"
+        timestamp = "2017-06-16T19:36:04.240704Z"
+
+        url = urllib.parse.urlunsplit(
+            urllib.parse.urlsplit("")._replace(
+                path="/v1/files/" + file_uuid,
+                query=urllib.parse.urlencode(
+                    (("replica", "aws"),
+                     ("timestamp", timestamp)),
+                    doseq=True,
+                ),
+            )
+        )
+
+        with override_s3_config("hca-dss-test-src"):
+            self.assertHeadResponse(
+                url,
+                requests.codes.ok
+            )
+
+            # TODO: (ttung) verify headers
+
+    def test_file_get_specific(self):
+        """
+        Verify we can successfully fetch a specific file UUID+version.
+        """
+        file_uuid = "1245b18c-b36b-4ebb-8220-0e0c6f4a87a4"
+        timestamp = "2017-06-16T19:36:04.240704Z"
+
+        url = urllib.parse.urlunsplit(
+            urllib.parse.urlsplit("")._replace(
+                path="/v1/files/" + file_uuid,
+                query=urllib.parse.urlencode(
+                    (("replica", "aws"),
+                     ("timestamp", timestamp)),
+                    doseq=True,
+                ),
+            )
+        )
+
+        with override_s3_config("hca-dss-test-src"):
+            response = self.assertGetResponse(
+                url,
+                requests.codes.found
+            )
+
+            url = response[0].headers['Location']
+            sha1 = response[0].headers['X-DSS-SHA1']
+            data = requests.get(url)
+            self.assertEqual(len(data.content), 11358)
+
+            # verify that the downloaded data matches the stated checksum
+            hasher = hashlib.sha1()
+            hasher.update(data.content)
+            self.assertEqual(hasher.hexdigest(), sha1)
+
+            # TODO: (ttung) verify more of the headers
+
+    def test_file_get_latest(self):
+        """
+        Verify we can successfully fetch the latest version of a file UUID.
+        """
+        file_uuid = "1245b18c-b36b-4ebb-8220-0e0c6f4a87a4"
+
+        url = urllib.parse.urlunsplit(
+            urllib.parse.urlsplit("")._replace(
+                path="/v1/files/" + file_uuid,
+                query=urllib.parse.urlencode(
+                    (("replica", "aws"),),
+                    doseq=True,
+                ),
+            )
+        )
+
+        with override_s3_config("hca-dss-test-src"):
+            response = self.assertGetResponse(
+                url,
+                requests.codes.found
+            )
+
+            url = response[0].headers['Location']
+            sha1 = response[0].headers['X-DSS-SHA1']
+            data = requests.get(url)
+            self.assertEqual(len(data.content), 8685)
+
+            # verify that the downloaded data matches the stated checksum
+            hasher = hashlib.sha1()
+            hasher.update(data.content)
+            self.assertEqual(hasher.hexdigest(), sha1)
+
+            # TODO: (ttung) verify more of the headers
 
 
 if __name__ == '__main__':
