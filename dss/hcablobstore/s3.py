c045c8ce21d9e80442d240eeabaca416ecb2af92
Tony Tung <tonytung@merly.org>
2017-06-22 13:54:55 -0700

    Code to verify that the blob checksum matches the expected checksum. (#134)

--- dss/hcablobstore/s3.py
+++ dss/hcablobstore/s3.py
@@ -31,3 +31,16 @@ class S3HCABlobStore(HCABlobStore):
             src_bucket, src_object_name,
             dst_bucket, dst_object_name,
             **kwargs)
+
+    def verify_blob_checksum(self, bucket: str, object_name: str, metadata: typing.Dict[str, str]) -> bool:
+        """
+        Given a blob, verify that the checksum on the cloud store matches the checksum in the metadata dictionary.  The
+        keys to the metadata dictionary will be the items in ``COPIED_METADATA``.  Each cloud-specific implementation
+        of ``HCABlobStore`` should extract the correct field and check it against the cloud-provided checksum.
+        :param bucket:
+        :param object_name:
+        :param metadata:
+        :return: True iff the checksum is correct.
+        """
+        checksum = self.handle.get_cloud_checksum(bucket, object_name)
+        return checksum == metadata[HCABlobStore.MANDATORY_METADATA['S3_ETAG']]
