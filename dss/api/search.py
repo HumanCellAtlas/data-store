d3cd351f038afc30f88bc6d2efe33b10687d8b86
Michael Baumann <mbaumann@ucsc.edu>
2017-06-26 06:33:32 -0700

    Support posting Elasticsearch query to search path.

--- dss/api/search.py
+++ dss/api/search.py
@@ -10,13 +10,32 @@ from ..util import connect_elasticsearch
 
 
 def list():
-    es = connect_elasticsearch(os.getenv("DSS_ES_ENDPOINT"), get_logger())  # TODO Use a connection manager
+    es_client = connect_elasticsearch(os.getenv("DSS_ES_ENDPOINT"), get_logger())  # TODO Use a connection manager
     get_logger().debug("Searching for: %s", request.values["query"])
-
     query = json.loads(request.values["query"])
-    s = Search(using=es, index=DSS_ELASTICSEARCH_INDEX_NAME, doc_type=DSS_ELASTICSEARCH_DOC_TYPE)\
-        .query("match", **query)
-    response = s.execute()
-    # TODO Format output
-    results = [{"id": hit.meta.id, "score": hit.meta.score} for hit in response]
-    return {"query": query, "results": results}
+    response = Search(using=es_client,
+                      index=DSS_ELASTICSEARCH_INDEX_NAME,
+                      doc_type=DSS_ELASTICSEARCH_DOC_TYPE).query("match", **query).execute()
+    return {"query": query, "results": format_results(request, response)}
+
+
+def post(query: dict):
+    get_logger().debug("Received posted query: %s", json.dumps(query, indent=4))
+    es_client = connect_elasticsearch(os.getenv("DSS_ES_ENDPOINT"), get_logger())  # TODO Use a connection manager
+    response = Search(using=es_client,
+                      index=DSS_ELASTICSEARCH_INDEX_NAME,
+                      doc_type=DSS_ELASTICSEARCH_DOC_TYPE).update_from_dict(query).execute()
+    return {"query": query, "results": format_results(request, response)}
+
+
+def format_results(request, response):
+    bundles_url_base = request.host_url + request.full_path[1:].replace('search?', 'bundles/')
+    result_list = []
+    for hit in response:
+        result = {
+            'bundle_id': hit.meta.id,
+            'bundle_url': bundles_url_base + hit.meta.id,
+            'search_score': hit.meta.score
+        }
+        result_list.append(result)
+    return result_list
--- tests/test_search.py
+++ tests/test_search.py
@@ -22,8 +22,34 @@ class TestSearch(unittest.TestCase, DSSAsserts):
         DSSAsserts.setup(self)
         self.app = dss.create_app().app.test_client()
 
-    def test_search(self):
+    def test_search_get(self):
         self.assertGetResponse(
             '/v1/search',
             query_string=dict(query=json.dumps(dict(foo=1))),
+            expected_code=requests.codes.ok)
+
+    def test_search_post(self):
+        query = "\
+            { \
+                \"query\": { \
+                    \"bool\": { \
+                        \"must\": [{ \
+                            \"match\": { \
+                                \"files.sample_json.donor.species\": \"Homo sapiens\" \
+                            } \
+                        }, { \
+                            \"match\": { \
+                                \"files.assay_json.single_cell.method\": \"Fluidigm C1\" \
+                            } \
+                        }, { \
+                            \"match\": { \
+                                \"files.sample_json.ncbi_biosample\": \"SAMN04303778\" \
+                            } \
+                        }] \
+                    } \
+                } \
+            }"
+        self.assertPostResponse(
+            '/v1/search',
+            json_request_body=(json.loads(query)),
             expected_code=requests.codes.ok)
