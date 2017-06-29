da7c28acff646ee73762b608dad76ebd8152b4b0
Tony Tung <ttung@chanzuckerberg.com>
2017-06-29 15:00:55 -0400

    *args, **kwargs for upload_file to ensure API happiness for inheritance

--- tests/fixtures/cloud_uploader.py
+++ tests/fixtures/cloud_uploader.py
@@ -11,7 +11,13 @@ class Uploader(object):
     def reset(self) -> None:
         raise NotImplementedError()
 
-    def upload_file(self, local_path: str, remote_path: str, metadata_keys: typing.Dict[str, str]=None) -> None:
+    def upload_file(
+            self,
+            local_path: str,
+            remote_path: str,
+            metadata_keys: typing.Dict[str, str]=None,
+            *args,
+            **kwargs) -> None:
         raise NotImplementedError()
 
 
@@ -35,19 +41,21 @@ class S3Uploader(Uploader):
             local_path: str,
             remote_path: str,
             metadata_keys: typing.Dict[str, str]=None,
-            tags: typing.Dict[str, str]=None) -> None:
+            tags: typing.Dict[str, str]=None,
+            *args,
+            **kwargs) -> None:
         if metadata_keys is None:
             metadata_keys = dict()
         if tags is None:
             tags = dict()
-        args = [
+        subprocess_args = [
             "aws", "s3", "cp",
             os.path.join(self.local_root, local_path),
             "s3://{}/{}".format(self.bucket, remote_path),
             "--metadata",
             json.dumps(metadata_keys)
         ]
-        subprocess.check_call(args)
+        subprocess.check_call(subprocess_args)
 
         tagset = dict(TagSet=[])  # type: typing.Dict[str, typing.List[dict]]
         for tag_key, tag_value in tags.items():
@@ -55,14 +63,14 @@ class S3Uploader(Uploader):
                 dict(
                     Key=tag_key,
                     Value=tag_value))
-        args = [
+        subprocess_args = [
             "aws",
             "s3api",
             "put-object-tagging",
             "--bucket", self.bucket,
             "--key", remote_path,
             "--tagging", json.dumps(tagset)]
-        subprocess.check_call(args)
+        subprocess.check_call(subprocess_args)
 
 
 class GSUploader(Uploader):
@@ -82,21 +90,27 @@ class GSUploader(Uploader):
         # This is not a check_call because of https://github.com/GoogleCloudPlatform/gsutil/issues/417
         subprocess.call(args)
 
-    def upload_file(self, local_path: str, remote_path: str, metadata_keys: typing.Dict[str, str]=None) -> None:
+    def upload_file(
+            self,
+            local_path: str,
+            remote_path: str,
+            metadata_keys: typing.Dict[str, str]=None,
+            *args,
+            **kwargs) -> None:
         if metadata_keys is None:
             metadata_keys = dict()
-        args = [
+        subprocess_args = [
             "gsutil",
         ]
         for metadata_key, metadata_value in metadata_keys.items():
-            args.extend([
+            subprocess_args.extend([
                 "-h",
                 "x-goog-meta-{}:{}".format(metadata_key, metadata_value)
             ])
 
-        args.extend([
+        subprocess_args.extend([
             "cp",
             os.path.join(self.local_root, local_path),
             "gs://{}/{}".format(self.bucket, remote_path),
         ])
-        subprocess.check_call(args)
+        subprocess.check_call(subprocess_args)
