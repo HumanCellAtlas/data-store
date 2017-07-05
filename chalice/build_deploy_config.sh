73277b2c75b50f83421ee2f083d085c7333e4aca
Andrey Kislyuk <kislyuk@gmail.com>
2017-07-05 16:55:04 -0700

    Parameterize chalice & domovoi configs wrt stage name (#189)

--- chalice/build_deploy_config.sh
+++ chalice/build_deploy_config.sh
@@ -16,20 +16,29 @@ get_api_id() {
 }
 
 if [[ $# != 1 ]]; then
-    echo "Usage: $(basename $0) apigateway-stage"
+    echo "Usage: $(basename $0) stage"
     exit 1
 fi
 
-stage=$1
+export stage=$1
 deployed_json="$(dirname $0)/.chalice/deployed.json"
 config_json="$(dirname $0)/.chalice/config.json"
 policy_json="$(dirname $0)/.chalice/policy.json"
-export lambda_name=$(jq -r .$stage.api_handler_name "$deployed_json")
+stage_policy_json="$(dirname $0)/.chalice/policy-${stage}.json"
+export app_name=$(cat "$config_json" | jq -r .app_name)
+policy_template="$(dirname $0)/../iam/policy-templates/${app_name}-lambda.json"
+export lambda_name="${app_name}-${stage}"
+export region_name=$(aws configure get region)
+export account_id=$(aws sts get-caller-identity | jq -r .Account)
 
-if [[ $lambda_name == "null" ]]; then
-    echo "Invalid stage $stage"
+dss_es_domain=${DSS_ES_DOMAIN:-dss-index-$stage}
+if ! aws es describe-elasticsearch-domain --domain-name $dss_es_domain; then
+    echo "Please create AWS elasticsearch domain $dss_es_domain or set DSS_ES_DOMAIN to an existing domain and try again"
     exit 1
 fi
+export DSS_ES_ENDPOINT=$(aws es describe-elasticsearch-domain --domain-name "$dss_es_domain" | jq -r .DomainStatus.Endpoint)
+
+cat "$config_json" | jq ".stages.$stage.api_gateway_stage=env.stage" | sponge "$config_json"
 
 export lambda_arn=$(aws lambda list-functions | jq -r '.Functions[] | select(.FunctionName==env.lambda_name) | .FunctionArn')
 if [[ -z $lambda_arn ]]; then
@@ -39,10 +48,6 @@ else
     export api_id=$(get_api_id)
     cat "$deployed_json" | jq .$stage.api_handler_arn=env.lambda_arn | jq .$stage.rest_api_id=env.api_id | sponge "$deployed_json"
 fi
-
-export DSS_ES_ENDPOINT=$(aws es describe-elasticsearch-domain --domain-name dss-index-$stage | jq -r .DomainStatus.Endpoint)
-export region_name=$(aws configure get region)
-export account_id=$(aws sts get-caller-identity | jq -r .Account)
 
 for var in $EXPORT_ENV_VARS_TO_LAMBDA; do
     cat "$config_json" | jq .stages.$stage.environment_variables.$var=env.$var | sponge "$config_json"
@@ -54,4 +59,5 @@ if [[ ${CI:-} == true ]]; then
     cat "$config_json" | jq .manage_iam_role=false | jq .iam_role_arn=env.iam_role_arn | sponge "$config_json"
 fi
 
-cat "${policy_json}.template" | envsubst '$DSS_S3_TEST_BUCKET $account_id $stage $region_name' > "$policy_json"
+cat "$policy_template" | envsubst '$DSS_S3_TEST_BUCKET $account_id $stage $region_name' > "$policy_json"
+cp "$policy_json" "$stage_policy_json"
--- daemons/build_deploy_config.sh
+++ daemons/build_deploy_config.sh
@@ -12,6 +12,19 @@ export lambda_name="${daemon_name}-${stage}" iam_role_name="${daemon_name}-${sta
 deployed_json="$(dirname $0)/${daemon_name}/.chalice/deployed.json"
 config_json="$(dirname $0)/${daemon_name}/.chalice/config.json"
 policy_json="$(dirname $0)/${daemon_name}/.chalice/policy.json"
+stage_policy_json="$(dirname $0)/${daemon_name}/.chalice/policy-${stage}.json"
+policy_template="$(dirname $0)/../iam/policy-templates/${daemon_name}-lambda.json"
+export region_name=$(aws configure get region)
+export account_id=$(aws sts get-caller-identity | jq -r .Account)
+
+dss_es_domain=${DSS_ES_DOMAIN:-dss-index-$stage}
+if ! aws es describe-elasticsearch-domain --domain-name $dss_es_domain; then
+    echo "Please create AWS elasticsearch domain $dss_es_domain or set DSS_ES_DOMAIN to an existing domain and try again"
+    exit 1
+fi
+export DSS_ES_ENDPOINT=$(aws es describe-elasticsearch-domain --domain-name "$dss_es_domain" | jq -r .DomainStatus.Endpoint)
+
+cat "$config_json" | jq ".stages.$stage.api_gateway_stage=env.stage" | sponge "$config_json"
 
 export lambda_arn=$(aws lambda list-functions | jq -r '.Functions[] | select(.FunctionName==env.lambda_name) | .FunctionArn')
 if [[ -z $lambda_arn ]]; then
@@ -20,10 +33,6 @@ if [[ -z $lambda_arn ]]; then
 else
     cat "$deployed_json" | jq .$stage.api_handler_arn=env.lambda_arn | sponge "$deployed_json"
 fi
-
-export DSS_ES_ENDPOINT=$(aws es describe-elasticsearch-domain --domain-name dss-index-$stage | jq -r .DomainStatus.Endpoint)
-export region_name=$(aws configure get region)
-export account_id=$(aws sts get-caller-identity | jq -r .Account)
 
 for var in $EXPORT_ENV_VARS_TO_LAMBDA; do
     cat "$config_json" | jq .stages.$stage.environment_variables.$var=env.$var | sponge "$config_json"
@@ -34,4 +43,5 @@ if [[ ${CI:-} == true ]]; then
     cat "$config_json" | jq .manage_iam_role=false | jq .iam_role_arn=env.iam_role_arn | sponge "$config_json"
 fi
 
-cat "${policy_json}.template" | envsubst '$DSS_S3_TEST_BUCKET $account_id $stage $region_name' > "$policy_json"
+cat "$policy_template" | envsubst '$DSS_S3_TEST_BUCKET $account_id $stage $region_name' > "$policy_json"
+cp "$policy_json" "$stage_policy_json"
