f06170479c12298f44e5ed47e1a26330158e0ccf
Andrey Kislyuk <kislyuk@gmail.com>
2017-07-05 16:28:40 -0700

    WIP

--- chalice/build_deploy_config.sh
+++ chalice/build_deploy_config.sh
@@ -25,8 +25,8 @@ deployed_json="$(dirname $0)/.chalice/deployed.json"
 config_json="$(dirname $0)/.chalice/config.json"
 policy_json="$(dirname $0)/.chalice/policy.json"
 stage_policy_json="$(dirname $0)/.chalice/policy-${stage}.json"
-policy_template="$(dirname $0)/../iam/policy-templates/dss-lambda.json"
 export app_name=$(cat "$config_json" | jq -r .app_name)
+policy_template="$(dirname $0)/../iam/policy-templates/${app_name}-lambda.json"
 export lambda_name="${app_name}-${stage}"
 export region_name=$(aws configure get region)
 export account_id=$(aws sts get-caller-identity | jq -r .Account)
@@ -47,7 +47,6 @@ if [[ -z $lambda_arn ]]; then
 else
     export api_id=$(get_api_id)
     cat "$deployed_json" | jq .$stage.api_handler_arn=env.lambda_arn | jq .$stage.rest_api_id=env.api_id | sponge "$deployed_json"
-    cat "$deployed_json" | jq .$stage.api_handler_arn=env.lambda_arn | jq .$stage.rest_api_id=env.api_id | sponge "$deployed_json"
 fi
 
 for var in $EXPORT_ENV_VARS_TO_LAMBDA; do
@@ -60,5 +59,5 @@ if [[ ${CI:-} == true ]]; then
     cat "$config_json" | jq .manage_iam_role=false | jq .iam_role_arn=env.iam_role_arn | sponge "$config_json"
 fi
 
-cat "${policy_template}" | envsubst '$DSS_S3_TEST_BUCKET $account_id $stage $region_name' > "${policy_json}"
+cat "$policy_template" | envsubst '$DSS_S3_TEST_BUCKET $account_id $stage $region_name' > "$policy_json"
 cp "$policy_json" "$stage_policy_json"
