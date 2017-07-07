6f88932491150aa4cd4690e3184d46a78a38b1b0
Andrey Kislyuk <kislyuk@gmail.com>
2017-07-06 19:26:22 -0700

    WIP

--- common.mk
+++ common.mk
@@ -1,4 +1,17 @@
 SHELL=/bin/bash
+
 ifndef DSS_HOME
 $(error Please run "source environment" in the data-store repo root directory before running make commands)
+endif
+
+ifeq ($(shell which jq),)
+$(error Please install jq using "apt-get install jq" or "brew install jq")
+endif
+
+ifeq ($(shell which sponge),)
+$(error Please install sponge using "apt-get install moreutils" or "brew install moreutils")
+endif
+
+ifeq ($(shell which envsubst),)
+$(error Please install envsubst using "apt-get install gettext" or "brew install gettext; brew link gettext")
 endif
