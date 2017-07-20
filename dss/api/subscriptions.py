568385880e1dd0e5cfbe102638e6e65732ed8888
Mackey22 <jjm447@cornell.edu>
2017-07-20 15:12:55 -0700

    Make connexion stop complaining about subscriptions file not existing.

---
+++ dss/api/subscriptions.py
@@ -0,0 +1,27 @@
+import datetime
+import io
+import json
+import re
+import typing
+
+import iso8601
+import requests
+
+from flask import jsonify, make_response, redirect, request
+from werkzeug.exceptions import BadRequest
+
+from ..blobstore import BlobNotFoundError
+from ..config import Config
+from ..hcablobstore import FileMetadata, HCABlobStore
+
+
+def get(uuid: str, replica: str):
+    pass
+
+
+def put(uuid: str, extras: dict, replica: str):
+    pass
+
+
+def delete(uuid: str, replica: str):
+    pass
