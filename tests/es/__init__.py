3c091fb43961d8b61fd009f3534a3efee5e1c492
Mackey22 <jjm447@cornell.edu>
2017-07-21 16:15:10 -0700

    Changing log -> logger per mbaumann's review.

--- tests/es/__init__.py
+++ tests/es/__init__.py
@@ -4,8 +4,8 @@ from elasticsearch import Elasticsearch
 
 
 logging.basicConfig(level=logging.INFO)
-log = logging.getLogger(__name__)
-log.setLevel(logging.INFO)
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.INFO)
 
 
 # Check if the Elasticsearch service is running,
@@ -14,8 +14,8 @@ def check_start_elasticsearch_service():
     try:
         es_client = Elasticsearch()
         es_info = es_client.info()
-        log.debug("The Elasticsearch service is running.")
-        log.debug("Elasticsearch info: %s", es_info)
+        logger.debug("The Elasticsearch service is running.")
+        logger.debug("Elasticsearch info: %s", es_info)
         close_elasticsearch_connections(es_client)
     except Exception:
         raise Exception("The Elasticsearch service does not appear to be running on this system, "
@@ -28,7 +28,7 @@ def elasticsearch_delete_index(index_name: str):
         es_client.indices.delete(index=index_name, ignore=[404])
         close_elasticsearch_connections(es_client)  # Prevents end-of-test complaints about open sockets
     except Exception as e:
-        log.warning("Error occurred while removing Elasticsearch index:%s Exception: %s", index_name, e)
+        logger.warning("Error occurred while removing Elasticsearch index:%s Exception: %s", index_name, e)
 
 
 # This prevents open socket errors after the test is over.
