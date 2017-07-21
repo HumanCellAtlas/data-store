c4f65502b6e9162c92e79d91a54d7ab6afd5a0ad
Mackey22 <jjm447@cornell.edu>
2017-07-20 17:55:40 -0700

    Only include percolate mapping if in document index.

--- dss/api/subscriptions.py
+++ dss/api/subscriptions.py
@@ -50,17 +50,19 @@ def _create_elasticsearch_index(es_client, idx):
             logger.debug("Using existing Elasticsearch index: {}".format(idx))
         else:
             logger.debug("Creating new Elasticsearch index: {}".format(idx))
-            index_mapping = {
-                "mappings": {
-                    DSS_ELASTICSEARCH_QUERIES_DOC_TYPE: {
-                        "properties": {
-                            "query": {
-                                "type": "percolator"
+            index_mapping = None
+            if idx == DSS_ELASTICSEARCH_INDEX_NAME:
+                index_mapping = {
+                    "mappings": {
+                        DSS_ELASTICSEARCH_QUERIES_DOC_TYPE: {
+                            "properties": {
+                                "query": {
+                                    "type": "percolator"
+                                }
                             }
                         }
                     }
                 }
-            }
             response = es_client.indices.create(idx, body=index_mapping)
             logger.debug("Index creation response: {}", (json.dumps(response, indent=4)))
 
