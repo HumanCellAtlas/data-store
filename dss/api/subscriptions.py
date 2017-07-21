a06eea40937807bac5f2be0c0b1f14ed27825138
Mackey22 <jjm447@cornell.edu>
2017-07-20 17:01:06 -0700

    Connect to elasticsearch with appropriate indices defined

--- dss/api/subscriptions.py
+++ dss/api/subscriptions.py
@@ -13,6 +13,15 @@ from werkzeug.exceptions import BadRequest
 from ..blobstore import BlobNotFoundError
 from ..config import Config
 from ..hcablobstore import FileMetadata, HCABlobStore
+from ... import DSS_ELASTICSEARCH_INDEX_NAME, DSS_ELASTICSEARCH_DOC_TYPE, get_logger
+from ... import DSS_ELASTICSEARCH_QUERIES_INDEX_NAME, DSS_ELASTICSEARCH_QUERIES_DOC_TYPE
+from ...util import connect_elasticsearch
+
+
+logger = get_logger()
+ELASTICSEARCH_ENDPOINT = os.getenv('DSS_ES_ENDPOINT')
+ELASTICSEARCH_INDICES = [DSS_ELASTICSEARCH_INDEX_NAME,
+                         DSS_ELASTICSEARCH_QUERIES_INDEX_NAME]
 
 
 def get(uuid: str, replica: str):
@@ -25,3 +34,30 @@ def put(uuid: str, extras: dict, replica: str):
 
 def delete(uuid: str, replica: str):
     pass
+
+
+def _connect_elasticsearch_with_indices_defined():
+    es_client = connect_elasticsearch(ELASTICSEARCH_ENDPOINT, logger)
+    for idx in ELASTICSEARCH_INDICES:
+        _create_elasticsearch_index(es_client, idx)
+    return es_client
+
+
+def _create_elasticsearch_index(es_client, idx):
+    try:
+        response = es_client.indices.exists(idx)
+        if response:
+            logger.debug("Using existing Elasticsearch index: {}".format(idx))
+        else:
+            logger.debug("Creating new Elasticsearch index: {}".format(idx))
+            response = es_client.indices.create(idx, body=None)
+            logger.debug("Index creation response: {}", (json.dumps(response, indent=4)))
+
+    except Exception as ex:
+        logger.critical("Unable to create index {} Exception: {}".format(idx))
+        except BlobNotFoundError as ex:
+            return (jsonify(dict(
+                message="Unable to create elasticsearch index {}.".format(idx),
+                exception=str(ex),
+                HTTPStatusCode=requests.codes.internal_server_error)),
+                requests.codes.internal_server_error)
