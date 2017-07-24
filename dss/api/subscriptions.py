3979c9e3238756cf5ef64474a494a129da456785
Mackey22 <jjm447@cornell.edu>
2017-07-24 16:58:59 -0700

    Add common imports and logger assignment.

--- dss/api/subscriptions.py
+++ dss/api/subscriptions.py
@@ -10,10 +10,15 @@ import requests
 from flask import jsonify, make_response, redirect, request
 from werkzeug.exceptions import BadRequest
 
+from .. import get_logger
+from .. import DSS_ELASTICSEARCH_INDEX_NAME, DSS_ELASTICSEARCH_DOC_TYPE, DSS_ELASTICSEARCH_QUERY_TYPE
+from .. import DSS_ELASTICSEARCH_SUBSCRIPTION_INDEX_NAME, DSS_ELASTICSEARCH_SUBSCRIPTION_TYPE
 from ..blobstore import BlobNotFoundError
 from ..config import Config
 from ..hcablobstore import FileMetadata, HCABlobStore
 from ..util.es import ElasticsearchClient, get_elasticsearch_index
+
+logger = get_logger()
 
 
 def get(uuid: str, replica: str):
