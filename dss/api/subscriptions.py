00c5f8ede23f5c28abea60f78971e841a97d2c16
Mackey22 <jjm447@cornell.edu>
2017-07-24 17:21:31 -0700

    Completed PUT /subscriptions functionality.

--- dss/api/subscriptions.py
+++ dss/api/subscriptions.py
@@ -3,6 +3,7 @@ import io
 import json
 import re
 import typing
+import uuid as uuid_
 
 import iso8601
 import requests
@@ -30,8 +31,53 @@ def find(replica: str):
 
 
 def put(extras: dict, replica: str):
-    pass
+    uuid = str(uuid_.uuid4())
+    query = extras['query']
+
+    es_client = ElasticsearchClient.get(logger)
+    get_elasticsearch_index(es_client, DSS_ELASTICSEARCH_SUBSCRIPTION_INDEX_NAME, logger)
+
+    percolate_registration = _register_percolate(es_client, uuid, query)
+
+    if percolate_registration['created']:
+        logger.debug("Percolate query registration succeeded:\n{}".format(percolate_registration))
+    else:
+        logger.critical("Percolate query registration failed:\n{}".format(percolate_registration))
+        return (jsonify(dict(
+            message="Unable to register elasticsearch percolate query {}.".format(uuid),
+            exception=str(ex),
+            HTTPStatusCode=requests.codes.internal_server_error)),
+            requests.codes.internal_server_error)
+
+    subscription_registration = _register_subscription(es_client, uuid, extras)
+    if subscription_registration['created']:
+        logger.debug("Event Subscription succeeded:\n{}".format(subscription_registration))
+    else:
+        logger.critical("Event Subscription failed:\n{}".format(subscription_registration))
+        return (jsonify(dict(
+            message="Unable to register elasticsearch percolate query {}.".format(uuid),
+            exception=str(ex),
+            HTTPStatusCode=requests.codes.internal_server_error)),
+            requests.codes.internal_server_error)
+
+    return jsonify(dict(uuid=uuid)), requests.codes.created
 
 
 def delete(uuid: str, replica: str):
     pass
+
+
+def _register_percolate(es_client: Elasticsearch, uuid: str, query: dict):
+    return es_client.index(index=DSS_ELASTICSEARCH_INDEX_NAME,
+                           doc_type=DSS_ELASTICSEARCH_QUERIES_DOC_TYPE,
+                           id=uuid,
+                           body=query,
+                           refresh=True)
+
+
+def _register_subscription(es_client: Elasticsearch, uuid: str, extras: dict):
+    return es_client.index(index=DSS_ELASTICSEARCH_QUERIES_INDEX_NAME,
+                           doc_type=DSS_ELASTICSEARCH_QUERIES_DOC_TYPE,
+                           id=uuid,
+                           body=extras,
+                           refresh=True)
