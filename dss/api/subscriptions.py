b7f1c78cd35be5381eb49d1aeab77321f5ef5833
Mackey22 <jjm447@cornell.edu>
2017-07-25 17:28:24 -0700

    GET, PUT, and DELETE /subscriptions functionality fully developed

--- dss/api/subscriptions.py
+++ dss/api/subscriptions.py
@@ -8,6 +8,9 @@ import uuid as uuid_
 import iso8601
 import requests
 
+from elasticsearch import Elasticsearch
+from elasticsearch.exceptions import NotFoundError
+from elasticsearch_dsl import Search
 from flask import jsonify, make_response, redirect, request
 from werkzeug.exceptions import BadRequest
 
@@ -23,19 +26,75 @@ logger = get_logger()
 
 
 def get(uuid: str, replica: str):
-    pass
+    owner = request.token_info['email']
+
+    es_client = ElasticsearchClient.get(logger)
+    id_exists = es_client.exists(index=DSS_ELASTICSEARCH_SUBSCRIPTION_INDEX_NAME,
+                                 doc_type=DSS_ELASTICSEARCH_SUBSCRIPTION_TYPE,
+                                 id=uuid)
+    if not id_exists:
+        return jsonify(dict(message="Subscription {} does not exist".format(uuid),
+                            exception="Placeholder",
+                            HTTPStatusCode=requests.codes.not_found)), requests.codes.not_found
+
+    response = es_client.get(index=DSS_ELASTICSEARCH_SUBSCRIPTION_INDEX_NAME,
+                             doc_type=DSS_ELASTICSEARCH_SUBSCRIPTION_TYPE,
+                             id=uuid)
+
+    source = response['_source']
+    source['uuid'] = uuid
+    source['replica'] = replica
+
+    if source['owner'] != owner:
+        return jsonify(dict(
+            message="You don't own subscription {}.".format(uuid),
+            exception="Placeholder",
+            HTTPStatusCode=requests.codes.forbidden)), requests.codes.forbidden
+
+    return jsonify(source), requests.codes.okay
 
 
 def find(replica: str):
-    pass
+    owner = request.token_info['email']
+    es_client = ElasticsearchClient.get(logger)
+
+    search_obj = Search(using=es_client,
+                        index=DSS_ELASTICSEARCH_SUBSCRIPTION_INDEX_NAME,
+                        doc_type=DSS_ELASTICSEARCH_SUBSCRIPTION_TYPE)
+    search = search_obj.query({'match': {'owner': owner}})
+
+    responses = []
+    for hit in search.scan():
+        responses.append({'uuid': hit.meta.id,
+                          'replica': replica,
+                          'owner': owner,
+                          'callback_url': hit.callback_url,
+                          'query': hit.query.to_dict()})
+
+    full_response = {'subscriptions': responses}
+    return jsonify(full_response), requests.codes.okay
 
 
 def put(extras: dict, replica: str):
     uuid = str(uuid_.uuid4())
     query = extras['query']
+    owner = request.token_info['email']
 
     es_client = ElasticsearchClient.get(logger)
-    get_elasticsearch_index(es_client, DSS_ELASTICSEARCH_SUBSCRIPTION_INDEX_NAME, logger)
+
+    index_mapping = {
+        "mappings": {
+            DSS_ELASTICSEARCH_SUBSCRIPTION_TYPE: {
+                "properties": {
+                    "owner": {
+                        "type": "string",
+                        "index": "not_analyzed"
+                    }
+                }
+            }
+        }
+    }
+    get_elasticsearch_index(es_client, DSS_ELASTICSEARCH_SUBSCRIPTION_INDEX_NAME, logger, index_mapping)
 
     percolate_registration = _register_percolate(es_client, uuid, query)
 
@@ -45,10 +104,11 @@ def put(extras: dict, replica: str):
         logger.critical("Percolate query registration failed:\n{}".format(percolate_registration))
         return (jsonify(dict(
             message="Unable to register elasticsearch percolate query {}.".format(uuid),
-            exception=str(ex),
+            exception="Placeholder",
             HTTPStatusCode=requests.codes.internal_server_error)),
             requests.codes.internal_server_error)
 
+    extras['owner'] = owner
     subscription_registration = _register_subscription(es_client, uuid, extras)
     if subscription_registration['created']:
         logger.debug("Event Subscription succeeded:\n{}".format(subscription_registration))
@@ -56,7 +116,7 @@ def put(extras: dict, replica: str):
         logger.critical("Event Subscription failed:\n{}".format(subscription_registration))
         return (jsonify(dict(
             message="Unable to register elasticsearch percolate query {}.".format(uuid),
-            exception=str(ex),
+            exception="Placeholder",
             HTTPStatusCode=requests.codes.internal_server_error)),
             requests.codes.internal_server_error)
 
@@ -64,20 +124,54 @@ def put(extras: dict, replica: str):
 
 
 def delete(uuid: str, replica: str):
-    pass
+    owner = request.token_info['email']
+
+    es_client = ElasticsearchClient.get(logger)
+    id_exists = es_client.exists(index=DSS_ELASTICSEARCH_SUBSCRIPTION_INDEX_NAME,
+                                 doc_type=DSS_ELASTICSEARCH_SUBSCRIPTION_TYPE,
+                                 id=uuid)
+    if not id_exists:
+        return (jsonify(dict(
+                message="Subscription {} does not exist".format(uuid),
+                exception="Placeholder",
+                HTTPStatusCode=requests.codes.not_found)),
+                requests.codes.not_found)
+
+    response = es_client.get(index=DSS_ELASTICSEARCH_SUBSCRIPTION_INDEX_NAME,
+                             doc_type=DSS_ELASTICSEARCH_SUBSCRIPTION_TYPE,
+                             id=uuid)
+
+    source = response['_source']
+
+    if source['owner'] != owner:
+        return (jsonify(dict(
+            message="You don't have rights to delete subscription {}.".format(uuid),
+            exception="Placeholder",
+            HTTPStatusCode=requests.codes.forbidden)),
+            requests.codes.forbidden)
+
+    es_client.delete(index=DSS_ELASTICSEARCH_SUBSCRIPTION_INDEX_NAME,
+                     doc_type=DSS_ELASTICSEARCH_SUBSCRIPTION_TYPE,
+                     id=uuid)
+
+    timestamp = datetime.datetime.utcnow()
+    time_deleted = timestamp.strftime("%Y-%m-%dT%H%M%S.%fZ")
+
+    return jsonify({'timeDeleted': time_deleted}), requests.codes.okay
 
 
 def _register_percolate(es_client: Elasticsearch, uuid: str, query: dict):
+    full_body = {'query': query}
     return es_client.index(index=DSS_ELASTICSEARCH_INDEX_NAME,
-                           doc_type=DSS_ELASTICSEARCH_QUERIES_DOC_TYPE,
+                           doc_type=DSS_ELASTICSEARCH_QUERY_TYPE,
                            id=uuid,
-                           body=query,
+                           body=full_body,
                            refresh=True)
 
 
 def _register_subscription(es_client: Elasticsearch, uuid: str, extras: dict):
-    return es_client.index(index=DSS_ELASTICSEARCH_QUERIES_INDEX_NAME,
-                           doc_type=DSS_ELASTICSEARCH_QUERIES_DOC_TYPE,
+    return es_client.index(index=DSS_ELASTICSEARCH_SUBSCRIPTION_INDEX_NAME,
+                           doc_type=DSS_ELASTICSEARCH_SUBSCRIPTION_TYPE,
                            id=uuid,
                            body=extras,
                            refresh=True)
