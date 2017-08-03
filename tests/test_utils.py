f4cb6fb161999ec0aecaa7fc5da724d12f5dd841
Tony Tung <ttung@chanzuckerberg.com>
2017-08-02 17:27:30 -0700

    Utility to generate the S3 chunk size as per the "HCA Storage System Disk Format" spec.

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -7,7 +7,7 @@ import unittest
 pkg_root = os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))  # noqa
 sys.path.insert(0, pkg_root)  # noqa
 
-from dss.util.aws import ARN
+from dss.util.aws import ARN, get_s3_chunk_size
 from tests import infra
 
 infra.start_verbose_logging()
@@ -18,6 +18,14 @@ class TestUtils(unittest.TestCase):
         arn.get_region()
         arn.get_account_id()
         str(arn)
+
+    def test_s3_chunk_size(self):
+        self.assertEqual(get_s3_chunk_size(10000 * 64 * 1024 * 1024 - 1), 64 * 1024 * 1024)
+        self.assertEqual(get_s3_chunk_size(10000 * 64 * 1024 * 1024 + 0), 64 * 1024 * 1024)
+        self.assertEqual(get_s3_chunk_size(10000 * 64 * 1024 * 1024 + 1), 65 * 1024 * 1024)
+        self.assertEqual(get_s3_chunk_size(10000 * 65 * 1024 * 1024 - 1), 65 * 1024 * 1024)
+        self.assertEqual(get_s3_chunk_size(10000 * 65 * 1024 * 1024 + 0), 65 * 1024 * 1024)
+        self.assertEqual(get_s3_chunk_size(10000 * 65 * 1024 * 1024 + 1), 66 * 1024 * 1024)
 
 if __name__ == '__main__':
     unittest.main()
