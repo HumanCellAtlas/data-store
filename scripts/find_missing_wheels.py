82b4624987c344ecc168120237777ec875e033c6
Andrey Kislyuk <kislyuk@gmail.com>
2017-08-24 14:11:46 -0700

    WIP

--- scripts/find_missing_wheels.py
+++ scripts/find_missing_wheels.py
@@ -5,7 +5,7 @@ This script manages installation dependencies for AWS Lambda functions.
 See http://chalice.readthedocs.io/en/latest/topics/packaging.html.
 """
 
-import os, argparse, platform
+import os, argparse, platform, subprocess, glob
 from tempfile import TemporaryDirectory
 
 from chalice.utils import OSUtils
@@ -14,23 +14,23 @@ from chalice.deploy.packager import DependencyBuilder
 parser = argparse.ArgumentParser(description=__doc__)
 parser.add_argument("pip_reqs")
 parser.add_argument("--build-wheels", action="store_true")
+parser.add_argument("--wheel-dir", default="vendor.in")
 args = parser.parse_args()
 
 with TemporaryDirectory() as td:
     compat_wheels, missing_wheels = DependencyBuilder(OSUtils())._download_dependencies(td, args.pip_reqs)
-    for w in missing_wheels:
-        print(w.identifier, w.filename)
-'''
-    need_wheels = [w for w in missing_wheels if not glob("vendor.in/{}.whl".format(w.identifier))]
+    need_wheels = [w for w in missing_wheels if not os.path.exists(os.path.join(args.wheel_dir, w.identifier))]
     if need_wheels:
-        need_wheel_ids = " ".join(wheel.identifier for wheel in missing_wheels)
         if args.build_wheels:
             if platform.system != "Linux":
-                parser.exit("{}: Expected to run on a Linux system.".format(parser.prog))
-            #pip download --dest vendor.in $$($(DSS_HOME)/scripts/find_missing_wheels.py requirements.txt)
-            #pip wheel --wheel-dir vendor.in vendor.in/*.tar.gz
-            #rm -f vendor.in/*.tar.gz
-            print('Please run "git add vendor.in/*.whl" and commit the result.')
+                parser.exit(f"{parser.prog}: Expected to run on a Linux system.")
+            for wheel in need_wheels:
+                wd = os.path.join(args.wheel_dir, w.identifier)
+                os.mkdir(wd)
+                subprocess.check_output(["pip", "download", wheel.identifier], cwd=wd)
+                if glob.glob(os.path.join(wd, "*.tar.gz")):
+                    subprocess.check_output("pip wheel *.tar.gz && rm -f *.tar.gz", shell=True, cwd=wd)
+            print(f'Please run "git add {args.wheel_dir}" and commit the result.')
         else:
-            parser.exit('Missing wheels: {}. Please run "FIXME --build-wheels" in a Linux VM'.format(need_wheel_ids))
-'''
+            w = " ".join(wheel.identifier for wheel in need_wheels)
+            parser.exit(f'Missing wheels: {w}. Please run "FIXME --build-wheels" in a Linux VM')
