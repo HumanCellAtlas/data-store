4df4cabea9364fb36f850754deabf6ba7930e217
Trent Smith <tsmith12@ucsc.edu>
2017-08-29 14:38:55 -0700

    WIP - adding TestGCPIndexer

--- tests/test_indexer.py
+++ tests/test_indexer.py
@@ -337,6 +337,48 @@ class TestAWSIndexer(unittest.TestCase, TestIndexerBase):
         return sample_event
 
 
+class TestGCPIndexer(unittest.TestCase, TestIndexerBase):
+
+    http_server_address = "127.0.0.1"
+    http_server_port = 8729
+
+    @classmethod
+    def setUpClass(cls):
+        cls.replica = "gcp"
+        Config.set_config(DeploymentStage.TEST_FIXTURE)
+        cls.blobstore, _, cls.test_fixture_bucket = Config.get_cloud_specific_handles(cls.replica)
+        Config.set_config(DeploymentStage.TEST)
+        _, _, cls.test_bucket = Config.get_cloud_specific_handles(cls.replica)
+        cls.es_server = ElasticsearchServer()
+        os.environ['DSS_ES_PORT'] = str(cls.es_server.port)
+        try:
+            cls.http_server = HTTPServer((cls.http_server_address, cls.http_server_port), PostTestHandler)
+            cls.http_server_thread = threading.Thread(target=cls.http_server.serve_forever)
+            cls.http_server_thread.start()
+        except OSError as ex:
+            logger.warning(f"Warning: occurred during test suite setup {ex}")
+
+    @staticmethod
+    def process_new_indexable_object(event, logger):
+        process_new_gs_indexable_object(event, logger)
+
+    def setUp(self):
+        self.app = dss.create_app().app.test_client()
+        elasticsearch_delete_index("_all")
+        PostTestHandler.reset()
+
+    def tearDown(self):
+        self.app = None
+        self.storageHelper = None
+
+    def create_sample_bundle_created_event(self, bundle_key: str) -> Dict:
+        with open(os.path.join(os.path.dirname(__file__), "sample_gs_bundle_created_event.json")) as fh:
+            sample_event = json.load(fh)
+        sample_event["bucket"] = self.test_bucket
+        sample_event["name"] = bundle_key
+        return sample_event
+
+
 class BundleBuilder:
     def __init__(self, replica, bundle_id=None, bundle_version=None):
         self.blobstore, _, _ = Config.get_cloud_specific_handles(replica)
