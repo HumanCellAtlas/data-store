a90b715fecfb6a6951aea5adaa69b7f976410db4
Andrey Kislyuk <kislyuk@gmail.com>
2017-09-06 15:53:43 -0700

    WIP

--- scripts/smoketest.py
+++ scripts/smoketest.py
@@ -36,21 +36,23 @@ def run(command, **kwargs):
 run("git clone --depth 1 --recurse-submodules https://github.com/HumanCellAtlas/data-store-cli")
 run("http --check-status https://${API_HOST}/v1/swagger.json > data-store-cli/swagger.json")
 run("pip install -r data-store-cli/requirements.txt")
-run("python -c 'import sys, hca.regenerate_api as r; r.generate_python_bindings(sys.argv[1])' swagger.json", cwd="data-store-cli")
+run("python -c 'import sys, hca.regenerate_api as r; r.generate_python_bindings(sys.argv[1])' swagger.json",
+    cwd="data-store-cli")
 run("find data-store-cli/hca -name '*.pyc' -delete")
 run("pip install --upgrade .", cwd="data-store-cli")
 
-run("hca upload --replica aws --staging-bucket $DSS_S3_BUCKET_TEST --file-or-dir data-bundle-examples/10X_v2/pbmc8k > bundle_upload_result.json")
-run("hca download --replica aws $(jq -r .bundle_uuid bundle_upload_result.json)")
+bundle_dir = "data-bundle-examples/10X_v2/pbmc8k"
+run(f"hca upload --replica aws --staging-bucket $DSS_S3_BUCKET_TEST --file-or-dir {bundle_dir} > upload.json")
+run("hca download --replica aws $(jq -r .bundle_uuid upload.json)")
 for i in range(10):
     try:
-        run("http -v --check-status https://${API_HOST}/v1/bundles/$(jq -r .bundle_uuid bundle_upload_result.json)?replica=gcp")
+        run("http -v --check-status https://${API_HOST}/v1/bundles/$(jq -r .bundle_uuid upload.json)?replica=gcp")
         break
     except SystemExit:
         time.sleep(1)
 else:
     parser.exit(RED("Failed to replicate bundle from AWS to GCP"))
-run("hca download --replica gcp $(jq -r .bundle_uuid bundle_upload_result.json)")
+run("hca download --replica gcp $(jq -r .bundle_uuid upload.json)")
 
 run("hca post-search")
 run('jq -n .query.match.foo=1 | http -v --check-status https://${API_HOST}/v1/search')
