cecaf415dea95e7abe061a7bde0c50e708ff5da8
Tony Tung <ttung@chanzuckerberg.com>
2017-09-26 23:46:28 -0700

    Use composition instead of inheritance for copy + metadata write.
    This will simplify the process of migrating to the parallel copy.
    
    Connects to #393

--- dss/events/chunkedtask/s3copyclient.py
+++ dss/events/chunkedtask/s3copyclient.py
@@ -1,6 +1,7 @@
 import typing
 
-from chainedawslambda.s3copyclient import S3CopyTask, S3CopyTaskKeys
+from chainedawslambda import Task
+from chainedawslambda.s3copyclient import S3CopyTask
 
 from ...api import files
 from ...blobstore.s3 import S3BlobStore
@@ -8,28 +9,69 @@ from ...blobstore.s3 import S3BlobStore
 # this must match the lambda name in daemons/Makefile
 AWS_S3_COPY_AND_WRITE_METADATA_CLIENT_PREFIX = "dss-s3-copy-write-metadata-"
 
-class S3CopyWriteBundleTaskKeys(S3CopyTaskKeys):
+
+class S3CopyWriteBundleTaskKeys:
     METADATA = "metadata"
     FILE_UUID = "file_uuid"
     FILE_VERSION = "file_version"
+    STAGE = "stage"
+    COPY_STATE = "copy_state"
+
+    class S3CopyWriteBundleTaskStages:
+        COPYING = "copying"
+        WRITING_METADATA = "writing_metadata"
 
 
-class S3CopyWriteBundleTask(S3CopyTask):
+class S3CopyWriteBundleTask(Task[dict, dict]):
     """
     This is a chunked task that does a multipart copy from one blob to another and writes a bundle manifest.
     """
     def __init__(self, state: dict, *args, **kwargs) -> None:
-        super().__init__(state)
         self.metadata = state[S3CopyWriteBundleTaskKeys.METADATA]
         self.file_uuid = state[S3CopyWriteBundleTaskKeys.FILE_UUID]
         self.file_version = state[S3CopyWriteBundleTaskKeys.FILE_VERSION]
+        self.stage = state.get(
+            S3CopyWriteBundleTaskKeys.STAGE,
+            S3CopyWriteBundleTaskKeys.S3CopyWriteBundleTaskStages.COPYING)
+        if S3CopyWriteBundleTaskKeys.COPY_STATE in state:
+            self.copy_task = S3CopyTask(state[S3CopyWriteBundleTaskKeys.COPY_STATE])
+        else:
+            self.copy_task = S3CopyTask(state)
 
     def get_state(self) -> dict:
-        state = super().get_state()
-        state[S3CopyWriteBundleTaskKeys.METADATA] = self.metadata
-        state[S3CopyWriteBundleTaskKeys.FILE_UUID] = self.file_uuid
-        state[S3CopyWriteBundleTaskKeys.FILE_VERSION] = self.file_version
-        return state
+        return {
+            S3CopyWriteBundleTaskKeys.METADATA: self.metadata,
+            S3CopyWriteBundleTaskKeys.FILE_UUID: self.file_uuid,
+            S3CopyWriteBundleTaskKeys.FILE_VERSION: self.file_version,
+            S3CopyWriteBundleTaskKeys.STAGE: self.stage,
+            S3CopyWriteBundleTaskKeys.COPY_STATE: self.copy_task.get_state(),
+        }
+
+    @staticmethod
+    def setup_copy_task(
+            metadata: str,
+            file_uuid: str,
+            file_version: str,
+            source_bucket: str, source_key: str,
+            destination_bucket: str, destination_key: str,
+            part_size_calculator: typing.Callable[[int], int],
+    ) -> dict:
+        """
+        Returns the initial state for a S3CopyWriteBundleTask to copy a blob from s3://`source_bucket`/`source_key` to
+        s3://`destination_bucket`/`destination_key`, and to write the bundle metadata.
+        """
+        return {
+            S3CopyWriteBundleTaskKeys.FILE_UUID: file_uuid,
+            S3CopyWriteBundleTaskKeys.FILE_VERSION: file_version,
+            S3CopyWriteBundleTaskKeys.METADATA: metadata,
+            S3CopyWriteBundleTaskKeys.STAGE: S3CopyWriteBundleTaskKeys.S3CopyWriteBundleTaskStages.COPYING,
+            S3CopyWriteBundleTaskKeys.COPY_STATE:
+                S3CopyTask.setup_copy_task(
+                    source_bucket, source_key,
+                    destination_bucket, destination_key,
+                    part_size_calculator,
+                ),
+        }
 
     @property
     def expected_max_one_unit_runtime_millis(self) -> int:
@@ -37,16 +79,28 @@ class S3CopyWriteBundleTask(S3CopyTask):
         return 60 * 1000
 
     def run_one_unit(self) -> typing.Optional[dict]:
-        result = super().run_one_unit()
-        if result is None:
-            return result
+        if self.stage == S3CopyWriteBundleTaskKeys.S3CopyWriteBundleTaskStages.COPYING:
+            result = self.copy_task.run_one_unit()
+            if result is None:
+                return result
 
-        # subtask is done!  let's write the file metadata.
-        handle = S3BlobStore()
+            # copying is done!
+            self.stage = S3CopyWriteBundleTaskKeys.S3CopyWriteBundleTaskStages.WRITING_METADATA
+            return None
 
-        files.write_file_metadata(handle, self.destination_bucket, self.file_uuid, self.file_version, self.metadata)
+        if self.stage == S3CopyWriteBundleTaskKeys.S3CopyWriteBundleTaskStages.WRITING_METADATA:
+            handle = S3BlobStore()
 
-        return {
-            S3CopyWriteBundleTaskKeys.FILE_UUID: self.file_uuid,
-            S3CopyWriteBundleTaskKeys.FILE_VERSION: self.file_version,
-        }
+            files.write_file_metadata(
+                handle,
+                self.copy_task.destination_bucket,
+                self.file_uuid,
+                self.file_version,
+                self.metadata)
+
+            return {
+                S3CopyWriteBundleTaskKeys.FILE_UUID: self.file_uuid,
+                S3CopyWriteBundleTaskKeys.FILE_VERSION: self.file_version,
+            }
+
+        raise ValueError("unknown state")
