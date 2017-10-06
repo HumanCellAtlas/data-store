8ea56a92ccefb88dba15d161a0f17212e6a78e48
Tony Tung <ttung@chanzuckerberg.com>
2017-10-06 12:58:54 -0700

    split infra.py into individual files.
    it's getting to be a bit of a kitchen sink.

---
+++ tests/infra/logging.py
@@ -0,0 +1,10 @@
+import logging
+
+
+def start_verbose_logging():
+    logging.basicConfig(level=logging.INFO)
+    for logger_name in logging.Logger.manager.loggerDict:  # type: ignore
+        if (logger_name.startswith("botocore") or
+                logger_name.startswith("boto3.resources") or
+                logger_name.startswith("elasticsearch")):
+            logging.getLogger(logger_name).setLevel(logging.WARNING)
