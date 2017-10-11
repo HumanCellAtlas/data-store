721e83ef86b6f4e4ae5c9e34095eee1b26bd7495
Tony Tung <ttung@chanzuckerberg.com>
2017-10-11 10:32:08 -0700

    Allow users of the URLBuilder API to set the query.
    Also adds a `has_query()` method, which returns True if there's a query field with a given name.
    A

--- dss/util/__init__.py
+++ dss/util/__init__.py
@@ -1,4 +1,4 @@
-from urllib.parse import SplitResult, parse_qs, urlencode, urlparse, urlunsplit
+from urllib.parse import SplitResult, urlencode, urlunsplit
 
 import typing
 
@@ -26,7 +26,13 @@ class UrlBuilder:
         self.splitted = SplitResult("", "", "", "", "")
         self.query = list()
 
-    def set(self, scheme: str=None, netloc: str=None, path: str=None, fragment: str=None) -> "UrlBuilder":
+    def set(
+            self,
+            scheme: str=None,
+            netloc: str=None,
+            path: str=None,
+            query: typing.Sequence[typing.Tuple[str, str]]=None,
+            fragment: str=None) -> "UrlBuilder":
         kwargs = dict()
         if scheme is not None:
             kwargs['scheme'] = scheme
@@ -34,11 +40,20 @@ class UrlBuilder:
             kwargs['netloc'] = netloc
         if path is not None:
             kwargs['path'] = path
+        if query is not None:
+            self.query = query
         if fragment is not None:
             kwargs['fragment'] = fragment
         self.splitted = self.splitted._replace(**kwargs)
 
         return self
+
+    def has_query(self, needle_query_name: str) -> bool:
+        """Returns True iff the URL being constructed has a query field with name `needle_query_name`."""
+        for query_name, _ in self.query:
+            if query_name == needle_query_name:
+                return True
+        return False
 
     def add_query(self, query_name: str, query_value: str) -> "UrlBuilder":
         self.query.append((query_name, query_value))
