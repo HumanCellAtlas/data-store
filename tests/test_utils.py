184a93f390970942f35607abd17141cac36de80d
Tony Tung <tonytung@merly.org>
2017-10-11 19:54:03 -0700

    Allow users of the URLBuilder API to set the query. (#548)
    Also adds a `has_query()` method, which returns True if there's a query field with a given name.
    A

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -7,12 +7,14 @@ import unittest
 pkg_root = os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))  # noqa
 sys.path.insert(0, pkg_root)  # noqa
 
+from dss.util import UrlBuilder
 from dss.util.aws import ARN, get_s3_chunk_size
-from tests import infra
+from tests.infra import logging
 
-infra.start_verbose_logging()
+logging.start_verbose_logging()
 
-class TestUtils(unittest.TestCase):
+
+class TestAwsUtils(unittest.TestCase):
     def test_aws_utils(self):
         arn = ARN(service="sns", resource="my_topic")
         arn.get_region()
@@ -26,6 +28,55 @@ class TestUtils(unittest.TestCase):
         self.assertEqual(get_s3_chunk_size(10000 * 65 * 1024 * 1024 - 1), 65 * 1024 * 1024)
         self.assertEqual(get_s3_chunk_size(10000 * 65 * 1024 * 1024 + 0), 65 * 1024 * 1024)
         self.assertEqual(get_s3_chunk_size(10000 * 65 * 1024 * 1024 + 1), 66 * 1024 * 1024)
+
+
+class TestUrlBuilder(unittest.TestCase):
+    def test_simple(self):
+        builder = UrlBuilder().set(
+            scheme="https",
+            netloc="humancellatlas.org",
+            path="/abc",
+            query=[
+                ("ghi", "1"),
+                ("ghi", "2"),
+            ],
+            fragment="def")
+        self.assertEqual("https://humancellatlas.org/abc?ghi=1&ghi=2#def", str(builder))
+
+    def test_has_query(self):
+        builder = UrlBuilder().set(
+            scheme="https",
+            netloc="humancellatlas.org",
+            path="/abc",
+            query=[
+                ("ghi", "1"),
+                ("ghi", "2"),
+            ],
+            fragment="def")
+        self.assertTrue(builder.has_query("ghi"))
+        self.assertFalse(builder.has_query("abc"))
+
+    def test_add_query(self):
+        builder = UrlBuilder().set(
+            scheme="https",
+            netloc="humancellatlas.org",
+            path="/abc",
+            query=[
+                ("ghi", "1"),
+                ("ghi", "2"),
+            ],
+            fragment="def")
+        self.assertTrue(builder.has_query("ghi"))
+        self.assertFalse(builder.has_query("abc"))
+
+        self.assertEqual("https://humancellatlas.org/abc?ghi=1&ghi=2#def", str(builder))
+
+        builder.add_query("abc", "3")
+        self.assertTrue(builder.has_query("ghi"))
+        self.assertTrue(builder.has_query("abc"))
+
+        self.assertEqual("https://humancellatlas.org/abc?ghi=1&ghi=2&abc=3#def", str(builder))
+
 
 if __name__ == '__main__':
     unittest.main()
