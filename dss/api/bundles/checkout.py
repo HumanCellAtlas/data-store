33b8d1e6828c4f122a0b7857254065aa8a65e18d
Roman Kisin <roman@parleypro.com>
2017-11-06 20:26:14 -0800

    Starting to hookup HTTP handler to SFN

--- dss/api/bundles/checkout.py
+++ dss/api/bundles/checkout.py
@@ -1,20 +1,34 @@
 import json
 
 import boto3
+import os
 import requests
 from flask import jsonify
 
+from dss.util.checkout import get_execution_id
 from ... import dss_handler, get_logger
 
 @dss_handler
 def post(uuid: str, json_request_body: dict, replica: str, version: str = None):
 
     sfn = boto3.client('stepfunctions')
-    lambda_input = {"input": "some input"}
-    input = json.dumps(lambda_input).encode('utf8')
+    sts_client = boto3.client("sts")
+
+    email = json_request_body['email']
+
+    sfn_input = {"bundle": uuid, "version": version, "email": email}
+    if "destination" in json_request_body:
+        sfn_input["bucket"] = json_request_body["destination"]
+    input = json.dumps(sfn_input)
+
+    region = boto3.Session().region_name
+    accountid = sts_client.get_caller_identity()['Account']
+    stage = os.environ["DSS_DEPLOYMENT_STAGE"]
+    state_machine_arn = f"arn:aws:states:{region}:{accountid}:stateMachine:dss-checkout-sfn-{stage}"
     response = sfn.start_execution(
-        stateMachineArn='my_state_machine_ARN',
-        name="a unique name",
+        stateMachineArn=state_machine_arn,
+        name=get_execution_id(),
         input=input
     )
-    return jsonify(dict(version=version, execution_id=response["executionArn"])), requests.codes.ok
+    print(">>>>>")
+    return jsonify(dict(version=version, url='TBD', execution_id=response["executionArn"])), requests.codes.ok
