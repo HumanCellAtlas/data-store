04a4d525f083e9c3e3b3cc162f6857593b3764d4
Roman Kisin <roman@parleypro.com>
2017-11-28 12:20:26 -0800

    Fault tolerance features

--- daemons/dss-checkout-sfn/app.py
+++ daemons/dss-checkout-sfn/app.py
@@ -2,6 +2,7 @@ import os
 import sys
 
 import domovoi
+import time
 
 pkg_root = os.path.abspath(os.path.join(os.path.dirname(__file__), 'domovoilib'))  # noqa
 sys.path.insert(0, pkg_root)  # noqa
