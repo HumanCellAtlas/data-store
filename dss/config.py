d64d672108fb9791c6c9f89bf2ff5a53241cf734
Trent Smith <tsmith12@ucsc.edu>
2017-11-29 15:04:11 -0800

    swap index and alias name parameters around

--- dss/config.py
+++ dss/config.py
@@ -181,7 +181,7 @@ class Config:
                           ) -> str:
         """Returns the index name"""
         deployment_stage = os.environ["DSS_DEPLOYMENT_STAGE"]
-        index = f"dss-{index_type.name}-{replica.name}-{deployment_stage}"
+        index = f"dss-{index_type.name}-{deployment_stage}-{replica.name}"
         if version:
             index = f"{index}-{version}"
         if Config._CURRENT_CONFIG == BucketConfig.TEST:
@@ -192,7 +192,7 @@ class Config:
     def get_es_alias_name(index_type: ESIndexType, replica: Replica) -> str:
         """Returns the alias for indexes"""
         deployment_stage = os.environ["DSS_DEPLOYMENT_STAGE"]
-        index = f"dss-{index_type.name}-{replica.name}-{deployment_stage}-alias"
+        index = f"dss-{index_type.name}-{deployment_stage}-{replica.name}"
         if Config._CURRENT_CONFIG == BucketConfig.TEST:
             index = f"{index}.{IndexSuffix.name}"
         return index
