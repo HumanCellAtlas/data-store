2ffbf843fb729c25e1cd13f97521710c622f65b2
Tony Tung <tonytung@merly.org>
2017-12-05 14:59:50 -0800

    Refactor the SFN invoke code used in the checkout service. (#706)

--- dss/api/bundles/checkout.py
+++ dss/api/bundles/checkout.py
@@ -1,19 +1,13 @@
-import os
-import json
-
 import requests
 import boto3
 from flask import jsonify
 
 from ..bundles import get_bundle
-from ... import Config, dss_handler
+from ... import Config, dss_handler, stepfunctions
 from ...util.checkout import get_execution_id
 
 @dss_handler
 def post(uuid: str, json_request_body: dict, replica: str, version: str = None):
-    sfn = boto3.client('stepfunctions')
-    sts_client = boto3.client("sts")
-
     email = json_request_body['email']
     dss_bucket = Config.get_s3_bucket()
 
@@ -23,17 +17,9 @@ def post(uuid: str, json_request_body: dict, replica: str, version: str = None):
     sfn_input = {"dss_bucket": dss_bucket, "bundle": uuid, "version": version, "email": email, "replica": replica}
     if "destination" in json_request_body:
         sfn_input["bucket"] = json_request_body["destination"]
-    execution_input = json.dumps(sfn_input)
 
-    region = boto3.Session().region_name
-    accountid = sts_client.get_caller_identity()['Account']
-    stage = os.environ["DSS_DEPLOYMENT_STAGE"]
-    state_machine_arn = f"arn:aws:states:{region}:{accountid}:stateMachine:dss-checkout-sfn-{stage}"
-    response = sfn.start_execution(
-        stateMachineArn=state_machine_arn,
-        name=get_execution_id(),
-        input=execution_input
-    )
+    response = stepfunctions.step_functions_invoke("dss-checkout-sfn-{stage}", get_execution_id(), sfn_input)
+
     return jsonify(dict(checkout_job_id=response["executionArn"])), requests.codes.ok
 
 @dss_handler
