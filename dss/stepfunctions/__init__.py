d3ed1d743dfacc68f0a1b04ed5b3422a85872415
Brian Hannafious <brian@connectedsignals.com>
2017-12-07 13:14:43 -0800

    address review comments

--- dss/stepfunctions/__init__.py
+++ dss/stepfunctions/__init__.py
@@ -2,21 +2,18 @@ import json
 import os
 import typing
 
-import boto3
+from ..util.aws import ARN
+from ..util.aws.clients import stepfunctions
 
 
 def step_functions_arn(state_machine_name_template: str) -> str:
     """
     The ARN of a state machine, with name derived from `state_machine_name_template`, with string formatting to
     replace {stage} with the dss deployment stage.
-    :param state_machine_name_template:
-    :return:
     """
 
-    sts_client = boto3.client("sts")
-
-    region = boto3.Session().region_name
-    accountid = sts_client.get_caller_identity()['Account']
+    region = ARN.get_region()
+    accountid = ARN.get_account_id()
     stage = os.environ["DSS_DEPLOYMENT_STAGE"]
     sfn_name = state_machine_name_template.format(stage=stage)
     state_machine_arn = f"arn:aws:states:{region}:{accountid}:stateMachine:{sfn_name}"
@@ -27,15 +24,10 @@ def step_functions_execution_arn(state_machine_name_template: str, execution_nam
     """
     The ARN of a state machine execution, with name derived from `state_machine_name_template`, with string formatting
     to replace {stage} with the dss deployment stage.
-    :param state_machine_name_template:
-    :param execution_name:
-    :return:
     """
 
-    sts_client = boto3.client("sts")
-
-    region = boto3.Session().region_name
-    accountid = sts_client.get_caller_identity()['Account']
+    region = ARN.get_region()
+    accountid = ARN.get_account_id()
     stage = os.environ["DSS_DEPLOYMENT_STAGE"]
     sfn_name = state_machine_name_template.format(stage=stage)
     state_machine_execution_arn = f"arn:aws:states:{region}:{accountid}:execution:{sfn_name}:{execution_name}"
@@ -46,69 +38,52 @@ def step_functions_invoke(state_machine_name_template: str, execution_name: str,
     """
     Invoke a step functions state machine.  The name of the state machine to be invoked will be derived from
     `state_machine_name_template`, with string formatting to replace {stage} with the dss deployment stage.
-    :param state_machine_name_template:
-    :param execution_name:
-    :param input:
-    :return:
     """
-
-    sfn = boto3.client('stepfunctions')
 
     execution_input = json.dumps(input)
     state_machine_arn = step_functions_arn(state_machine_name_template)
 
-    response = sfn.start_execution(
+    response = stepfunctions.start_execution(
         stateMachineArn=state_machine_arn,
         name=execution_name,
         input=execution_input
     )
+
     return response
 
 
 def step_functions_describe_execution(state_machine_name_template: str, execution_name: str) -> typing.Any:
     """
     Return description of a step function exectuion, possible in-progress, completed, errored, etc.
-    :param state_machine_name_template:
-    :param execution_name:
-    :return:
     """
-
-    sfn = boto3.client('stepfunctions')
 
     execution_arn = step_functions_execution_arn(state_machine_name_template, execution_name)
 
-    resp = sfn.describe_execution(
+    resp = stepfunctions.describe_execution(
         executionArn=execution_arn
     )
 
     return resp
 
 
-def step_functions_list_executions(state_machine_name_template: str, k_results_per_page: int=100) -> typing.Iterable:
+def step_functions_list_executions(state_machine_name_template: str, k_results_per_page: int=None) -> typing.Iterable:
     """
     List step function executions, peforming paging in the background.
     Maximum 100 results per page.
-    :param state_machine_name_template:
-    :param k_results_per_page:
-    :return:
     """
-
-    sfn = boto3.client('stepfunctions')
 
     state_machine_arn = step_functions_arn(state_machine_name_template)
 
     kwargs = dict(
         stateMachineArn=state_machine_arn,
-        maxResults=k_results_per_page
     )
 
-    while True:
-        resp = sfn.list_executions(**kwargs)
+    if k_results_per_page is not None:
+        kwargs['maxResults'] = k_results_per_page
 
-        for e in resp['executions']:
-            yield e
+    paginator = stepfunctions.get_paginator('list_executions')
+    page_iterator = paginator.paginate(**kwargs)
 
-        if resp.get('nextToken', False):
-            kwargs['nextToken'] = resp['nextToken']
-        else:
-            break
+    for page in page_iterator:
+        for ex in page['executions']:
+            yield ex
