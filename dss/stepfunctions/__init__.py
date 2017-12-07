e214ec101ccbfc45aa907e0be870d0e084df4619
Tony Tung <ttung@chanzuckerberg.com>
2017-12-07 14:12:54 -0800

    Use aws.ARN.get_region() and aws.ARN.get_account_id()

--- dss/stepfunctions/__init__.py
+++ dss/stepfunctions/__init__.py
@@ -4,6 +4,8 @@ import typing
 
 import boto3
 
+from ..util import aws
+
 
 def step_functions_invoke(state_machine_name_template: str, execution_name: str, input) -> typing.Any:
     """
@@ -16,12 +18,11 @@ def step_functions_invoke(state_machine_name_template: str, execution_name: str,
     """
 
     sfn = boto3.client('stepfunctions')
-    sts_client = boto3.client("sts")
 
     execution_input = json.dumps(input)
 
-    region = boto3.Session().region_name
-    accountid = sts_client.get_caller_identity()['Account']
+    region = aws.ARN.get_region()
+    accountid = aws.ARN.get_account_id()
     stage = os.environ["DSS_DEPLOYMENT_STAGE"]
     sfn_name = state_machine_name_template.format(stage=stage)
     state_machine_arn = f"arn:aws:states:{region}:{accountid}:stateMachine:{sfn_name}"
