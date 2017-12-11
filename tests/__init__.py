ac350bb0dfafcf09a16d6c4713dcf6506e488ee2
Matt Weiden <538456+mweiden@users.noreply.github.com>
2017-12-11 14:11:10 -0800

    Add the 'eventually' convenience method (#727)

--- tests/__init__.py
+++ tests/__init__.py
@@ -1,5 +1,33 @@
 import datetime
+import functools
+import time
 
 
 def get_version():
     return datetime.datetime.utcnow().strftime("%Y-%m-%dT%H%M%S.%fZ")
+
+
+def eventually(timeout: float, interval: float, errors: set={AssertionError}):
+    """
+    @eventually runs a test until all assertions are satisfied or a timeout is reached.
+    :param timeout: time until the test fails
+    :param interval: time between attempts of the test
+    :param errors: the exceptions to catch and retry on
+    :return: the result of the function or a raised assertion error
+    """
+    def decorate(func):
+        @functools.wraps(func)
+        def call(*args, **kwargs):
+            timeout_time = time.time() + timeout
+            error_tuple = tuple(errors)
+            while True:
+                try:
+                    return func(*args, **kwargs)
+                except error_tuple:
+                    if time.time() >= timeout_time:
+                        raise
+                    time.sleep(interval)
+
+        return call
+
+    return decorate
