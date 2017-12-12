f0ea744da99c3958a096fc96fe955c2b615b3951
Matt Weiden <538456+mweiden@users.noreply.github.com>
2017-12-12 12:39:24 -0800

    Create IndexHandler class. (#743)
    This improves modularity of the indexing code and separates the
    unpacking of the AWS s3 events from GCP CloudStorage events.

--- daemons/dss-index/app.py
+++ daemons/dss-index/app.py
@@ -9,7 +9,7 @@ pkg_root = os.path.abspath(os.path.join(os.path.dirname(__file__), 'domovoilib')
 sys.path.insert(0, pkg_root)  # noqa
 
 import dss
-from dss.events.handlers.index import process_new_s3_indexable_object, process_new_gs_indexable_object
+from dss.events.handlers.index import AWSIndexHandler, GCPIndexHandler
 
 app = domovoi.Domovoi()
 
@@ -17,13 +17,15 @@ dss.Config.set_config(dss.BucketConfig.NORMAL)
 
 s3_bucket = dss.Config.get_s3_bucket()
 
+
 @app.s3_event_handler(bucket=s3_bucket, events=["s3:ObjectCreated:*"])
 def dispatch_s3_indexer_event(event, context) -> None:
     app.log.setLevel(logging.DEBUG)
     if event.get("Event") == "s3:TestEvent":
         app.log.info("DSS index daemon received S3 test event")
     else:
-        process_new_s3_indexable_object(event, app.log)
+        AWSIndexHandler.process_new_indexable_object(event, app.log)
+
 
 @app.sns_topic_subscriber("dss-gs-bucket-events-" + os.environ["DSS_GS_BUCKET"])
 def dispatch_gs_indexer_event(event, context):
@@ -31,4 +33,4 @@ def dispatch_gs_indexer_event(event, context):
     This handler receives GS events via the Google Cloud Function deployed from daemons/dss-gs-event-relay.
     """
     gs_event = json.loads(event['Records'][0]['Sns']['Message'])
-    process_new_gs_indexable_object(gs_event['data'], app.log)
+    GCPIndexHandler.process_new_indexable_object(gs_event['data'], app.log)
