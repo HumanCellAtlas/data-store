d6cf67845fdff4d9d3ce1f9aea775be3ad8a31c0
Tony Tung <tonytung@merly.org>
2017-12-13 02:37:09 -0800

    S3 parallel copy + metadata write state machine app (#710)
    Dispatch logic to the s3 parallel copy

---
+++ daemons/dss-s3-copy-write-metadata-sfn/app.py
@@ -0,0 +1,17 @@
+import os
+import sys
+
+import domovoi
+
+pkg_root = os.path.abspath(os.path.join(os.path.dirname(__file__), 'domovoilib'))  # noqa
+sys.path.insert(0, pkg_root)  # noqa
+
+import dss.stepfunctions.s3copyclient as s3copyclient
+import dss.stepfunctions.generator as generator
+
+app = domovoi.Domovoi()
+
+
+annotation_processor = generator.StateMachineAnnotationProcessor()
+sfn = annotation_processor.process_annotations(s3copyclient.copy_write_metadata_sfn)
+app.register_state_machine(sfn)
