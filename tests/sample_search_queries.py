cb42a50fc6b7a4fe6a8afc4fb6c8e62da04bac33
Trent Smith <1429913+Bento007@users.noreply.github.com>
2017-12-18 11:54:02 -0800

    added v4 data for testing (#785)

--- tests/sample_search_queries.py
+++ tests/sample_search_queries.py
@@ -70,3 +70,61 @@ smartseq2_paired_ends_v2_or_v3_query = \
             }
         }
     }
+
+smartseq2_paired_ends_v3_or_v4_query = \
+    {
+        'query': {
+            'bool': {
+                'must': [
+                    {
+                        'bool': {
+                            'should': [
+                                {'match': {'files.sample_json.ncbi_biosample': "SAMN04303778"}},
+                                {'match': {'files.sample_json.samples.content.sample_id': "SAMN04303778"}}],
+                            'minimum_should_match': 1
+                        }
+                    },
+                    {
+                        'bool': {
+                            'should': [
+                                {'match': {'files.assay_json.content.single_cell.cell_handling': "Fluidigm C1"}},
+                                {'match': {'files.assay_json.single_cell.cell_handling': "Fluidigm C1"}}
+                            ],
+                            'minimum_should_match': 1
+                        }
+                    },
+                    {
+                        'bool': {
+                            'should': [
+                                {'match': {'files.sample_json.donor.species.text': "Homo sapiens"}},
+                                {'match': {'files.sample_json.samples.content.genus_species.text': "Homo sapiens"}}
+                            ],
+                            'minimum_should_match': 1
+                        }
+                    }
+                ]
+            }
+        }
+    }
+
+
+smartseq2_paired_ends_v4_query = \
+    {
+        'query': {
+            'bool': {
+                'must': [{
+                    'match': {
+                        "files.sample_json.samples.content.genus_species.text": "Homo sapiens"
+                    }
+                }, {
+                    'match': {
+                        "files.assay_json.content.single_cell.cell_handling": "Fluidigm C1"
+                    }
+                }, {
+                    'match': {
+                        "files.sample_json.samples.content.sample_id": "SAMN04303778"
+                    }
+                }]
+            }
+        }
+    }
