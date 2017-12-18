5828a0e2a35384b8cc0fd2e1593963b9b5dd56a1
Trent Smith <tsmith12@ucsc.edu>
2017-12-18 08:34:03 -0800

    added setUpClass and tearDownClass to test_smoketest.py

--- tests/test_smoketest.py
+++ tests/test_smoketest.py
@@ -44,78 +44,78 @@ def run(command, runner=check_call, **kwargs):
 
 @infra.testmode.standalone
 class Smoketest(unittest.TestCase):
-    def test_smoketest(self):
+    def setUpClass(cls):
         if os.path.exists("dcp-cli"):
             run("git pull --recurse-submodules", cwd="dcp-cli")
         else:
             run("git clone --depth 1 --recurse-submodules https://github.com/HumanCellAtlas/dcp-cli")
+        cls.workdir = tempfile.TemporaryDirectory(dir=os.getcwd(), prefix="smoketest-", suffix='.tmp')
 
-        workdir = tempfile.TemporaryDirectory(dir=os.getcwd(), prefix="smoketest-", suffix='.tmp')
-        try:
-            venv = os.path.join(workdir.name, 'venv')
-            run(f"virtualenv -p {sys.executable} {venv}")
-            venv_bin = os.path.join(venv, 'bin', '')
-            run(f"{venv_bin}pip install --upgrade .", cwd="dcp-cli")
+        venv = os.path.join(cls.workdir.name, 'venv')
+        run(f"virtualenv -p {sys.executable} {venv}")
+        cls.venv_bin = os.path.join(venv, 'bin', '')
+        run(f"{cls.venv_bin}pip install --upgrade .", cwd="dcp-cli")
 
-            bundle_dir = os.path.join(workdir.name, "bundle")
-            shutil.copytree("data-bundle-examples/10X_v2/pbmc8k", bundle_dir)
-            sample_id = str(uuid.uuid4())
-            with open(os.path.join(bundle_dir, "async_copied_file"), "wb") as fh:
-                fh.write(os.urandom(ASYNC_COPY_THRESHOLD + 1))
+    def test_smoketest(self):
+        bundle_dir = os.path.join(self.workdir.name, "bundle")
+        shutil.copytree("data-bundle-examples/10X_v2/pbmc8k", bundle_dir)
+        sample_id = str(uuid.uuid4())
+        with open(os.path.join(bundle_dir, "async_copied_file"), "wb") as fh:
+            fh.write(os.urandom(ASYNC_COPY_THRESHOLD + 1))
 
-            os.chdir(workdir.name)
+        os.chdir(self.workdir.name)
 
-            cli_config = {"DSSClient": {"swagger_url": os.environ["SWAGGER_URL"]}}
-            cli_config_filename = f"{workdir.name}/cli_config.json"
-            with open(cli_config_filename, "w") as fh2:
-                fh2.write(json.dumps(cli_config))
-            os.environ["HCA_CONFIG_FILE"] = f"{workdir.name}/cli_config.json"
+        cli_config = {"DSSClient": {"swagger_url": os.environ["SWAGGER_URL"]}}
+        cli_config_filename = f"{self.workdir.name}/cli_config.json"
+        with open(cli_config_filename, "w") as fh2:
+            fh2.write(json.dumps(cli_config))
+        os.environ["HCA_CONFIG_FILE"] = f"{self.workdir.name}/cli_config.json"
 
-            run(f"cat {bundle_dir}/sample.json | jq .uuid=env.sample_id | sponge {bundle_dir}/sample.json",
-                env=dict(os.environ, sample_id=sample_id))
-            run(f"{venv_bin}hca dss upload "
-                "--replica aws "
-                "--staging-bucket $DSS_S3_BUCKET_TEST "
-                f"--src-dir {bundle_dir} > upload.json")
-            run(f"{venv_bin}hca dss download --replica aws --bundle-uuid $(jq -r .bundle_uuid upload.json)")
-            for i in range(10):
-                try:
-                    run("http -v --check-status https://${API_HOST}/v1/bundles/$(jq -r .bundle_uuid upload.json)?replica=gcp")
-                    break
-                except SystemExit:
-                    time.sleep(1)
-            else:
-                parser.exit(RED("Failed to replicate bundle from AWS to GCP"))
-            run(f"{venv_bin}hca dss download --replica gcp --bundle-uuid $(jq -r .bundle_uuid upload.json)")
+        run(f"cat {bundle_dir}/sample.json | jq .uuid=env.sample_id | sponge {bundle_dir}/sample.json",
+            env=dict(os.environ, sample_id=sample_id))
+        run(f"{self.venv_bin}hca dss upload "
+            "--replica aws "
+            "--staging-bucket $DSS_S3_BUCKET_TEST "
+            f"--src-dir {bundle_dir} > upload.json")
+        run(f"{self.venv_bin}hca dss download --replica aws --bundle-uuid $(jq -r .bundle_uuid upload.json)")
+        for i in range(10):
+            try:
+                run("http -v --check-status https://${API_HOST}/v1/bundles/$(jq -r .bundle_uuid upload.json)?replica=gcp")
+                break
+            except SystemExit:
+                time.sleep(1)
+        else:
+            parser.exit(RED("Failed to replicate bundle from AWS to GCP"))
+        run(f"{self.venv_bin}hca dss download --replica gcp --bundle-uuid $(jq -r .bundle_uuid upload.json)")
 
-            for replica in "aws", "gcp":
-                run(f"{venv_bin}hca dss post-search --es-query='{{}}' --replica {replica} > /dev/null")
+        for replica in "aws", "gcp":
+            run(f"{self.venv_bin}hca dss post-search --es-query='{{}}' --replica {replica} > /dev/null")
 
-            search_route = "https://${API_HOST}/v1/search"
-            for replica in "aws", "gcp":
-                run(f"jq -n '.es_query.query.match[env.k]=env.v' | http --check {search_route} replica==aws > res.json",
-                    env=dict(os.environ, k="files.sample_json.uuid", v=sample_id))
-                with open("res.json") as fh2:
-                    res = json.load(fh2)
-                    print(json.dumps(res, indent=4))
-                    assert len(res["results"]) == 1
+        search_route = "https://${API_HOST}/v1/search"
+        for replica in "aws", "gcp":
+            run(f"jq -n '.es_query.query.match[env.k]=env.v' | http --check {search_route} replica==aws > res.json",
+                env=dict(os.environ, k="files.sample_json.uuid", v=sample_id))
+            with open("res.json") as fh2:
+                res = json.load(fh2)
+                print(json.dumps(res, indent=4))
+                assert len(res["results"]) == 1
 
-                res = run(f"{venv_bin}hca dss put-subscription "
-                          "--callback-url https://example.com/ "
-                          "--es-query '{}' "
-                          f"--replica {replica}",
-                          runner=check_output)
-                sub_id = json.loads(res.decode())["uuid"]
-                run(f"{venv_bin}hca dss get-subscriptions --replica {replica}")
-                run(f"{venv_bin}hca dss delete-subscription --replica {replica} --uuid {sub_id}")
-        finally:
-            if args.clean:
-                workdir.cleanup()
-            else:
-                print(f"Leaving temporary working directory at {workdir}.", file=sys.stderr)
-                # Disable workdir destructor
-                workdir._finalizer.detach()  # type: ignore
+            res = run(f"{self.venv_bin}hca dss put-subscription "
+                      "--callback-url https://example.com/ "
+                      "--es-query '{}' "
+                      f"--replica {replica}",
+                      runner=check_output)
+            sub_id = json.loads(res.decode())["uuid"]
+            run(f"{self.venv_bin}hca dss get-subscriptions --replica {replica}")
+            run(f"{self.venv_bin}hca dss delete-subscription --replica {replica} --uuid {sub_id}")
 
+    def tearDownClass(cls):
+        if args.clean:
+            cls.cleanup()
+        else:
+            print(f"Leaving temporary working directory at {cls.workdir}.", file=sys.stderr)
+            # Disable workdir destructor
+            cls.workdir._finalizer.detach()  # type: ignore
 
 if __name__ == '__main__':
     args, sys.argv[1:] = parser.parse_known_args()
