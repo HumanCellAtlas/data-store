369d8f2da46f264a2eafd58fbf86e87d4d65a3b2
Hannes Schmidt <hannes@ucsc.edu>
2017-12-27 23:58:17 -0800

    Fix unused imports, line wraps

--- daemons/dss-visitation/app.py
+++ daemons/dss-visitation/app.py
@@ -5,7 +5,6 @@ import domovoi
 pkg_root = os.path.abspath(os.path.join(os.path.dirname(__file__), 'domovoilib'))  # noqa
 sys.path.insert(0, pkg_root)  # noqa
 
-import dss
 from dss import BucketConfig, Config
 from dss.stepfunctions.visitation.implementation import sfn
 
