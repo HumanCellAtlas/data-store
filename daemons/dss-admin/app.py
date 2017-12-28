6b95f55ab74f649a359497204a7eeb5bbdbe7480
Hannes Schmidt <hannes@ucsc.edu>
2017-12-27 23:58:17 -0800

    Generalized admin CLI

--- daemons/dss-admin/app.py
+++ daemons/dss-admin/app.py
@@ -1,12 +1,15 @@
+import argparse
+import inspect
+
 import os
 import sys
 import json
 import domovoi
+from typing import Callable, Any, Mapping
 
 pkg_root = os.path.abspath(os.path.join(os.path.dirname(__file__), 'domovoilib'))  # noqa
 sys.path.insert(0, pkg_root)  # noqa
 
-import dss
 from dss import BucketConfig, Config, Replica
 from dss.stepfunctions.visitation.reindex import Reindex
 
@@ -14,25 +17,55 @@ from dss.stepfunctions.visitation.reindex import Reindex
 Config.set_config(BucketConfig.NORMAL)
 
 
-def kickoff_reindex(event, context):
-    replica = Replica[event['replica']]
-    bucket = event['bucket']
-    number_of_workers = event['number_of_workers']
+class Target:
+    """
+    The target of an admin operation. After adding a subclass here, a corresponding subcommand (aka target) should be
+    added to the admin CLI (admin-cli.py). Every method in the target subclass should correspond to an subsubcommand
+    (aka action) of the target subcommand.
+    """
+    pass
 
-    assert 1 < number_of_workers
-    # handle = Config.get_cloud_specific_handles(replica)[0]
-    # assert handle.check_bucket_exists(bucket)
 
-    name = Reindex.start(replica.name, bucket, number_of_workers)
+class IndexTarget(Target):
+    """
+    Admin operations on the Elasticsearch index.
+    """
+    def __init__(self, replica: str, bucket: str = None) -> None:
+        super().__init__()
+        self.replica = Replica[replica]
+        self.bucket = bucket or self.replica.bucket
 
-    return json.dumps({
-        'name': name
-    })
+    def repair(self, workers: int) -> Mapping[str, Any]:
+        raise NotImplementedError()
+
+    def verify(self, workers: int) -> Mapping[str, Any]:
+        return self._reindex(workers)
+
+    def _reindex(self, workers: int) -> Mapping[str, Any]:
+        assert 1 < workers
+        visitation_id = Reindex.start(self.replica.name, self.bucket, workers)
+        return {'visitation_id': visitation_id}
+
+
+def _invoke(f: Callable, kwargs: Mapping[str, Any]) -> Any:
+    """
+    Invoke the given function with a given dictionary of keyword arguments, silently ignoring any extra arguments.
+    """
+    signature = inspect.signature(f)
+    kwargs = {name: kwargs.get(name, param.default) for name, param in signature.parameters.items()}
+    return f(**kwargs)
 
 
 class DSSAdmin(domovoi.Domovoi):
-    def __call__(self, event, context):
-        return kickoff_reindex(event, context)
+    def __call__(self, options, context):
+        target_class_name = options['target'].capitalize() + Target.__name__
+        target_class = globals()[target_class_name]
+        assert issubclass(target_class, Target)
+        target = _invoke(target_class, options)
+        action_name = options['action']
+        action = getattr(target, action_name)
+        result = _invoke(action, options)
+        return json.dumps(result)
 
 
 app = DSSAdmin()
