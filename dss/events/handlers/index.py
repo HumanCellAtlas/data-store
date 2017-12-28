ee47e218a134122ffb40c76d8b9c02ca43bbebcc
Hannes Schmidt <hannes@ucsc.edu>
2017-12-27 23:58:17 -0800

    Consolidate indexing functionality in bundle and tombstone classes
    Only moved code and performed minimal changes required to make it work.

--- dss/events/handlers/index.py
+++ dss/events/handlers/index.py
@@ -27,40 +27,14 @@ class IndexHandler:
     def _index_and_notify(replica: Replica, bundle_fqid: BundleFQID, logger):
         logger.info(f"Indexing bundle {bundle_fqid} from replica '{replica.name}'.")
         doc = BundleDocument.from_replica(replica, bundle_fqid, logger)
-        index_name = doc.prepare_index()
-        versions = doc.get_indexed_versions()
-        old_version = versions.pop(index_name, None)
-        if versions:
-            logger.warning(f"Removing stale copies of the bundle document for {bundle_fqid} from the following "
-                           f"index(es): {json.dumps(versions)}.")
-            doc.remove_versions(versions)
-        if old_version:
-            old_doc = doc.from_index(replica, bundle_fqid, index_name, logger, version=old_version)
-            if doc == old_doc:
-                logger.info(f"Document for bundle {bundle_fqid} is already up-to-date in index {index_name} at "
-                            f"version {old_version}.")
-            else:
-                logger.warning(f"Updating an older copy of the document for bundle {bundle_fqid} in index "
-                               f"{index_name} at version {old_version}.")
-                doc.add_to_index(index_name)
-        else:
-            logger.info(f"Writing the document for bundle {bundle_fqid} in index "
-                        f"{index_name} for the first time.")
-            doc.add_to_index(index_name)
-        doc.notify_matching_subscribers(index_name)
+        doc.index_and_notify()
         logger.debug(f"Finished indexing bundle {bundle_fqid} from replica '{replica.name}'.")
 
     @staticmethod
     def _delete_from_index(replica: Replica, tombstone_id: TombstoneID, logger):
         logger.info(f"Received {replica.name} deletion event with tombstone identifier: {tombstone_id}")
         tombstone_document = BundleTombstoneDocument.from_replica(replica, tombstone_id, logger)
-        dead_documents = tombstone_document.list_dead_bundles()
-        for document in dead_documents:
-            index_name = document.prepare_index()
-            document.clear()
-            document.update(tombstone_document)
-            document.add_to_index(index_name)
-            logger.info(f"Deleted from {replica.name} bundle: {document.fqid}")
+        tombstone_document.index()
 
 
 class AWSIndexHandler(IndexHandler):
