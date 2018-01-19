268eb31f458f25fa6113cc3d3b764cb30831e9a1
Roman Kisin <roman@parleypro.com>
2018-01-18 21:02:02 -0800

    Re-factor Sns-based runner

--- daemons/dss-scalability-test/app.py
+++ daemons/dss-scalability-test/app.py
@@ -1,46 +1,52 @@
+import json
 import random
 import tempfile
 import os
 import uuid
 import datetime
+
+import boto3
 import requests
 import sys
 import logging
+import io
 
-
-from awscli.customizations.s3uploader import S3Uploader
 import domovoi
 
 pkg_root = os.path.abspath(os.path.join(os.path.dirname(__file__), 'domovoilib'))  # noqa
 sys.path.insert(0, pkg_root)  # noqa
 
 from dss.util.aws import AWS_MIN_CHUNK_SIZE
+from dss import Replica, Config
 
 app = domovoi.Domovoi()
 app.log.setLevel(logging.DEBUG)
 
 file_keys = []
-# TODO (rkisin): make bucket name configurable
-test_bucket = "org-humancellatlas-dss-test"
-replica = "aws"
+test_bucket = os.environ["DSS_S3_CHECKOUT_BUCKET"]
+replica = Replica.aws
+s3_client = boto3.client('s3')
 
 @app.sns_topic_subscriber("dss-scalability-init-" + os.environ["DSS_DEPLOYMENT_STAGE"])
-def init(event, context):
+def init_test(event, context):
     app.log.info("DSS scalability test  daemon received init event.")
 
-    tempdir = tempfile.gettempdir()
-    test_files = event["test_file_keys"]
-    test_large_files = event["test_large_file_keys"]
+    print(f"event: {str(event)}")
+    msg = json.loads(event["Records"][0]["Sns"]["Message"])
 
-    create_test_files(AWS_MIN_CHUNK_SIZE + 1, test_large_files, tempdir)
-    create_test_files(1024, test_files, tempdir)
+    test_files = msg["test_file_keys"]
+    test_large_files = msg["test_large_file_keys"]
+
+    create_test_files(AWS_MIN_CHUNK_SIZE + 1, test_large_files)
+    create_test_files(1024, test_files)
 
 @app.sns_topic_subscriber("dss-scalability-put-file-" + os.environ["DSS_DEPLOYMENT_STAGE"])
 def put_file(event, context):
-    app.log.info("DSS scalability test  daemon received put file event.")
+    #app.log.info("DSS scalability test  daemon received put file event.")
+    msg = json.loads(event["Records"][0]["Sns"]["Message"])
 
-    test_files = event["test_file_keys"]
-    test_large_files = event["test_large_file_keys"]
+    test_files = msg["test_file_keys"]
+    test_large_files = msg["test_large_file_keys"]
 
     scheme = "s3"
 
@@ -51,7 +57,7 @@ def put_file(event, context):
     headers = {'content-type': 'application/json'}
 
     rand_file_key = random.choice(test_files + test_large_files)
-    app.log.info(f"File put file key: {rand_file_key}")
+    #app.log.info(f"File put file key: {rand_file_key}")
 
     request_body = {"bundle_uuid": bundle_uuid,
                     "creator_uid": 0,
@@ -64,12 +70,19 @@ def put_file(event, context):
         json=request_body,
     ).json()
 
-def create_test_files(self, size: int, file_keys, tempdir):
-    app.log.info(f"Creating {count} test files size {size}")
+def create_test_files(size: int, file_keys):
+    app.log.info(f"Creating {len(file_keys)} test files size {size} in {test_bucket}")
     for key in file_keys:
         src_data = os.urandom(size + 1)
-        with tempfile.NamedTemporaryFile(delete=True) as fh:
+        with tempfile.NamedTemporaryFile(delete=False) as fh:
             fh.write(src_data)
             fh.flush()
-            S3Uploader(tempdir, self.test_bucket).checksum_and_upload_file(fh.name, key, "text/plain")
-    app.log.info("Uploaded test files")
+            upload(fh.name, test_bucket, key)
+        app.log.info(f"Uploaded test file: s3://{test_bucket}/{key}")
+
+def upload(local_path: str, bucket: str, key: str):
+    app.log.info("%s", f"Uploading {local_path} to s3://{bucket}/{key}")
+    try:
+        s3_client.upload_file(local_path, bucket, key)
+    except Exception as e:
+        app.log.error(f"Unable to upload file: {str(e)}")
