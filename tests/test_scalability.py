d7d21c0a677df1040af436379cb7dd11bfceb69b
Roman Kisin <roman@parleypro.com>
2018-01-18 21:02:02 -0800

    Re-factor Sns-based runner

--- tests/test_scalability.py
+++ tests/test_scalability.py
@@ -4,30 +4,37 @@ import uuid
 
 from locust import Locust, TaskSet, task
 
+
 sys.path.append(os.getcwd())
 from tests.scalability.SnsClient import SnsLocust
+from tests.scalability.TaskSetStart import TaskSetStart
 
 FILE_COUNT = 2
 LARGE_FILE_COUNT = 2
 test_file_keys = []
 test_large_file_keys = []
 
+taskSetInit = TaskSetStart()
+
 class MyTaskSet(TaskSet):
     def on_start(self):
-        for _ in range(FILE_COUNT):
-            test_file_keys.append(f"dss-scalability-test/{uuid.uuid4()}")
+        if not taskSetInit.isStarted():
+            print(">>>>>>>>>>>> Started")
+            for _ in range(FILE_COUNT):
+                test_file_keys.append(f"dss-scalability-test/{uuid.uuid4()}")
 
-        for _ in range(LARGE_FILE_COUNT):
-            test_large_file_keys.append(f"dss-scalability-test/large/{uuid.uuid4()}")
+            for _ in range(LARGE_FILE_COUNT):
+                test_large_file_keys.append(f"dss-scalability-test/large/{uuid.uuid4()}")
 
-        sns_msg = dict(
-            test_file_keys=test_file_keys,
-            test_large_file_keys=test_large_file_keys
-        )
-        sns_topic = "dss-scalability-init"
-        self.client.invoke(sns_topic, sns_msg)
+            sns_msg = dict(
+                test_file_keys=test_file_keys,
+                test_large_file_keys=test_large_file_keys
+            )
 
-    @task(3)
+            sns_topic = "dss-scalability-init"
+            self.client.invoke(sns_topic, sns_msg)
+            taskSetInit.start()
+
     def put_file(self):
         sns_msg = dict(
             test_file_keys=test_file_keys,
@@ -50,3 +57,4 @@ class MyLocust(SnsLocust):
     task_set = MyTaskSet
     min_wait = 500
     max_wait = 3000
+
