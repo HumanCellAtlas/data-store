cdd3f2857cf869ab3f7bccfe7748883658f64ef2
Trent Smith <1429913+Bento007@users.noreply.github.com>
2018-01-19 19:39:19 -0800

    S3UrlCache will store the contents of a URL in S3 and retrieve it upon request (#802)
    This reduces the need to rely on external resource for frequently used URL content.

--- dss/storage/validator.py
+++ dss/storage/validator.py
@@ -1,23 +1,121 @@
 """Utilities in this file are used to removed extra fields from index data before adding to the index."""
-import re
+import io
+from io import BytesIO
+
+import boto3
+from cloud_blobstore import BlobNotFoundError
 from jsonschema import validators
 from jsonschema import _utils, _validators
 
 import json
-from urllib.request import urlopen
-from urllib.error import URLError
-import ssl
 from typing import List
 import logging
 
-
-def read_json_url(link):
-    gcontext = ssl.SSLContext(ssl.PROTOCOL_TLSv1)
-    with urlopen(link, context=gcontext) as f:
-        return json.load(f)
+from dss import Replica, Config
+from hashlib import sha1
+import requests
 
 
-def remove_json_fields(json_data: dict, path: List[str], fields: List[str]):
+DSS_Draft4Validator = validators.create(meta_schema=_utils.load_schema("draft4"),
+                                        validators={'$ref': _validators.ref,
+                                                    'additionalProperties': _validators.additionalProperties,
+                                                    'properties': _validators.properties_draft4,
+                                                    'required': _validators.required_draft4
+                                                    },
+                                        version="draft4"
+                                        )
+
+
+class SizeLimitError(IOError):
+    def __init__(self, url, limit) -> None:
+        super().__init__(f"{url} not cached. The URL's contents have exceeded {limit} bytes.")
+
+
+class S3UrlCache:
+    """Caches content of arbitrary URLs the first time they are requested. Currently only supports content lengths of up
+     to a few megabytes."""
+    _max_size_default = 64 * 1024 * 1024  # The default max_size per URL = 64 MB
+    _chunk_size_default = 1024 * 1024  # The default chunk_size = 1 MB
+
+    def __init__(self, logger: logging.Logger,
+                 max_size: int = _max_size_default,
+                 chunk_size: int = _chunk_size_default):
+        """
+        :param max_size: The maximum number of bytes of content that can be cached. An error is returned if the content
+        is greater than max_size, and the URL is not cached.
+        :param chunk_size: The amount of content retrieved from the URL per request."""
+        self.max_size = max_size if max_size is not None else self._max_size_default
+        self.chunk_size = chunk_size if chunk_size is not None else self._chunk_size_default
+        self.s3_client = boto3.client('s3')
+
+        # A URL's contents are only stored in S3 to keep the data closer to the aws lambas which use them.
+        self.blobstore, _, self.bucket = Config.get_cloud_specific_handles(Replica.aws)
+        self.logger = logger
+
+    def resolve(self, url: str) -> bytearray:
+        """
+        Requests the contents of a URL by first checking for the contents in an S3 bucket. If not present, the contents
+        are retrieved from the URL and stored in S3.
+
+        :param url: The url to retrieve the content from.
+        """
+        key = self._url_to_key(url)
+
+        # if key in S3 bucket return value from there.
+        try:
+            content = bytearray(self.blobstore.get(self.bucket, key))
+        except BlobNotFoundError:
+            self.logger.info("%s", f"{url} not found in cache. Adding it to {self.bucket} with key {key}.")
+            with requests.get(url, stream=True) as resp_obj:
+                content = bytearray()
+                for chunk in resp_obj.iter_content(chunk_size=self.chunk_size):
+                    #  check if max_size exceeded before storing content to avoid storing large chunks
+                    if len(content) + len(chunk) > self.max_size:
+                        raise SizeLimitError(url, self.max_size)
+                    content.extend(chunk)
+            self._upload_content(key, url, content)
+        return content
+
+    def evict(self, url: str) -> bool:
+        '''
+        Removes the cached URL content from S3.
+        :param url: the url for the content to removed from S3'''
+        if self.contains(url):
+            self.logger.info(f"{url} removed from cache in {self.bucket}.")
+            self.blobstore.delete(self.bucket, self._url_to_key(url))
+        else:
+            self.logger.info(f"{url} not found and not removed from cache.")
+
+    def contains(self, url: str) -> bool:
+        key = self._url_to_key(url)
+        try:
+            self.blobstore.get_user_metadata(self.bucket, key)['dss_cached_url']
+        except BlobNotFoundError:
+            return False
+        else:
+            return True
+
+    def _reverse_key_lookup(self, key: str) -> str:
+        return self.blobstore.get_user_metadata(self.bucket, key)['dss_cached_url']
+
+    @staticmethod
+    def _url_to_key(url: str) -> str:
+        return 'cache/' + sha1(url.encode("utf-8")).hexdigest()
+
+    def _upload_content(self, key: str, url: str, content: bytearray) -> None:
+        meta_data = {
+            "Metadata": {'dss_cached_url': url},
+            "ContentType": "application/octet-stream",
+        }
+        self.s3_client.upload_fileobj(
+            Fileobj=io.BytesIO(content),
+            Bucket=self.bucket,
+            Key=key,
+            ExtraArgs=meta_data,
+        )
+
+
+def remove_json_fields(json_data: dict, path: List[str], fields: List[str]) -> None:
     """
     Removes fields from the path in json_data.
 
@@ -32,21 +130,17 @@ def remove_json_fields(json_data: dict, path: List[str], fields: List[str]):
         current.pop(field)
 
 
-DSS_Draft4Validator = validators.create(
-    meta_schema=_utils.load_schema("draft4"),
-    validators={
-        u"$ref": _validators.ref,
-        u"additionalProperties": _validators.additionalProperties,
-        u"properties": _validators.properties_draft4,
-        u"required": _validators.required_draft4,
-    },
-    version="draft4",
-)
-
-resolver = validators.RefResolver(referrer='', base_uri='')
-
-
 def scrub_index_data(index_data: dict, bundle_id: str, logger: logging.Logger) -> list:
+    cache = S3UrlCache(logger)
+
+    def request_json(url):
+        return json.loads(cache.resolve(url).decode("utf-8"))
+
+    resolver = validators.RefResolver(referrer='',
+                                      base_uri='',
+                                      handlers={'http': request_json,
+                                                'https': request_json}
+                                      )
     extra_fields = []
     extra_documents = []
     for document in index_data.keys():
@@ -54,7 +148,7 @@ def scrub_index_data(index_data: dict, bundle_id: str, logger: logging.Logger) -
         schema_url = None if core is None else core.get('schema_url')
         if schema_url is not None:
             try:
-                schema = read_json_url(schema_url)
+                schema = request_json(schema_url)
             except Exception as ex:
                 extra_documents.append(document)
                 logger.warning("%s", f"Unable to retrieve schema from {document} in {bundle_id} "
---
+++ tests/test_s3urlcache.py
@@ -0,0 +1,202 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+import logging
+import os
+import io
+import shutil
+import sys
+import unittest
+import threading
+import requests
+from http.server import BaseHTTPRequestHandler, HTTPServer
+
+pkg_root = os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))  # noqa
+sys.path.insert(0, pkg_root)  # noqa
+
+from dss import Config, BucketConfig
+from dss.config import Replica
+from tests.infra import start_verbose_logging, testmode
+from dss.storage.validator import S3UrlCache
+from dss.storage.validator import SizeLimitError
+from dss.util import networking
+
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.INFO)
+
+start_verbose_logging()
+
+KiB = 1024
+MB = KiB ** 2
+
+randomdata = 'a random string of data'
+
+class HTTPInfo:
+    address = "127.0.0.1"
+    port = None
+    server = None
+    thread = None
+
+    @classmethod
+    def make_url(cls):
+        cls.url = f"http://{cls.address}:{cls.port}"
+
+
+def setUpModule():
+    HTTPInfo.port = networking.unused_tcp_port()
+    HTTPInfo.server = HTTPServer((HTTPInfo.address, HTTPInfo.port), GetTestHandler)
+    HTTPInfo.make_url()
+    HTTPInfo.thread = threading.Thread(target=HTTPInfo.server.serve_forever)
+    HTTPInfo.thread.start()
+
+    global randomdata
+    randomdata = os.urandom(MB)
+
+
+def tearDownModule():
+    HTTPInfo.server.shutdown()
+
+@testmode.standalone
+class TestS3UrlCache(unittest.TestCase):
+
+    @classmethod
+    def setUpClass(cls):
+        replica = Replica.aws
+        Config.set_config(BucketConfig.TEST_FIXTURE)
+        cls.blobstore, _, cls.test_fixture_bucket = Config.get_cloud_specific_handles(replica)
+        Config.set_config(BucketConfig.TEST)
+        cls.test_bucket = replica.bucket
+
+    def setUp(self):
+        self.urls_to_cleanup = set()
+        self.cache = S3UrlCache(logger)
+
+    def tearDown(self):
+        self._delete_cached_urls()
+
+    def test_store_in_cache(self):
+        """The URL contents are stored in S3 and the contents returned, when requested url is not found in cache."""
+        url = f"{HTTPInfo.url}/{KiB}"
+        self.urls_to_cleanup.add(url)
+        url_key = S3UrlCache._url_to_key(url)
+
+        self._delete_cached_urls()
+        with self.assertLogs(logger, "INFO") as log_monitor:
+            url_content = self.cache.resolve(url)
+
+        original_data = randomdata[:KiB]
+        self.assertEqual(len(url_content), KiB)
+        self.assertEqual(url_content, original_data)
+        self.assertTrue(log_monitor.output[0].endswith(f"{url} not found in cache. Adding it to "
+                                                       f"{self.test_bucket} with key {url_key}."))
+
+    def test_retrieve_from_cache(self):
+        """Stored URL contents is retrieved from S3, when a cached url is requested"""
+        url = f"{HTTPInfo.url}/{KiB}"
+        self.urls_to_cleanup.add(url)
+        with self.assertLogs(logger, 'INFO') as log_monitor:
+            url_content = self.cache.resolve(url)
+            cached_content = self.cache.resolve(url)
+        self.assertEqual(len(log_monitor.output), 1)
+        self.assertEqual(url_content, cached_content)
+
+    def test_bad_url(self):
+        bad_urls = ['', '//', 'http://?']
+        self.urls_to_cleanup.update(bad_urls)
+
+        for url in bad_urls:
+            with self.subTest(bad_url=url):
+                with self.assertRaises(requests.RequestException):
+                    self.cache.resolve(url)
+
+    def test_url_SizeLimitError(self):
+        """Exception returned when URL content size is greater than max_size."""
+        url = f"{HTTPInfo.url}/{KiB}"
+        self.urls_to_cleanup.add(url)
+        self.cache.max_size = 1
+        with self.assertRaises(SizeLimitError) as ex:
+            self.cache.resolve(url)
+        self.assertEqual(ex.exception.args[0], f"{url} not cached. The URL's contents have exceeded "
+                                               f"{self.cache.max_size} bytes.")
+
+    def test_content_size_0(self):
+        url = f"{HTTPInfo.url}/0"
+        self.urls_to_cleanup.add(url)
+        url_content = self.cache.resolve(url)
+        self.assertEqual(len(url_content), 0)
+
+    def test_chunked_content(self):
+        size = KiB * 10
+        url = f"{HTTPInfo.url}/{size}"
+        self.urls_to_cleanup.add(url)
+        self.cache.max_size = 1 * MB
+        self.cache.chunk_size = 1 * KiB
+        url_content = self.cache.resolve(url)
+        original_data = randomdata[:size]
+        self.assertEqual(url_content, original_data)
+
+    def test_stored_url_metadata(self):
+        url = f"{HTTPInfo.url}/{KiB}"
+        self.urls_to_cleanup.add(url)
+        url_key = S3UrlCache._url_to_key(url)
+        self.cache.resolve(url)
+
+        with self.subTest("check dss_cached_url"):
+            cached_url = self.cache._reverse_key_lookup(url_key)
+            self.assertEqual(cached_url, url)
+
+        with self.subTest("check content_type"):
+            contentType = self.blobstore.get_content_type(self.test_bucket, url_key)
+            self.assertEqual(contentType, "application/octet-stream")
+
+    def test_evict(self):
+        url = f"{HTTPInfo.url}/{KiB}"
+        self.urls_to_cleanup.add(url)
+        url_key = S3UrlCache._url_to_key(url)
+
+        with self.assertLogs(logger, "INFO") as log_monitor:
+            # Verify the URL is cached
+            self.cache.resolve(url)
+            self.assertTrue(self.cache.contains(url))
+            # Remove the URL from cache
+            self.cache.evict(url)
+            self.assertTrue(not self.cache.contains(url))
+            self.cache.evict(url)
+            self.assertTrue(not self.cache.contains(url))
+
+        self.assertTrue(log_monitor.output[0].endswith(f"{url} not found in cache. Adding it to "
+                                                       f"{self.test_bucket} with key {url_key}."))
+        self.assertTrue(log_monitor.output[1].endswith(f"{url} removed from cache in {self.test_bucket}."))
+        self.assertTrue(log_monitor.output[2].endswith(f"{url} not found and not removed from cache."))
+
+    def test_contains(self):
+        url = f"{HTTPInfo.url}/{KiB}"
+        self.urls_to_cleanup.add(url)
+
+        self.assertTrue(not self.cache.contains(url))
+        self.cache.resolve(url)
+        self.assertTrue(self.cache.contains(url))
+
+    def _delete_cached_urls(self):
+        for url in self.urls_to_cleanup:
+            self.cache.evict(url)
+
+
+class GetTestHandler(BaseHTTPRequestHandler):
+    _response_code = 200
+
+    def do_GET(self):
+        length = int(self.path.split('/')[1])
+
+        self.send_response(self._response_code)
+        self.send_header("content-length", length)
+        self.end_headers()
+
+        if length:
+            shutil.copyfileobj(io.BytesIO(randomdata[:length]), self.wfile)
+        else:
+            return
+
+if __name__ == "__main__":
+    unittest.main()
