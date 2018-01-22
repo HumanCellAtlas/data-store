6a59fd6d0008b331ba0fb3b8d06f531699ca0fd2
Roman Kisin <romankisin@czipa1osx052.hsd1.ca.comcast.net>
2018-01-21 22:01:14 -0800

    SFN-based test

--- tests/test_scalability.py
+++ tests/test_scalability.py
@@ -1,60 +1,23 @@
 import os
 import sys
-import uuid
 
-from locust import Locust, TaskSet, task
+from locust import TaskSet, task
 
 
 sys.path.append(os.getcwd())
-from tests.scalability.SnsClient import SnsLocust
-from tests.scalability.TaskSetStart import TaskSetStart
 
-FILE_COUNT = 2
-LARGE_FILE_COUNT = 2
-test_file_keys = []
-test_large_file_keys = []
-
-taskSetInit = TaskSetStart()
+from tests.scalability.SfnClient import SfnLocust
 
 class MyTaskSet(TaskSet):
     def on_start(self):
-        if not taskSetInit.isStarted():
-            print(">>>>>>>>>>>> Started")
-            for _ in range(FILE_COUNT):
-                test_file_keys.append(f"dss-scalability-test/{uuid.uuid4()}")
+        self.client.start_execution()
 
-            for _ in range(LARGE_FILE_COUNT):
-                test_large_file_keys.append(f"dss-scalability-test/large/{uuid.uuid4()}")
-
-            sns_msg = dict(
-                test_file_keys=test_file_keys,
-                test_large_file_keys=test_large_file_keys
-            )
-
-            sns_topic = "dss-scalability-init"
-            self.client.invoke(sns_topic, sns_msg)
-            taskSetInit.start()
-
-    def put_file(self):
-        sns_msg = dict(
-            test_file_keys=test_file_keys,
-            test_large_file_keys=test_large_file_keys
-        )
-        sns_topic = "dss-scalability-put-file"
-        self.client.invoke(sns_topic, sns_msg)
-
-    @task(1)
-    def checkout(self):
-        sns_msg = dict(
-            test_file_keys=test_file_keys,
-            test_large_file_keys=test_large_file_keys
-        )
-        sns_topic = "dss-scalability-checkout"
-        #self.invoke_test(sns_topic, sns_msg)
+    @task
+    def check_execution(self):
+        self.client.check_execution()
 
 
-class MyLocust(SnsLocust):
+class MyLocust(SfnLocust):
     task_set = MyTaskSet
     min_wait = 500
     max_wait = 3000
-
