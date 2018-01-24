2371acc4c1524a12f9e2af0eed9b31d7848ec1bb
Tony Tung <tonytung@merly.org>
2018-01-23 16:08:40 -0800

    [easy] Use mutablemapping as return type instead. (#868)

--- dss/stepfunctions/gscopyclient/__init__.py
+++ dss/stepfunctions/gscopyclient/__init__.py
@@ -1,7 +1,12 @@
+import typing
+
 from .implementation import Key, sfn
 
 
-def copy_sfn_event(source_bucket: str, source_key: str, destination_bucket: str, destination_key: str) -> dict:
+def copy_sfn_event(
+        source_bucket: str, source_key: str,
+        destination_bucket: str, destination_key: str
+) -> typing.MutableMapping[str, str]:
     """Returns the initial event object to start the gs-gs copy stepfunction."""
     return {
         Key.SOURCE_BUCKET: source_bucket,
