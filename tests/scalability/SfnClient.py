7a4739c86cb7ce8cb2994ae479df2fba7bcedb9d
Roman Kisin <roman@parleypro.com>
2018-01-24 23:01:58 -0800

    Use DynamoDB to record test results

--- tests/scalability/SfnClient.py
+++ tests/scalability/SfnClient.py
@@ -1,4 +1,6 @@
 import uuid
+import boto3
+
 
 import time
 from locust import events, Locust
@@ -6,44 +8,47 @@ from locust import events, Locust
 
 from dss import stepfunctions
 
-sfn_template = "dss-scalability-test-{stage}"
 type = 'sfn'
-class SfnClient():
-    execution_id = None
-    start_time = None
+sfn_template = "dss-scalability-test-{stage}"
 
-    def start_execution(self):
+dynamodb = boto3.resource('dynamodb')
+table = dynamodb.Table('scalability_test')
+
+class SfnClient():
+    def start_test_execution(self, test_run_id: str):
         self.execution_id = str(uuid.uuid4())
-        test_input = {"test": "test"}
+        test_input = {"execution_id": self.execution_id, "test_run_id": test_run_id}
         stepfunctions.step_functions_invoke(sfn_template, self.execution_id, test_input)
         self.start_time = time.time()
 
     def check_execution(self):
-
+        assert self.execution_id is not None
+        time.sleep(30)
         try:
-            assert self.execution_id is not None
             for i in range(10):
-                response = stepfunctions.step_functions_describe_execution(sfn_template, self.execution_id)
-                print(f"response: {response}")
-                status = response["status"]
-                if status == 'RUNNING':
-                    time.sleep(10)
-                elif status == 'SUCCEEDED':
-                    start_date = response["startDate"]
-                    stop_date = response["stopDate"]
-                    total_time = (stop_date - start_date).total_seconds()
+                time.sleep(10)
+                response = table.get_item(
+                    Key={
+                        'execution_id': self.execution_id
+                    }
+                )
+                item = response.get('Item')
+                print(f"Got stuff from dynamo {self.execution_id}")
+                print(str(item))
+                if item is None:
+                    pass
+                elif item["status"] == 'SUCCEEDED':
+                    total_time = item["duration"]
                     events.request_success.fire(request_type=type, name=sfn_template, response_time=total_time,
                                                 response_length=0)
                     break
                 else:
-                    start_date = response["startDate"]
-                    stop_date = response["stopDate"]
-                    total_time = (stop_date - start_date).total_seconds()
+                    total_time = item["duration"]
                     events.request_failure.fire(request_type=type, name=sfn_template, response_time=total_time,
                                                 exception=None)
                     break
         except Exception as e:
-            print(f"Failed to to start step function execution {sfn_template}: {str(e)}")
+            print(f"Failed to get execution results  {sfn_template}: {str(e)}")
             total_time = int((time.time() - self.start_time) * 1000)
             events.request_failure.fire(request_type=type, name=sfn_template, response_time=total_time, exception=e)
 
