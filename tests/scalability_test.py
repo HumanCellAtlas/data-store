c218f9d12c4898d3a99a70f6a50273b3a720525d
Roman Kisin <roman@parleypro.com>
2018-01-24 23:01:58 -0800

    Use DynamoDB to record test results

--- tests/scalability_test.py
+++ tests/scalability_test.py
@@ -1,5 +1,6 @@
 import os
 import sys
+import uuid
 
 from locust import TaskSet, task
 
@@ -8,12 +9,16 @@ sys.path.append(os.getcwd())
 
 from tests.scalability.SfnClient import SfnLocust
 
+test_run_id = str(uuid.uuid4())
+
 class MyTaskSet(TaskSet):
     def on_start(self):
-        self.client.start_execution()
+        pass
+
 
     @task
     def check_execution(self):
+        self.client.start_test_execution(test_run_id)
         self.client.check_execution()
 
 
