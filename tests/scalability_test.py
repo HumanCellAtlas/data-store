b6c6ad5715431a31fb1dd2ac986ee403faeb74d3
Roman Kisin <roman@parleypro.com>
2018-01-29 17:32:25 -0800

    Eliminate client side status checking

--- tests/scalability_test.py
+++ tests/scalability_test.py
@@ -7,18 +7,20 @@ from locust import TaskSet, task
 
 sys.path.append(os.getcwd())
 
-from tests.scalability.SnsClient import SnsLocust  # noqa
+from tests.scalability.SnsClient import SnsLocust, SnsClient  # noqa
+
+test_run_id = str(uuid.uuid4())
+print('>>>Run once')
+sns_client = SnsClient()
+sns_client.start_test_run(test_run_id)
 
 class MyTaskSet(TaskSet):
     def on_start(self):
-        self.test_run_id = str(uuid.uuid4())
         self.test_execution_id = str(uuid.uuid4())
 
     @task
     def check_execution(self):
-        self.client.start_test_execution(self.test_run_id, self.test_execution_id)
-        time.sleep(30)
-        self.client.check_execution()
+        self.client.start_test_execution(test_run_id, self.test_execution_id)
 
 class MyLocust(SnsLocust):
     task_set = MyTaskSet
