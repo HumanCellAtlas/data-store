e52df28de014167cde283236abe3a2cbf39fb79a
Roman Kisin <roman@parleypro.com>
2018-02-01 19:23:47 -0800

    Script creating DynamoDB tables, domovoi 1.5.1

--- daemons/dss-scalability-test/app.py
+++ daemons/dss-scalability-test/app.py
@@ -1,3 +1,4 @@
+import calendar
 import datetime
 import json
 import logging
@@ -8,7 +9,7 @@ import time
 
 import boto3
 import domovoi
-
+from decimal import Decimal
 
 pkg_root = os.path.abspath(os.path.join(os.path.dirname(__file__), 'domovoilib'))  # noqa
 sys.path.insert(0, pkg_root)  # noqa
@@ -166,13 +167,17 @@ def fallback(event, context):
 def save_results(event, result: str):
     table = dynamodb.Table('scalability_test')
     start_time = event['bundle']['start_time']
+
+    expiration_ttl = int(time.time()) + 14 * 24 * 60 * 60   # 14 days
+
     table.put_item(
         Item={
             'run_id': event["test_run_id"],
             'execution_id': event["execution_id"],
             'duration': current_time() - start_time - WAIT_CHECKOUT * 1000,
             'status': result,
-            'created_on': datetime.datetime.now().isoformat()
+            'created_on': datetime.datetime.now().isoformat(),
+            'expiration_ttl': expiration_ttl
         }
     )
 
@@ -201,3 +206,50 @@ def launch_exec(event, context):
     execution_id = msg["execution_id"]
     test_input = {"execution_id": execution_id, "test_run_id": run_id}
     stepfunctions.step_functions_invoke("dss-scalability-test-{stage}", execution_id, test_input)
+
+@app.dynamodb_stream_handler(table_name="scalability_test", batch_size=50)
+def handle_dynamodb_stream(event, context):
+    success_count = 0
+    failure_count = 0
+    records = 0
+    duration_sum = 0
+    run_id = None
+    for record in event['Records']:
+        if record['eventName'] == 'INSERT':
+            run_id = record['dynamodb']['NewImage']['run_id']['S']
+            status = record['dynamodb']['NewImage']['status']['S']
+            duration = record['dynamodb']['NewImage']['duration']['N']
+            duration_sum += Decimal(duration)
+            records += 1
+            if status == 'SUCCEEDED':
+                success_count += 1
+            else:
+                failure_count += 1
+    if records > 0:
+        table = dynamodb.Table('scalability_test_run')
+        run_entry_pk = {'run_id': run_id}
+        run_entry = table.get_item(Key=run_entry_pk)
+        if run_entry.get('Item'):
+            old_avg_duration = run_entry['Item']['average_duration']
+            old_count = run_entry['Item']['succeeded_count'] + run_entry['Item']['failed_count']
+
+            table.update_item(
+                Key=run_entry_pk,
+                UpdateExpression='SET '
+                                 'succeeded_count = succeeded_count + :success_count, '
+                                 'failed_count = failed_count + :failure_count, '
+                                 'average_duration = :new_average',
+                ExpressionAttributeValues={
+                    ':success_count': success_count,
+                    ':failure_count': failure_count,
+                    ':new_average': old_avg_duration + (duration_sum / records - old_avg_duration) /
+                                                       (old_count + records)
+                }
+            )
+        else:
+            print('No run entries found')
+    else:
+        print('No INSERT records to process')
+
+    return 'Successfully processed {} records.'.format(len(event['Records']))
+
