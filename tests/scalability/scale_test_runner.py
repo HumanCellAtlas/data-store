aa92fbc506d9bac696ffd6203a96107f12f9b903
Roman Kisin <roman@parleypro.com>
2018-02-02 15:02:08 -0800

    Removed Locust, implement simple runner

---
+++ tests/scalability/scale_test_runner.py
@@ -0,0 +1,43 @@
+import datetime
+import os
+import sys
+import time
+import uuid
+
+sys.path.append(os.getcwd())    # noqa
+
+from .sns import SnsClient
+
+UNIT_OF_TIME = 1 * 1000 * 1000  # 1 sec in microseconds
+
+class ScaleTestRunner:
+    def __init__(self, rps: int, duration_seconds: int):
+        self.rps = rps
+        self.duration_seconds = duration_seconds
+        self.run_id = str(uuid.uuid4())
+        self.sns_client = SnsClient()
+        self.loading = '.' * duration_seconds
+
+    def run(self):
+        self.end_time = datetime.datetime.now() + datetime.timedelta(seconds=self.duration_seconds)
+        self.sns_client.start_test_run(self.run_id)
+        for cnt in range(self.duration_seconds):
+            self.generate_load_rps()
+            self.loading = self.loading[:cnt] + '#' + self.loading[cnt + 1:]
+            print('\r%s Sending at %3d percent!' % (self.loading, (cnt + 1) * 100 / self.duration_seconds), end='')
+        self.sns_client.stop()
+
+    def generate_load_rps(self):
+        start = datetime.datetime.now()
+        for cnt in range(self.rps):
+            self.sns_client.start_test_execution(self.run_id, str(uuid.uuid4()))
+        duration = datetime.datetime.now() - start
+        elapsed_time = duration.microseconds / UNIT_OF_TIME
+
+        if elapsed_time < 1.0:
+            time.sleep(1.0 - elapsed_time)
+
+
+if __name__ == "__main__":
+    runner = ScaleTestRunner(1, 20)
+    runner.run()
