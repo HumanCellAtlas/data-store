c213846b6e757d9d96e3c3a7a47dd475b1736d67
Roman Kisin <roman@parleypro.com>
2018-02-06 21:29:51 -0800

    Paralelization and wait function to spport for burst load

--- daemons/dss-scalability-test/app.py
+++ daemons/dss-scalability-test/app.py
@@ -1,4 +1,3 @@
-import calendar
 import datetime
 import json
 import logging
@@ -6,93 +5,25 @@ import os
 import sys
 import tempfile
 import time
+from decimal import Decimal
 
 import boto3
 import domovoi
-from decimal import Decimal
 
 pkg_root = os.path.abspath(os.path.join(os.path.dirname(__file__), 'domovoilib'))  # noqa
 sys.path.insert(0, pkg_root)  # noqa
 
 from dss import stepfunctions
+from dss.stepfunctions import generator
+
 from hca.dss import DSSClient
 from json_generator import generate_sample
 
 AWS_MIN_CHUNK_SIZE = 64 * 1024 * 1024
 WAIT_CHECKOUT = 3
+PARALLELIZATION_FACTOR = 10
 
 app = domovoi.Domovoi()
-
-state_machine_def = {
-    "Comment": "DSS scalability test state machine.",
-    "StartAt": "UploadBundle",
-    "TimeoutSeconds": 600,  # 10 minutes, in seconds.
-    "States": {
-        "UploadBundle": {
-            "Type": "Task",
-            "Resource": None,
-            "InputPath": "$",
-            "ResultPath": "$.bundle",
-            "OutputPath": "$",
-            "Next": "DownloadBundle",
-            "Catch": [{
-                "ErrorEquals": ["States.TaskFailed"],
-                "Next": "fallback"
-            }]
-        },
-        "DownloadBundle": {
-            "Type": "Task",
-            "Resource": None,
-            "InputPath": "$",
-            "OutputPath": "$",
-            "ResultPath": "$.download",
-            "Next": "CheckoutBundle",
-            "Catch": [{
-                "ErrorEquals": ["States.TaskFailed"],
-                "Next": "fallback"
-            }]
-        },
-        "CheckoutBundle": {
-            "Type": "Task",
-            "Resource": None,
-            "InputPath": "$",
-            "ResultPath": "$.checkout",
-            "OutputPath": "$",
-            "Next": "Wait_Checkout",
-            "Catch": [{
-                "ErrorEquals": ["States.TaskFailed"],
-                "Next": "fallback"
-            }]
-        },
-        "Wait_Checkout": {
-            "Type": "Wait",
-            "Seconds": WAIT_CHECKOUT,
-            "Next": "CheckoutDownloadStatus"
-        },
-        "CheckoutDownloadStatus": {
-            "Type": "Task",
-            "Resource": None,
-            "InputPath": "$",
-            "ResultPath": "$.checkout.status",
-            "OutputPath": "$",
-            "Next": "CompleteTest",
-            "Catch": [{
-                "ErrorEquals": ["States.TaskFailed"],
-                "Next": "fallback"
-            }]
-        },
-        "CompleteTest": {
-            "Type": "Task",
-            "Resource": None,
-            "End": True,
-        },
-        "fallback": {
-            "Type": "Task",
-            "Resource": None,
-            "End": True,
-        }
-    }
-}
 
 app.log.setLevel(logging.DEBUG)
 
@@ -113,8 +44,7 @@ def current_time():
     return int(round(time.time() * 1000))
 
 
-@app.step_function_task(state_name="UploadBundle", state_machine_definition=state_machine_def)
-def upload_bundle(event, context):
+def upload_bundle(event, context, branch_id):
     app.log.info("Upload bundle")
     with tempfile.TemporaryDirectory() as src_dir:
         # TODO: insert json generator here
@@ -129,8 +59,7 @@ def upload_bundle(event, context):
             return {"bundle_id": bundle_output['bundle_uuid'], "start_time": start_time}
 
 
-@app.step_function_task(state_name="DownloadBundle", state_machine_definition=state_machine_def)
-def download_bundle(event, context):
+def download_bundle(event, context, branch_id):
     app.log.info("Download bundle")
     bundle_id = event['bundle']['bundle_id']
     with tempfile.TemporaryDirectory() as dest_dir:
@@ -138,42 +67,38 @@ def download_bundle(event, context):
     return {}
 
 
-@app.step_function_task(state_name="CheckoutBundle", state_machine_definition=state_machine_def)
-def checkout_bundle(event, context):
+def checkout_bundle(event, context, branch_id):
     bundle_id = event['bundle']['bundle_id']
     app.log.info(f"Checkout bundle: {bundle_id}")
     checkout_output = client.post_bundles_checkout(uuid=bundle_id, replica='aws', email='rkisin@chanzuckerberg.com')
     return {"job_id": checkout_output['checkout_job_id']}
 
 
-@app.step_function_task(state_name="CheckoutDownloadStatus", state_machine_definition=state_machine_def)
-def checkout_status(event, context):
+def checkout_status(event, context, branch_id):
     job_id = event['checkout']['job_id']
     app.log.info(f"Checkout status job_id: {job_id}")
     # checkout_output = client.get_bundles_checkout(job_id)
     # return checkout_output['status']
 
 
-@app.step_function_task(state_name="CompleteTest", state_machine_definition=state_machine_def)
 def complete_test(event, context):
     save_results(event, 'SUCCEEDED')
 
 
-@app.step_function_task(state_name="fallback", state_machine_definition=state_machine_def)
-def fallback(event, context):
-    save_results(event, 'FAILED')
+def fallback(event, context, branch_id):
+    return {"failed", branch_id}
 
 
 def save_results(event, result: str):
     table = dynamodb.Table('scalability_test')
-    start_time = event['bundle']['start_time']
+    start_time = event[0]['bundle']['start_time']
 
-    expiration_ttl = int(time.time()) + 14 * 24 * 60 * 60   # 14 days
+    expiration_ttl = int(time.time()) + 14 * 24 * 60 * 60  # 14 days
 
     table.put_item(
         Item={
-            'run_id': event["test_run_id"],
-            'execution_id': event["execution_id"],
+            'run_id': event[0]["test_run_id"],
+            'execution_id': event[0]["execution_id"],
             'duration': current_time() - start_time - WAIT_CHECKOUT * 1000,
             'status': result,
             'created_on': datetime.datetime.now().isoformat(),
@@ -199,13 +124,20 @@ def launch_test_run(event, context):
         }
     )
 
+
 @app.sns_topic_subscriber("dss-scalability-test-" + os.environ["DSS_DEPLOYMENT_STAGE"])
 def launch_exec(event, context):
     msg = json.loads(event["Records"][0]["Sns"]["Message"])
     run_id = msg["run_id"]
     execution_id = msg["execution_id"]
-    test_input = {"execution_id": execution_id, "test_run_id": run_id}
+    nextBatch = roundTime()
+    test_input = {
+        "execution_id": execution_id,
+        "test_run_id": run_id,
+        "batch": nextBatch.isoformat() + 'Z'
+    }
     stepfunctions.step_functions_invoke("dss-scalability-test-{stage}", execution_id, test_input)
+
 
 @app.dynamodb_stream_handler(table_name="scalability_test", batch_size=50)
 def handle_dynamodb_stream(event, context):
@@ -252,3 +184,106 @@ def handle_dynamodb_stream(event, context):
         print('No INSERT records to process')
 
     return 'Successfully processed {} records.'.format(len(event['Records']))
+
+
+def roundTime(to=datetime.timedelta(minutes=5)):
+    roundTo = to.total_seconds()
+    now = datetime.datetime.now()
+    seconds = (now - now.min).seconds
+    rounding = (seconds + roundTo / 2) // roundTo * roundTo
+    return now + datetime.timedelta(0, rounding - seconds, -now.microsecond)
+
+
+exec_branch_def = {
+    "StartAt": "UploadBundle{t}",
+    "States": {
+
+        "UploadBundle{t}": {
+            "Type": "Task",
+            "Resource": upload_bundle,
+            "InputPath": "$",
+            "ResultPath": "$.bundle",
+            "OutputPath": "$",
+            "Next": "DownloadBundle{t}",
+            "Catch": [{
+                "ErrorEquals": ["States.TaskFailed"],
+                "Next": "fallback{t}"
+            }]
+        },
+        "DownloadBundle{t}": {
+            "Type": "Task",
+            "Resource": download_bundle,
+            "InputPath": "$",
+            "OutputPath": "$",
+            "ResultPath": "$.download",
+            "Next": "CheckoutBundle{t}",
+            "Catch": [{
+                "ErrorEquals": ["States.TaskFailed"],
+                "Next": "fallback{t}"
+            }]
+        },
+        "CheckoutBundle{t}": {
+            "Type": "Task",
+            "Resource": checkout_bundle,
+            "InputPath": "$",
+            "ResultPath": "$.checkout",
+            "OutputPath": "$",
+            "Next": "Wait_Checkout{t}",
+            "Catch": [{
+                "ErrorEquals": ["States.TaskFailed"],
+                "Next": "fallback{t}"
+            }]
+        },
+        "Wait_Checkout{t}": {
+            "Type": "Wait",
+            "Seconds": WAIT_CHECKOUT,
+            "Next": "CheckoutDownloadStatus{t}"
+        },
+        "CheckoutDownloadStatus{t}": {
+            "Type": "Task",
+            "Resource": checkout_status,
+            "InputPath": "$",
+            "ResultPath": "$.checkout.status",
+            "OutputPath": "$",
+            "End": True,
+            "Catch": [{
+                "ErrorEquals": ["States.TaskFailed"],
+                "Next": "fallback{t}"
+            }]
+        },
+        "fallback{t}": {
+            "Type": "Task",
+            "Resource": fallback,
+            "End": True,
+        }
+    }
+}
+
+state_machine_def = {
+    "Comment": "DSS scalability test state machine.",
+    "StartAt": "WaitUntil",
+    "TimeoutSeconds": 3600,
+    "States": {
+        "WaitUntil": {
+            "Type": "Wait",
+            "TimestampPath": "$.batch",
+            "Next": "Executors"
+        },
+
+        "Executors": {
+            "Type": "Parallel",
+            "Branches": generator.ThreadPoolAnnotation(exec_branch_def, PARALLELIZATION_FACTOR, "{t}"),
+            "Next": "CompleteTest",
+        },
+
+        "CompleteTest": {
+            "Type": "Task",
+            "Resource": complete_test,
+            "End": True,
+        }
+    }
+}
+
+annotation_processor = generator.StateMachineAnnotationProcessor()
+sfn = annotation_processor.process_annotations(state_machine_def)
+app.register_state_machine(sfn)
