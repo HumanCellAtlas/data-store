HumanCellAtlas/data-store

commit 16742eae967356645d7b451b011d50d352a51865

Serialize tests that use local ES instance; increase ES heap

Makefile
@@ -7,36 +7,44 @@ lint:
 mypy:
 	mypy --ignore-missing-imports $(MODULES)
 
-test_srcs := $(wildcard tests/test_*.py)
-all_test_srcs := $(addprefix all__, $(test_srcs))
-integration_test_srcs := $(addprefix integration__, $(test_srcs))
-
-test: lint mypy $(test_srcs)
+tests:=$(wildcard tests/test_*.py)
+serial_tests:=tests/test_search.py \
+	          tests/test_indexer.py \
+			  tests/test_subscriptions.py
+parallel_tests:=$(filter-out $(serial_tests),$(tests))
+	
+# Run all standalone tests in parallel
+#
+test: $(tests)
 	coverage combine
 	rm -f .coverage.*
 
-$(test_srcs): %.py :
-	set -o pipefail \
-	&& DSS_TEST_MODE="standalone" coverage run -p --source=dss -m unittest $(DSS_UNITTEST_OPTS) $@  2>&1 \
-	| sed -e "s/^/$$$$ /"
-
-integration_test: lint mypy $(integration_test_srcs)
+# Serialize the standalone tests that start a local Elasticsearch instance in
+# order to prevent more than one such instance at a time.
+#
+safe_test: _serial_test $(parallel_tests)
 	coverage combine
 	rm -f .coverage.*
 
-$(integration_test_srcs): integration__%.py :
-	set -o pipefail \
-	&& DSS_TEST_MODE="integration" coverage run -p --source=dss -m unittest $(DSS_UNITTEST_OPTS) $*.py 2>&1 \
-	| sed -e "s/^/$$$$ /"
+_serial_test:
+	$(MAKE) -j1 $(serial_tests)
 
-all_test: lint mypy $(all_test_srcs)
-	coverage combine
-	rm -f .coverage.*
+# A pattern rule that runs a single test script
+#	
+$(tests): %.py :
+	export DSS_TEST_MODE=$${DSS_TEST_MODE:-standalone} \
+	&& set -o pipefail \
+	&& coverage run -p --source=dss -m unittest $(DSS_UNITTEST_OPTS) $*.py 2>&1 | sed -e "s/^/$$$$ /"
 
-$(all_test_srcs): all__%.py :
-	set -o pipefail \
-	&& DSS_TEST_MODE="integration standalone" coverage run -p --source=dss -m unittest $(DSS_UNITTEST_OPTS) $*.py 2>&1 \
-	| sed -e "s/^/$$$$ /"
+# Run standalone and integration tests
+#	
+all_test:
+	DSS_TEST_MODE="standalone integration" $(MAKE) test
+
+# Run integration tests only
+#
+integration_test:
+	DSS_TEST_MODE="integration" $(MAKE) test
 
 smoketest: all__tests/test_smoketest.py
 
@@ -84,6 +92,5 @@ requirements.txt requirements-dev.txt : %.txt : %.txt.in
 
 requirements-dev.txt : requirements.txt.in
 
-.PHONY: lint mypy
-.PHONY: test all_test integration_test $(test_srcs) $(integration_test_srcs) $(standalone_test_srcs)
+.PHONY: lint mypy test safe_test _serial_test all_test integration_test $(tests)
 .PHONY: deploy deploy-chalice deploy-daemons

.travis.yml
@@ -29,10 +29,12 @@ install:
 - mkdir make4
 - dpkg -x make*.deb make4
 - export PATH=$(pwd)/make4/usr/bin:$PATH
+before_script:
+- export -n _JAVA_OPTIONS # https://github.com/travis-ci/travis-ci/issues/8408
 script:
 - export DSS_UNITTEST_OPTS="$DSS_UNITTEST_OPTS -v"
 - set -eo pipefail
-- if [[ ! $TRAVIS_DSS_INTEGRATION_MODE ]]; then make -j4 test; fi
+- if [[ ! $TRAVIS_DSS_INTEGRATION_MODE ]]; then make -j4 safe_test; fi
 - if [[ $TRAVIS_DSS_INTEGRATION_MODE ]]; then make -j4 integration_test; fi
 - if [[ $TRAVIS_BRANCH == staging ]]; then source environment.staging; fi
 after_success:

dss/util/es.py
@@ -55,17 +55,21 @@ class ElasticsearchServer:
             port = networking.unused_tcp_port()
             transport_port = networking.unused_tcp_port()
 
-            proc = subprocess.Popen(
-                [
-                    elasticsearch_binary,
+            # Set Elasticsearch's initial and max heap to 1.6 GiB, 40% of what's available on Travis, according to
+            # guidance from https://www.elastic.co/guide/en/elasticsearch/reference/current/heap-size.html
+            env = dict(os.environ, ES_JAVA_OPTIONS="-Xms1638m -Xmx1638m")
+
+            # Work around https://github.com/travis-ci/travis-ci/issues/8408
+            if '_JAVA_OPTIONS' in env:  # no coverage
+                logger.warning("_JAVA_OPTIONS is set. This may override the options just set via ES_JAVA_OPTIONS.")
+
+            args = [elasticsearch_binary,
                     "-E", f"http.port={port}",
                     "-E", f"transport.tcp.port={transport_port}",
                     "-E", f"path.data={self.tempdir.name}",
-                    "-E", "logger.org.elasticsearch=warn"
-                ],
-                # Set ES heap to 512 MiB so we can safely run up to four instances in parallel on a Travis container
-                # with 4 GiB of RAM. https://www.elastic.co/guide/en/elasticsearch/reference/current/heap-size.html
-                env=dict(os.environ, ES_JAVA_OPTS="-Xms512m -Xmx512m"))
+                    "-E", "logger.org.elasticsearch=warn"]
+            logger.debug("Running %r with environment %r", args, env)
+            proc = subprocess.Popen(args, env=env)
             for ix in range(startup_timeout_seconds):
                 try:
                     sock = socket.create_connection(("127.0.0.1", port), 1)

commit 950492dc9fe84d719ba61a2b554a964122a74354

Fix: `test` target should depend on `mypy` and `lint`
Regression from 16742eae967356645d7b451b011d50d352a51865

Makefile
@@ -15,14 +15,14 @@ parallel_tests:=$(filter-out $(serial_tests),$(tests))
 
 # Run all standalone tests in parallel
 #
-test: $(tests)
+test: mypy lint $(tests)
 	coverage combine
 	rm -f .coverage.*
 
 # Serialize the standalone tests that start a local Elasticsearch instance in
 # order to prevent more than one such instance at a time.
 #
-safe_test: _serial_test $(parallel_tests)
+safe_test: mypy lint _serial_test $(parallel_tests)
 	coverage combine
 	rm -f .coverage.*
 