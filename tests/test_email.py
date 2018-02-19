f367cdb5d1882f429b45630a77d7b7dd34b290e1
Roman Kisin <roman@parleypro.com>
2018-02-19 10:25:25 -0800

    Fix tests

--- tests/test_email.py
+++ tests/test_email.py
@@ -9,6 +9,7 @@ pkg_root = os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))  # noq
 sys.path.insert(0, pkg_root)  # noqa
 
 import dss.util.email
+from dss import Replica
 from tests.infra import testmode
 from dss import Replica
 
