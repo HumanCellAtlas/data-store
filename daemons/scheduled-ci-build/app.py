b7e8e7a9128108ab7656e6a45552866e06e76c79
Hannes Schmidt <hannes@ucsc.edu>
2018-03-02 22:06:26 -0800

    Generalize CI build scheduler lambda a bit

--- daemons/scheduled-ci-build/app.py
+++ daemons/scheduled-ci-build/app.py
@@ -14,28 +14,28 @@ configure_daemon_logging()
 app = domovoi.Domovoi(configure_logs=False)
 
 
-def define_test_type(test_type: str, envvar: str, schedule_expression: str):
+def define_build(branch_name: str, build_type: str, schedule_expression: str, **env):
     @app.scheduled_function(schedule_expression)
-    def integration_test(event, context):
+    def build(event, context):
         travis_token = os.environ["TRAVIS_TOKEN"]
         rule_name = event["resources"][0].split(":")[-1]
         body = {
             'request': {
-                'branch': "master",
-                'message': f"{test_type} test started by {context.function_name} from {rule_name}",
+                'branch': branch_name,
+                'message': f"{build_type} build started by {context.function_name} from {rule_name}",
                 'config': {
                     'merge_mode': "deep_merge",
                     'env': {
-                        'matrix': [f"{envvar}=1"]
+                        'matrix': [f"{k}={v}" for k, v in env.items()]
                     },
                 },
-            }}
+            }
+        }
         headers = {
             'Accept': "application/json",
             'Travis-API-Version': "3",
             'Authorization': f"token {travis_token}",
         }
-
         return requests.post(
             "https://api.travis-ci.org/repo/HumanCellAtlas%2Fdata-store/requests",
             headers=headers,
@@ -43,4 +43,4 @@ def define_test_type(test_type: str, envvar: str, schedule_expression: str):
         ).json()
 
 
-define_test_type("Integration", "TRAVIS_DSS_INTEGRATION_MODE", "rate(60 minutes)")
+define_build("master", "Integration test", "rate(60 minutes)", TRAVIS_DSS_INTEGRATION_MODE=1)
