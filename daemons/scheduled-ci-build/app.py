37427a76b1d690cdf551bbf39e75a80cecbe9090
Hannes Schmidt <hannes@ucsc.edu>
2018-03-05 10:04:39 +0000

    Eliminate entry point factory from scheduled CI test

--- daemons/scheduled-ci-build/app.py
+++ daemons/scheduled-ci-build/app.py
@@ -14,34 +14,36 @@ configure_daemon_logging()
 app = domovoi.Domovoi(configure_logs=False)
 
 
-def define_build(branch_name: str, build_type: str, schedule_expression: str, **env):
-    @app.scheduled_function(schedule_expression)
-    def build(event, context):
-        travis_token = os.environ["TRAVIS_TOKEN"]
-        rule_name = event["resources"][0].split(":")[-1]
-        body = {
-            'request': {
-                'branch': branch_name,
-                'message': f"{build_type} build started by {context.function_name} from {rule_name}",
-                'config': {
-                    'merge_mode': "deep_merge",
-                    'env': {
-                        'matrix': [f"{k}={v}" for k, v in env.items()]
-                    },
+def trigger_ci_build(event, context, branch_name: str, build_type: str, **env):
+    travis_token = os.environ["TRAVIS_TOKEN"]
+    rule_name = event["resources"][0].split(":")[-1]
+    body = {
+        'request': {
+            'branch': branch_name,
+            'message': f"{build_type} build started by {context.function_name} from {rule_name}",
+            'config': {
+                'merge_mode': "deep_merge",
+                'env': {
+                    'matrix': [f"{k}={v}" for k, v in env.items()]
                 },
-            }
+            },
         }
-        headers = {
-            'Accept': "application/json",
-            'Travis-API-Version': "3",
-            'Authorization': f"token {travis_token}",
-        }
-        return requests.post(
-            "https://api.travis-ci.org/repo/HumanCellAtlas%2Fdata-store/requests",
-            headers=headers,
-            json=body,
-        ).json()
+    }
+    headers = {
+        'Accept': "application/json",
+        'Travis-API-Version': "3",
+        'Authorization': f"token {travis_token}",
+    }
+    return requests.post(
+        "https://api.travis-ci.org/repo/HumanCellAtlas%2Fdata-store/requests",
+        headers=headers,
+        json=body,
+    ).json()
 
 
-# Run integration tests against the `dev` deployment stage at the top of each hour
-define_build("master", "Integration test", "cron(0 * * * ? *)", TRAVIS_DSS_INTEGRATION_MODE=1)
+@app.scheduled_function("cron(0 * * * ? *)")
+def integration_test_on_master(event, context):
+    """
+    Run integration tests against the `dev` deployment stage at the top of each hour
+    """
+    trigger_ci_build(event, context, "master", "Integration test", TRAVIS_DSS_INTEGRATION_MODE=1)
