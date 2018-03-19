9d6f3f14540fdad350c6f073d971cd66820dc256
Hannes Schmidt <hannes@ucsc.edu>
2018-03-18 19:22:43 -0700

    Add require() and reject() to complement `assert`

--- dss/util/__init__.py
+++ dss/util/__init__.py
@@ -64,3 +64,38 @@ class UrlBuilder:
         result = self.splitted._replace(query=urlencode(self.query, doseq=True))
 
         return urlunsplit(result)
+
+
+class RequirementError(RuntimeError):
+    """
+    Unlike assertions, unsatisfied requirements do not consitute a bug in the program.
+    """
+
+
+def require(condition: bool, *args, exception: type = RequirementError):
+    """
+    Raise a RequirementError, or an instance of the given exception class, if the given condition is False.
+
+    :param condition: the boolean condition to be required
+
+    :param args: optional positional arguments to be passed to the exception constructor. Typically only one such
+                 argument should be provided: a string containing a textual description of the requirement.
+
+    :param exception: a custom exception class to be instantiated and raised if the condition does not hold
+    """
+    reject(not condition, *args, exception=exception)
+
+
+def reject(condition: bool, *args, exception: type = RequirementError):
+    """
+    Raise a RequirementError, or an instance of the given exception class, if the given condition is True.
+
+    :param condition: the boolean condition to be rejected
+
+    :param args: optional positional arguments to be passed to the exception constructor. Typically only one such
+                 argument should be provided: a string containing a textual description of the rejected condition.
+
+    :param exception: a custom exception class to be instantiated and raised if the condition occurs
+    """
+    if condition:
+        raise exception(*args)
