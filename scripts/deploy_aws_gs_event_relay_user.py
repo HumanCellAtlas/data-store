fa631ad07f79b1ccd38fa78316bd8b488995e390
Brian Hannafious <bhannafi@ucsc.edu>
2018-03-21 15:18:26 -0700

    dev/integration/staging share gs-event-relay user

--- scripts/deploy_aws_gs_event_relay_user.py
+++ scripts/deploy_aws_gs_event_relay_user.py
@@ -8,8 +8,7 @@ IAM = boto3.client('iam')
 STS = boto3.client('sts')
 
 region = os.environ['AWS_DEFAULT_REGION']
-stage = os.environ['DSS_DEPLOYMENT_STAGE']
-username = f'dss-gs-event-relay-{stage}'
+username = f'dss-gs-event-relay'
 account_id = STS.get_caller_identity().get('Account')
 resource_arn = f'arn:aws:sns:{region}:{account_id}:*'
 
