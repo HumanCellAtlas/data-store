fefd62f8c34191ff87bad40e12962a8cf3d14152
Roman Kisin <roman@parleypro.com>
2018-03-20 21:36:20 -0700

    lint

--- scripts/deploy_dss_sfn_queue.py
+++ scripts/deploy_dss_sfn_queue.py
@@ -6,7 +6,7 @@ import os
 sqs = boto3.resource('sqs')
 
 # Create the queue. This returns an SQS.Queue instance
-queue = sqs.create_queue(QueueName='dss-dlq-sfn-'+os.environ["DSS_DEPLOYMENT_STAGE"], Attributes={'DelaySeconds': '5'})
+queue = sqs.create_queue(QueueName='dss-dlq-sfn-' + os.environ["DSS_DEPLOYMENT_STAGE"], Attributes={'DelaySeconds': '5'})
 
 # You can now access identifiers and attributes
 print(queue.url)
