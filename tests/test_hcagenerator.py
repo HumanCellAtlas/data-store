6ea45e46a3d99a364dc232e672d981713e0ad8a2
Trent Smith <tsmith12@ucsc.edu>
2018-03-23 12:49:34 -0700

    Removed JSON schemas that contain oneOf

--- tests/test_hcagenerator.py
+++ tests/test_hcagenerator.py
@@ -16,13 +16,16 @@ schema_urls = [
     "https://raw.githubusercontent.com/HumanCellAtlas/metadata-schema/4.6.0/json_schema/analysis_bundle.json",
     "https://raw.githubusercontent.com/HumanCellAtlas/metadata-schema/4.6.0/json_schema/assay_bundle.json",
     "https://raw.githubusercontent.com/HumanCellAtlas/metadata-schema/4.6.0/json_schema/project_bundle.json",
-    # TODO (tsmith) uncomment once boolean logic is supported in jsongenerator.py git issue #1012
-    # "https://raw.githubusercontent.com/HumanCellAtlas/metadata-schema/4.6.0/json_schema/sample_bundle.json",
+    "https://schema.humancellatlas.org/bundle/5.1.0/project",
+    "https://schema.humancellatlas.org/bundle/5.1.0/submission",
+    "https://schema.humancellatlas.org/bundle/5.1.0/ingest_audit",
 ]
 
 
 @testmode.standalone
 class TestHCAGenerator(unittest.TestCase):
+    repeat = 25
+
     @classmethod
     def setUpClass(cls):
         Config.set_config(BucketConfig.TEST)
@@ -36,6 +39,7 @@ class TestHCAGenerator(unittest.TestCase):
             self.assertEqual(self.faker.schemas[name], {'$ref': url, 'id': url})
 
     def test_generation(self):
+        for i in range(self.repeat):
             for name in self.faker.schemas.keys():
                 with self.subTest(name):
                     fake_json = self.faker.generate(name)
