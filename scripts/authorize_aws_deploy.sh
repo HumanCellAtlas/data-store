c5d1be427537ecc7a6eb27c8ff37c363d97d3731
Brian Hannafious <32105697+xbrianh@users.noreply.github.com>
2018-03-26 14:04:23 -0700

    Including missing env var in authorize_aws_deploy (#1095)
    Fix type in iam/policy-templates/ci-cd.json

--- scripts/authorize_aws_deploy.sh
+++ scripts/authorize_aws_deploy.sh
@@ -28,6 +28,7 @@ envsubst_vars='$DSS_DEPLOYMENT_STAGE
                $DSS_S3_CHECKOUT_BUCKET_TEST_FIXTURES
                $DSS_S3_CHECKOUT_BUCKET_INTEGRATION
                $DSS_S3_CHECKOUT_BUCKET_STAGING
+               $DSS_PARAMETER_STORE
                $account_id'
 
 aws iam put-${iam_principal_type}-policy \
