ed5c79ad9e15db38c25cd70826277c5ec1b7aa85
Hannes Schmidt <hannes@ucsc.edu>
2018-04-11 14:08:20 -0700

    Create a typing stub for AWS Lambda context objects
    … so it can be used in type hints.

--- dss/util/types.py
+++ dss/util/types.py
@@ -9,3 +9,42 @@ AnyJSON = Union[str, int, float, bool, None, JSONObj, List[Any]]
 # Most JSON structures, however, start with an JSON object, so we'll use the shorter name for that type:
 
 JSON = Mapping[str, AnyJSON]
+
+
+# A stub for the AWS Lambda context
+
+class LambdaContext(object):
+
+    @property
+    def aws_request_id(self) -> str:
+        raise NotImplementedError
+
+    @property
+    def log_group_name(self) -> str:
+        raise NotImplementedError
+
+    @property
+    def log_stream_name(self) -> str:
+        raise NotImplementedError
+
+    @property
+    def function_name(self) -> str:
+        raise NotImplementedError
+
+    @property
+    def memory_limit_in_mb(self) -> str:
+        raise NotImplementedError
+
+    @property
+    def function_version(self) -> str:
+        raise NotImplementedError
+
+    @property
+    def invoked_function_arn(self) -> str:
+        raise NotImplementedError
+
+    def get_remaining_time_in_millis(self) -> int:
+        raise NotImplementedError
+
+    def log(self, msg: str) -> None:
+        raise NotImplementedError
