937522c0bcdc4ff0ff6dc02bf08a412f6748b150
Roman Kisin <roman@parleypro.com>
2018-04-13 11:05:37 -0700

    Make email optional

--- daemons/dss-checkout-sfn/app.py
+++ daemons/dss-checkout-sfn/app.py
@@ -86,12 +86,13 @@ def pre_execution_check(event, context):
 @app.step_function_task(state_name="Notify", state_machine_definition=state_machine_def)
 def notify_complete(event, context):
     replica = Replica[event["replica"]]
-    result = send_checkout_success_email(email_sender, event["email"], get_dst_bucket(event),
-                                         event["schedule"]["dst_location"], replica)
+    if "email" in event:
+        result = send_checkout_success_email(email_sender, event["email"], get_dst_bucket(event),
+                                             event["schedule"]["dst_location"], replica)
     # record results of execution into S3
     put_status_succeeded(event['execution_name'], replica, get_dst_bucket(event),
                          event["schedule"]["dst_location"])
-    return {"result": result}
+    return {"result": result} if result else {}
 
 
 @app.step_function_task(state_name="NotifyFailure", state_machine_definition=state_machine_def)
@@ -102,10 +103,11 @@ def notify_complete_failure(event, context):
     elif "validation" in event:
         checkout_status = event["validation"].get("checkout_status", "Unknown error code")
         cause = "{} ({})".format(event["validation"].get("cause", "Unknown error"), checkout_status)
-    result = send_checkout_failure_email(email_sender, event["email"], cause)
+    if "email" in event:
+        result = send_checkout_failure_email(email_sender, event["email"], cause)
     # record results of execution into S3
     put_status_failed(event['execution_name'], cause)
-    return {"result": result}
+    return {"result": result} if result else {}
 
 
 def get_dst_bucket(event):
