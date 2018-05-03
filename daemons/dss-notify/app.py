26da19b244f9074a5c4461cad749737f38441452
Hannes Schmidt <hannes@ucsc.edu>
2018-05-03 16:49:15 -0700

    Work around kislyuk/domovoi#9 (connected to #1211)

--- daemons/dss-notify/app.py
+++ daemons/dss-notify/app.py
@@ -6,6 +6,7 @@ import domovoi
 pkg_root = os.path.abspath(os.path.join(os.path.dirname(__file__), 'domovoilib'))  # noqa
 sys.path.insert(0, pkg_root)  # noqa
 
+from dss import Config
 from dss.logging import configure_lambda_logging
 from dss.notify.notifier import Notifier
 
@@ -20,7 +21,11 @@ def deploy_notifier():
 app = domovoi.Domovoi(configure_logs=False)
 
 
-@app.scheduled_function("rate(1 minute)")
 def run_notifier(event, context):
     notifier = Notifier.from_config()
     notifier.run(context)
+
+
+# FIXME: https://github.com/HumanCellAtlas/data-store/issues/1211
+run_notifier.__name__ += "_" + Config.deployment_stage()
+run_notifier = app.scheduled_function("rate(1 minute)")(run_notifier)
