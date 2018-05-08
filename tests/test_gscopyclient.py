fa1f75fe5ff001423f47b3c66b13ca76ee304053
Brian Hannafious <bhannafi@ucsc.edu>
2018-05-08 13:51:23 -0700

    comment

--- tests/test_gscopyclient.py
+++ tests/test_gscopyclient.py
@@ -51,7 +51,7 @@ class TestGSCopy(unittest.TestCase):
         # NOTE: compose(…) does not seem to support setting a storage class.  The canonical way of changing storage
         # class is to call update_storage_class(…), but Google's libraries does not seem to handle
         # update_storage_class(…) calls for large objects.
-        # Second NOTE: nearline storage is not as snappy as normal storage, and requires longer timeouts.
+        # Second NOTE: nearline storage upload is not as snappy as normal storage, and requires longer timeouts.
         final_blob_obj = Config.get_native_handle(
             Replica.gcp,
             connect_timeout=60,
