219d44463cfddd716ccbce344ca08e57ad5cb8e6
Brian Hannafious <32105697+xbrianh@users.noreply.github.com>
2018-05-08 14:10:50 -0700

    Lengthen nearline storage timeouts in TestGSCopy (#1220)
    * Lengthen nearline storage timeouts in TestGSCopy
    
    During test setup, TestGSCopy uploads a blob to GCP "nearline" storage,
    which is a semi-cold storage service. Since nearline storage uploads
    do not appear to be particularly snappy, use longer connect/read
    timeouts during upload.

--- dss/config.py
+++ dss/config.py
@@ -116,13 +116,21 @@ class Config:
 
     @staticmethod
     @functools.lru_cache()
-    def get_native_handle(replica: "Replica") -> typing.Any:
+    def get_native_handle(replica: "Replica",
+                          connect_timeout: float=None,
+                          read_timeout: float=None) -> typing.Any:
+
+        if connect_timeout is None:
+            connect_timeout = Config.BLOBSTORE_CONNECT_TIMEOUT
+        if read_timeout is None:
+            read_timeout = Config.BLOBSTORE_READ_TIMEOUT
+
         if replica == Replica.aws:
             return boto3.client(
                 "s3",
                 config=botocore.config.Config(
-                    connect_timeout=Config.BLOBSTORE_CONNECT_TIMEOUT,
-                    read_timeout=Config.BLOBSTORE_READ_TIMEOUT,
+                    connect_timeout=connect_timeout,
+                    read_timeout=read_timeout,
                     retries={'max_attempts': Config.BLOBSTORE_BOTO_RETRIES}
                 )
             )
@@ -133,7 +141,7 @@ class Config:
             # stdlib `requests` package, which has straightforward timeout usage.
             class SessionWithTimeouts(google.auth.transport.requests.AuthorizedSession):
                 def request(self, *args, **kwargs):
-                    kwargs['timeout'] = (Config.BLOBSTORE_CONNECT_TIMEOUT, Config.BLOBSTORE_READ_TIMEOUT)
+                    kwargs['timeout'] = (connect_timeout, read_timeout)
                     return super().request(*args, **kwargs)
 
             credentials = service_account.Credentials.from_service_account_file(
