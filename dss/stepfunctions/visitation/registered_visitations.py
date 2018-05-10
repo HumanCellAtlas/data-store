e50b16a957c38b74a3c2c7ff801666bf9a182b6c
Hannes Schmidt <hannes@ucsc.edu>
2018-05-09 22:35:58 -0700

    Simplify visitation registration

--- dss/stepfunctions/visitation/registered_visitations.py
+++ dss/stepfunctions/visitation/registered_visitations.py
@@ -2,8 +2,7 @@
 from .integration_test import IntegrationTest
 from .reindex import Reindex
 
-
-registered_visitations = {
-    'IntegrationTest': IntegrationTest,
-    'Reindex': Reindex
-}
+registered_visitations = {c.__name__: c for c in [
+    IntegrationTest,
+    Reindex,
+]}
