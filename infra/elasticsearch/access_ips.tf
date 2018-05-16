2be2724d81e058c377270a8bde51096dd484d958
Brian Hannafious <32105697+xbrianh@users.noreply.github.com>
2018-05-16 11:12:29 -0700

    Elasticsearch infra tweaks (#1240)

---
+++ infra/elasticsearch/access_ips.tf
@@ -0,0 +1,8 @@
+data "aws_secretsmanager_secret_version" "source_ips" {
+  secret_id = "${var.DSS_SECRETS_STORE}/${var.DSS_DEPLOYMENT_STAGE}/${var.ES_ALLOWED_SOURCE_IP_SECRETS_NAME}"
+}
+
+locals {
+  ips_str = "${data.aws_secretsmanager_secret_version.source_ips.secret_string}"
+  access_ips = "${compact(split(",", local.ips_str))}"
+}
