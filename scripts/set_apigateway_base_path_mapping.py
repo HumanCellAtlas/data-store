dfd90fda433dc0d45f3a71377ca123ede6573f74
Brian Hannafious <bhannafi@ucsc.edu>
2018-05-18 14:51:40 -0700

    Script for setting up the Apigateway base path map
    A script for idempotently mapping the AWS Apigateway custom domain
    name to the Apigateway stage. It should be executed for a
    first-time deployment after successfully running `make deploy-infra`
    and `make deploy`.

---
+++ scripts/set_apigateway_base_path_mapping.py
@@ -0,0 +1,46 @@
+#!/usr/bin/env python
+
+"""
+This script idempotently maps the AWS Apigateway custom domain name to the Apigateway
+stage. It should be executed for a first-time deployment after successfully
+running `make deploy-infra` and `make deploy`.
+"""
+
+import os
+import sys
+import json
+import boto3
+
+stage = os.environ['DSS_DEPLOYMENT_STAGE']
+domain_name = os.environ['API_DOMAIN_NAME']
+
+APIGATEWAY = boto3.client('apigateway')
+LAMBDA = boto3.client('lambda')
+lambda_name = f'dss-{stage}'
+
+lambda_arn = None
+paginator = LAMBDA.get_paginator('list_functions')
+for page in paginator.paginate():
+    for l in page['Functions']:
+        if lambda_name == l['FunctionName']:
+            lambda_arn = l['FunctionArn']
+            break
+
+if not lambda_arn:
+    raise Exception(f'Lambda function {lambda_name} not found. Did you run `make deploy`?')
+
+policy = json.loads(
+    LAMBDA.get_policy(FunctionName=lambda_name)['Policy']
+)
+
+source_arn = policy['Statement'][0]['Condition']['ArnLike']['AWS:SourceArn']
+api_id = source_arn.split(':')[5].split('/')[0]
+
+try:
+    APIGATEWAY.create_base_path_mapping(
+        domainName=domain_name,
+        restApiId=api_id,
+        stage=stage
+    )
+except APIGATEWAY.exceptions.ConflictException:
+    pass
