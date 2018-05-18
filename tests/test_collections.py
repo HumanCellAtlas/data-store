be6430c89320cec087c35c0ceef3e737dd405bd2
Andrey Kislyuk <kislyuk@gmail.com>
2018-05-17 18:01:41 -0700

    WIP

--- tests/test_collections.py
+++ tests/test_collections.py
@@ -32,6 +32,7 @@ class TestCollections(unittest.TestCase, DSSAssertMixin, DSSUploadMixin):
                         'hca-dss-sha1': sums['sha1'].lower(),
                         'hca-dss-sha256': sums['sha256'].lower()}
             fh.seek(0)
+            # TODO: consider switching to unmanaged uploader (putobject w/blob)
             s3.Bucket(s3_test_bucket).Object(src_key).upload_fileobj(fh, ExtraArgs={"Metadata": metadata})
         source_url = f"s3://{s3_test_bucket}/{src_key}"
         file_uuid = str(uuid4())
@@ -52,29 +53,29 @@ class TestCollections(unittest.TestCase, DSSAssertMixin, DSSUploadMixin):
         contents = [col_file_item] * 8 + [col_ptr_item] * 8
 
         with self.subTest("PUT new collection"):
-            res = requests.put("http://localhost:5000/v1/collections",
+            res = self.app.put("/v1/collections",
                                headers=get_auth_header(authorized=True),
                                params=dict(uuid=str(uuid4()), version=datetime.now().isoformat(), replica="aws"),
                                json=dict(name="n", description="d", details={}, contents=contents))
             res.raise_for_status()
             uuid, version = res.json()["uuid"], res.json()["version"]
         with self.subTest("GET created collection"):
-            res = requests.get("http://localhost:5000/v1/collections/{}".format(uuid),
+            res = self.app.get("/v1/collections/{}".format(uuid),
                                headers=get_auth_header(authorized=True),
                                params=dict(version=version, replica="aws"))
             res.raise_for_status()
             self.assertEqual(res.json()["contents"], contents)
         with self.subTest("GET latest version of collection"):
-            res = requests.get("http://localhost:5000/v1/collections/{}".format(uuid),
+            res = self.app.get("/v1/collections/{}".format(uuid),
                                headers=get_auth_header(authorized=True),
                                params=dict(replica="aws"))
             res.raise_for_status()
             self.assertEqual(res.json()["contents"], contents)
-        res = requests.get("http://localhost:5000/v1/collections/{}".format(uuid),
+        res = self.app.get("/v1/collections/{}".format(uuid),
                            headers=get_auth_header(authorized=True),
                            params=dict(replica="aws", version="9000"))
         self.assertEqual(res.status_code, requests.codes.not_found)
-        res = requests.patch("http://localhost:5000/v1/collections/{}".format(uuid),
+        res = self.app.patch("/v1/collections/{}".format(uuid),
                              headers=get_auth_header(authorized=True),
                              params=dict(replica="aws"),
                              json=dict())
@@ -85,14 +86,14 @@ class TestCollections(unittest.TestCase, DSSAssertMixin, DSSUploadMixin):
                               dict(addContents=contents),
                               dict(removeContents=contents)]:
             with self.subTest("PATCH with {}".format(patch_payload)):
-                res = requests.patch("http://localhost:5000/v1/collections/{}".format(uuid),
+                res = self.app.patch("/v1/collections/{}".format(uuid),
                                      headers=get_auth_header(authorized=True),
                                      params=dict(version=version, replica="aws"),
                                      json=patch_payload)
                 res.raise_for_status()
                 version = res.json()["version"]
         with self.subTest("Get updated version of collection"):
-            res = requests.get("http://localhost:5000/v1/collections/{}".format(uuid),
+            res = self.app.get("/v1/collections/{}".format(uuid),
                                headers=get_auth_header(authorized=True),
                                params=dict(replica="aws"))
             res.raise_for_status()
@@ -102,7 +103,7 @@ class TestCollections(unittest.TestCase, DSSAssertMixin, DSSUploadMixin):
                              dict(contents=[], description='bar', details={"1": 2}, name='cn'))
         with self.subTest("PUT invalid fragment reference"):
             invalid_ptr = dict(type="foo", uuid=file_uuid, version=file_version, fragment="/xyz")
-            res = requests.put("http://localhost:5000/v1/collections",
+            res = self.app.put("/v1/collections",
                                headers=get_auth_header(authorized=True),
                                params=dict(uuid=str(uuid4()), version=datetime.now().isoformat(), replica="aws"),
                                json=dict(name="n", description="d", details={}, contents=[invalid_ptr]))
@@ -112,28 +113,28 @@ class TestCollections(unittest.TestCase, DSSAssertMixin, DSSUploadMixin):
         with self.subTest("Dedup semantics"):
             pass
         with self.subTest("GET access control"):
-            res = requests.get("http://localhost:5000/v1/collections/{}".format(uuid),
+            res = self.app.get("/v1/collections/{}".format(uuid),
                                headers=get_auth_header(authorized=False),
                                params=dict(replica="aws"))
             self.assertEqual(res.status_code, requests.codes.forbidden)
         with self.subTest("PATCH access control"):
-            res = requests.patch("http://localhost:5000/v1/collections/{}".format(uuid),
+            res = self.app.patch("/v1/collections/{}".format(uuid),
                                  headers=get_auth_header(authorized=False),
                                  params=dict(replica="aws"),
                                  json=dict(description="foo"))
             self.assertEqual(res.status_code, requests.codes.forbidden)
         with self.subTest("DELETE access control"):
-            res = requests.delete("http://localhost:5000/v1/collections/{}".format(uuid),
+            res = self.app.delete("/v1/collections/{}".format(uuid),
                                   headers=get_auth_header(authorized=False),
                                   params=dict(replica="aws"))
             self.assertEqual(res.status_code, requests.codes.forbidden)
         with self.subTest("Delete collection"):
-            res = requests.delete("http://localhost:5000/v1/collections/{}".format(uuid),
+            res = self.app.delete("/v1/collections/{}".format(uuid),
                                   headers=get_auth_header(authorized=True),
                                   params=dict(replica="aws"))
             res.raise_for_status()
         with self.subTest("Verify deleted"):
-            res = requests.get("http://localhost:5000/v1/collections/{}".format(uuid),
+            res = self.app.get("/v1/collections/{}".format(uuid),
                                headers=get_auth_header(authorized=True),
                                params=dict(replica="aws"))
             self.assertEqual(res.status_code, requests.codes.not_found)
