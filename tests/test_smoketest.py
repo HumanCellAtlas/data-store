8bebaf1a15f5c76c27af3389d606dbe8b6777932
Trent Smith <tsmith12@ucsc.edu>
2018-06-01 15:28:21 -0700

    Adding GCP replica to smoketest
    * Moved common setup code to setUpClass
    * Added support for testing additional replicas
    * Added GSBlobstore for GS checkout verification.

--- tests/test_smoketest.py
+++ tests/test_smoketest.py
@@ -15,6 +15,7 @@ from dss.api.files import ASYNC_COPY_THRESHOLD
 from tests.infra import testmode
 from dss.storage.checkout import get_dst_bundle_prefix
 from cloud_blobstore.s3 import S3BlobStore
+from cloud_blobstore.gs import GSBlobStore
 
 
 parser = argparse.ArgumentParser(description=__doc__)
@@ -52,9 +53,12 @@ def run_for_json(command, **kwargs):
 
 @testmode.integration
 class Smoketest(unittest.TestCase):
-    replicas = "aws", "gcp"
+    replicas = {
+        "aws",
+        "gcp"
+    }
     notification_bucket = os.environ['DSS_S3_BUCKET_TEST']
-    checkout_bucket = os.environ['DSS_S3_CHECKOUT_BUCKET']
+    checkout_bucket = {'aws': os.environ['DSS_S3_CHECKOUT_BUCKET'], 'gcp': os.environ['DSS_GS_CHECKOUT_BUCKET']}
     test_bucket = os.environ['DSS_S3_BUCKET_TEST']
 
     @classmethod
@@ -68,37 +72,36 @@ class Smoketest(unittest.TestCase):
             # Disable workdir destructor
             cls.workdir._finalizer.detach()  # type: ignore
 
-    def test_smoketest(self):
-
         # Create a virtualenv and install the CLI
         #
-        venv = os.path.join(self.workdir.name, "venv")
+        venv = os.path.join(cls.workdir.name, "venv")
         run(f"virtualenv -p {sys.executable} {venv}")
-        venv_bin = os.path.join(venv, "bin", "")
-        run(f"{venv_bin}pip install --upgrade .", cwd="dcp-cli")
-
-        # Prepare the bundle using stock metadata and random data
-        #
-        bundle_dir = os.path.join(self.workdir.name, "bundle")
-        shutil.copytree("data-bundle-examples/10X_v2/pbmc8k", bundle_dir)
-        with open(os.path.join(bundle_dir, "async_copied_file"), "wb") as fh:
-            file_size = ASYNC_COPY_THRESHOLD + 1  # Ensure that files need to be copied asynchronously
-            fh.write(os.urandom(file_size))
-        # Tweak the metadata to a specific sample UUID
-        sample_id = str(uuid.uuid4())
-        run(f"cat {bundle_dir}/sample.json | jq .id=env.sample_id | sponge {bundle_dir}/sample.json",
-            env=dict(os.environ, sample_id=sample_id))
-        query = {'query': {'match': {'files.sample_json.id': sample_id}}}
-
-        os.chdir(self.workdir.name)
+        cls.venv_bin = os.path.join(venv, "bin", "")
+        run(f"{cls.venv_bin}pip install --upgrade .", cwd="dcp-cli")
 
         # Configure the CLI
         #
         cli_config = {"DSSClient": {"swagger_url": os.environ["SWAGGER_URL"]}}
-        cli_config_filename = f"{self.workdir.name}/cli_config.json"
+        cli_config_filename = f"{cls.workdir.name}/cli_config.json"
         with open(cli_config_filename, "w") as fh2:
             fh2.write(json.dumps(cli_config))
-        os.environ["HCA_CONFIG_FILE"] = f"{self.workdir.name}/cli_config.json"
+        os.environ["HCA_CONFIG_FILE"] = f"{cls.workdir.name}/cli_config.json"
+
+        # Prepare the bundle using stock metadata and random data
+        #
+        cls.bundle_dir = os.path.join(cls.workdir.name, "bundle")
+        shutil.copytree("data-bundle-examples/10X_v2/pbmc8k", cls.bundle_dir)
+        with open(os.path.join(cls.bundle_dir, "async_copied_file"), "wb") as fh:
+            fh.write(os.urandom(ASYNC_COPY_THRESHOLD + 1))
+
+    def smoketest(self, starting_replica):
+        # Tweak the metadata to a specific sample UUID
+        sample_id = str(uuid.uuid4())
+        run(f"cat {self.bundle_dir}/sample.json | jq .id=env.sample_id | sponge {self.bundle_dir}/sample.json",
+            env=dict(os.environ, sample_id=sample_id))
+        query = {'query': {'match': {'files.sample_json.id': sample_id}}}
+
+        os.chdir(self.workdir.name)
 
         # Create a subscription for each replica using the query
         #
@@ -110,89 +113,93 @@ class Smoketest(unittest.TestCase):
                                             Params=dict(Bucket=self.notification_bucket,
                                                         Key=notification_key,
                                                         ContentType='application/json'))
-            put_response = run_for_json([f'{venv_bin}hca', 'dss', 'put-subscription',
+            put_response = run_for_json([f'{self.venv_bin}hca', 'dss', 'put-subscription',
                                          '--callback-url', url,
                                          '--method', 'PUT',
                                          '--es-query', json.dumps(query),
                                          '--replica', replica])
             subscription_id = put_response['uuid']
-            self.addCleanup(run, f"{venv_bin}hca dss delete-subscription --replica {replica} --uuid {subscription_id}")
+            self.addCleanup(run, f"{self.venv_bin}hca dss delete-subscription --replica {replica} "
+                                 f"--uuid {subscription_id}")
             self.addCleanup(s3.delete_object, Bucket=self.notification_bucket, Key=notification_key)
             notifications_proofs[replica] = (subscription_id, notification_key)
-            get_response = run_for_json(f"{venv_bin}hca dss get-subscription "
+            get_response = run_for_json(f"{self.venv_bin}hca dss get-subscription "
                                         f"--replica {replica} "
                                         f"--uuid {subscription_id}")
             self.assertEquals(subscription_id, get_response['uuid'])
             self.assertEquals(url, get_response['callback_url'])
-            list_response = run_for_json(f"{venv_bin}hca dss get-subscriptions --replica {replica}")
+            list_response = run_for_json(f"{self.venv_bin}hca dss get-subscriptions --replica {replica}")
             self.assertIn(get_response, list_response['subscriptions'])
 
         # Create the bundle
         #
-        res = run_for_json(f"{venv_bin}hca dss upload "
-                           "--replica aws "
+        res = run_for_json(f"{self.venv_bin}hca dss upload "
+                           f"--replica {starting_replica} "
                            f"--staging-bucket {self.test_bucket} "
-                           f"--src-dir {bundle_dir}")
+                           f"--src-dir {self.bundle_dir}")
         bundle_uuid = res['bundle_uuid']
         bundle_version = res['version']
         file_count = len(res['files'])
 
         # Download that bundle
         #
-        run(f"{venv_bin}hca dss download --replica aws --bundle-uuid {bundle_uuid}")
+        run(f"{self.venv_bin}hca dss download --replica {starting_replica} --bundle-uuid {bundle_uuid}")
 
         # Initiate a bundle checkout
         #
-        res = run_for_json(f"{venv_bin}hca dss post-bundles-checkout --uuid {bundle_uuid} --replica aws")
+        res = run_for_json(f"{self.venv_bin}hca dss post-bundles-checkout --uuid {bundle_uuid} "
+                           f"--replica {starting_replica}")
         checkout_job_id = res['checkout_job_id']
         print(f"Checkout jobId: {checkout_job_id}")
-        assert checkout_job_id
+        self.assertTrue(checkout_job_id)
 
-        # Wait for the bundle to appear in the other replica
+        # Wait for the bundle to appear in the other replicas
         #
-        for i in range(10):
-            try:
-                run(f"http -Iv --check-status GET https://${{API_DOMAIN_NAME}}/v1/bundles/{bundle_uuid}?replica=gcp")
-            except SystemExit:
-                time.sleep(1)
+        other_replicas = self.replicas - {starting_replica}
+        for replica in other_replicas:
+            for i in range(10):
+                try:
+                    run(f"http -Iv --check-status "
+                        f"GET https://${{API_DOMAIN_NAME}}/v1/bundles/{bundle_uuid}?replica={replica}")
+                except SystemExit:
+                    time.sleep(1)
+                else:
+                    break
             else:
-                break
-        else:
-            parser.exit(RED("Failed to replicate bundle from AWS to GCP"))
+                parser.exit(RED(f"Failed to replicate bundle from {starting_replica} to {replica}"))
 
-        # Download bundle from other replica
-        #
-        run(f"{venv_bin}hca dss download --replica gcp --bundle-uuid {bundle_uuid}")
+            # Download bundle from other replica
+            #
+            run(f"{self.venv_bin}hca dss download --replica {replica} --bundle-uuid {bundle_uuid}")
 
-        # Run a CLI search against the two replicas
-        #
         for replica in self.replicas:
-            run(f"{venv_bin}hca dss post-search --es-query='{{}}' --replica {replica} > /dev/null")
+            # Run a CLI search against the replicas
+            #
+            run(f"{self.venv_bin}hca dss post-search --es-query='{{}}' --replica {replica} > /dev/null")
 
-        # Hit search route directly against each replica
-        #
-        search_route = "https://${API_DOMAIN_NAME}/v1/search"
-        for replica in self.replicas:
+            # Hit search route directly against each replica
+            #
+            search_route = "https://${API_DOMAIN_NAME}/v1/search"
             res = run_for_json(f'http --check {search_route} replica=={replica}',
                                input=json.dumps({'es_query': query}).encode())
             print(json.dumps(res, indent=4))
-            assert len(res['results']) == 1
+            self.assertEqual(len(res['results']), 1)
 
         # Wait for the checkout to complete and assert its success
         #
         for i in range(10):
-            res = run_for_json(
-                f"{venv_bin}hca dss get-bundles-checkout --checkout-job-id {checkout_job_id} --replica aws")
+            res = run_for_json(f"{self.venv_bin}hca dss get-bundles-checkout --checkout-job-id {checkout_job_id} "
+                               f"--replica {starting_replica}")
             status = res['status']
             self.assertGreater(len(status), 0)
             if status == 'RUNNING':
                 time.sleep(6)
             else:
                 self.assertEqual(status, 'SUCCEEDED')
-                blob_handle = S3BlobStore.from_environment()
+                blob_handle = self.get_blobstore(starting_replica)
                 object_key = get_dst_bundle_prefix(bundle_uuid, bundle_version)
-                print(f"Checking bucket {self.checkout_bucket} object key: {object_key}")
-                files = list(blob_handle.list(self.checkout_bucket, object_key))
+                print(f"Checking bucket {self.checkout_bucket[starting_replica]} object key: {object_key}")
+                files = list(blob_handle.list(self.checkout_bucket[starting_replica], object_key))
                 self.assertEqual(len(files), file_count)
                 break
         else:
@@ -206,6 +213,19 @@ class Smoketest(unittest.TestCase):
             self.assertEquals(subscription_id, notification['subscription_id'])
             self.assertEquals(bundle_uuid, notification['match']['bundle_uuid'])
             self.assertEquals(bundle_version, notification['match']['bundle_version'])
+
+    def test_smoketest(self):
+        for replica in self.replicas:
+            with self.subTest(replica):
+                self.smoketest(replica)
+
+    def get_blobstore(self, replica):
+        if replica is 'aws':
+            return S3BlobStore.from_environment()
+        elif replica is 'gcp':
+            return GSBlobStore.from_auth_credentials(os.getenv('GOOGLE_APPLICATION_CREDENTIALS'))
+        else:
+            raise NameError("Replica not found")
 
     @classmethod
     def tearDownClass(cls):
