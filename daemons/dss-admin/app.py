6bdb835594a5830f12e3046cbc62f53bb75d0986
Hannes Schmidt <hannes@ucsc.edu>
2018-06-06 00:18:27 -0700

    Consistency check covers blobs, files, and bundle manifests
    (fixes #1274, fixes #1275)

--- daemons/dss-admin/app.py
+++ daemons/dss-admin/app.py
@@ -13,7 +13,6 @@ from dss import BucketConfig, Config, Replica
 from dss.logging import configure_lambda_logging
 from dss.stepfunctions.visitation.index import IndexVisitation
 from dss.stepfunctions.visitation.storage import StorageVisitation
-from dss.util import tracing
 from dss.util.types import JSON
 
 
@@ -60,15 +59,21 @@ class StorageTarget(Target):
     """
     Admin operations on the storage buckets and the replication between them.
     """
-    def __init__(self, replicas: Mapping[str, str]) -> None:
+    def __init__(self, replicas: Mapping[str, str], prefix: str = None) -> None:
+        super().__init__()
         replicas = ((Replica[replica], bucket) for replica, bucket in replicas.items())
         self.replicas = {replica.name: bucket or replica.bucket for replica, bucket in replicas}
-        super().__init__()
+        self.prefix = prefix or ''
 
-    def verify(self, workers: int) -> JSON:
+    def verify(self, workers: int, folder: str, quick: bool = False) -> JSON:
         assert 1 < workers
         replicas, buckets = zip(*self.replicas.items())
-        return StorageVisitation.start(workers, replicas=replicas, buckets=buckets)
+        return StorageVisitation.start(workers,
+                                       replicas=replicas,
+                                       buckets=buckets,
+                                       prefix=self.prefix,
+                                       folder=folder,
+                                       quick=quick)
 
 
 def _invoke(f: Callable, kwargs: Mapping[str, Any]) -> Any:
