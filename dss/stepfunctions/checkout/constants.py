50ddbb6d2835afe4426ba563797c0cf84593ab57
Tony Tung <tonytung@merly.org>
2018-06-25 11:23:11 -0700

    Remove magic constants from checkout code. (#1354)
    Test plan: Deployed daemon and ran integration+standalone checkout tests.

---
+++ dss/stepfunctions/checkout/constants.py
@@ -0,0 +1,16 @@
+class EventConstants:
+    """Externally visible constants used in the SFN messages."""
+
+    EXECUTION_NAME = "execution_name"
+    BUNDLE_UUID = "bundle"
+    BUNDLE_VERSION = "version"
+    DSS_BUCKET = "dss_bucket"
+    DST_BUCKET = "bucket"
+    REPLICA = "replica"
+    STATUS = "status"
+    EMAIL = "email"
+
+    STATUS_COMPLETE_COUNT = "complete_count"
+    STATUS_TOTAL_COUNT = "total_count"
+    STATUS_CHECK_COUNT = "check_count"
+    STATUS_OVERALL_STATUS = "checkout_status"
