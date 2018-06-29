127f6f6c059985b7086304a49bcba891c8074101
Trent Smith <tsmith12@ucsc.edu>
2018-06-29 10:48:36 -0700

    Add retries to google blobstore

--- dss/config.py
+++ dss/config.py
@@ -101,6 +101,7 @@ class Config:
     BLOBSTORE_READ_TIMEOUT = None  # type: float
     BLOBSTORE_BOTO_RETRIES = None  # type: int
     BLOBSTORE_GS_MAX_CUMULATIVE_RETRY = None  # type: float  ## seconds
+    BLOBSTORE_GS_RETRIES: int = None
 
     _ALLOWED_EMAILS = None  # type: typing.Optional[str]
     _CURRENT_CONFIG = BucketConfig.ILLEGAL  # type: BucketConfig
@@ -136,11 +137,8 @@ class Config:
 
     @staticmethod
     def _get_native_gcp_handle() -> typing.Any:
-        if Config.BLOBSTORE_GS_MAX_CUMULATIVE_RETRY is not None:
-            google.resumable_media.common.MAX_CUMULATIVE_RETRY = Config.BLOBSTORE_GS_MAX_CUMULATIVE_RETRY
-
         if Config.BLOBSTORE_CONNECT_TIMEOUT is None and Config.BLOBSTORE_READ_TIMEOUT is None:
-            return Client.from_service_account_json(
+            client = Client.from_service_account_json(
                 os.environ['GOOGLE_APPLICATION_CREDENTIALS'],
             )
         else:
@@ -158,10 +156,24 @@ class Config:
 
             # _http is a "private" parameter, and we may need to re-visit GCP timeout retry
             # strategies in the future.
-            return Client(
+            client = Client(
                 _http=SessionWithTimeouts(credentials),
                 credentials=credentials
             )
+
+        if Config.BLOBSTORE_GS_RETRIES is not None:
+            from requests.adapters import HTTPAdapter
+            from requests.packages.urllib3.util.retry import Retry
+            retry = Retry(total=Config.BLOBSTORE_GS_RETRIES,
+                          backoff_factor=0.3,
+                          status_forcelist=(500, 502, 504)
+                          )
+            adapter = HTTPAdapter(max_retries=retry)
+            # _http is a "private" parameter, and we may need to re-visit GCP timeout retry
+            # strategies in the future.
+            client._http.mount('https://', adapter)
+            client._http.mount('http://', adapter)
+        return client
 
     @staticmethod
     @functools.lru_cache()
