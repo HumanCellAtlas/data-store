97df4e85030afb7a2675eea8f055d71512e52da2
Brian Hannafious <32105697+xbrianh@users.noreply.github.com>
2018-07-11 19:42:12 -0700

    Infra build system cosmetics (#1421)
    * Sort env_vars_to_infra in infra/build_deploy_config.py
    * Mark generated TF files as generated
    * Switch variables.tf from json to hcl (HashiCorp Configuration Language)

--- infra/build_deploy_config.py
+++ infra/build_deploy_config.py
@@ -16,7 +16,15 @@ parser.add_argument("component")
 args = parser.parse_args()
 
 
-terraform_backend_template = """terraform {{
+terraform_variable_template = """
+variable "{name}" {{
+  default = "{val}"
+}}
+"""
+
+terraform_backend_template = """# Auto-generated during infra build process.
+# Please edit infra/build_deploy_config.py directly.
+terraform {{
   backend "s3" {{
     bucket = "{bucket}"
     key = "dss-{comp}-{stage}.tfstate"
@@ -26,7 +34,8 @@ terraform_backend_template = """terraform {{
 }}
 """
 
-terraform_providers_template = """
+terraform_providers_template = """# Auto-generated during infra build process.
+# Please edit infra/build_deploy_config.py directly.
 provider aws {{
   region = "{aws_region}"
 }}
@@ -37,54 +46,47 @@ provider google {{
 """
 
 env_vars_to_infra = [
-    "DSS_DEPLOYMENT_STAGE",
+    "API_DOMAIN_NAME",
     "AWS_DEFAULT_OUTPUT",
     "AWS_DEFAULT_REGION",
-    "GCP_DEFAULT_REGION",
-    "DSS_S3_BUCKET",
-    "DSS_S3_BUCKET_TEST",
-    "DSS_S3_BUCKET_TEST_FIXTURES",
-    "DSS_S3_BUCKET_INTEGRATION",
-    "DSS_S3_BUCKET_STAGING",
-    "DSS_S3_BUCKET_PROD",
-    "DSS_S3_CHECKOUT_BUCKET",
-    "DSS_S3_CHECKOUT_BUCKET_TEST",
-    "DSS_S3_CHECKOUT_BUCKET_INTEGRATION",
-    "DSS_S3_CHECKOUT_BUCKET_STAGING",
-    "DSS_S3_CHECKOUT_BUCKET_PROD",
+    "DSS_CERTIFICATE_ADDITIONAL_NAMES",
+    "DSS_CERTIFICATE_DOMAIN",
+    "DSS_CERTIFICATE_VALIDATION",
+    "DSS_DEPLOYMENT_STAGE",
+    "DSS_ES_DOMAIN",
+    "DSS_ES_INSTANCE_COUNT",
+    "DSS_ES_INSTANCE_TYPE",
+    "DSS_ES_VOLUME_SIZE",
     "DSS_GS_BUCKET",
+    "DSS_GS_BUCKET_INTEGRATION",
+    "DSS_GS_BUCKET_PROD",
+    "DSS_GS_BUCKET_REGION",
+    "DSS_GS_BUCKET_STAGING",
     "DSS_GS_BUCKET_TEST",
     "DSS_GS_BUCKET_TEST_FIXTURES",
-    "DSS_GS_BUCKET_INTEGRATION",
-    "DSS_GS_BUCKET_STAGING",
-    "DSS_GS_BUCKET_PROD",
-    "DSS_GS_CHECKOUT_BUCKET",
-    "DSS_GS_CHECKOUT_BUCKET_TEST",
-    "DSS_GS_CHECKOUT_BUCKET_STAGING",
-    "DSS_GS_CHECKOUT_BUCKET_PROD",
-    "DSS_ES_DOMAIN",
-    "DSS_ES_VOLUME_SIZE",
-    "DSS_ES_INSTANCE_TYPE",
-    "DSS_ES_INSTANCE_COUNT",
-    "DSS_SECRETS_STORE",
-    "DSS_DEPLOYMENT_STAGE",
-    "ES_ALLOWED_SOURCE_IP_SECRETS_NAME",
-    "DSS_GS_BUCKET_REGION",
-    "DSS_GS_BUCKET_TEST_REGION",
     "DSS_GS_BUCKET_TEST_FIXTURES_REGION",
-    "API_DOMAIN_NAME",
-    "DSS_CERTIFICATE_DOMAIN",
-    "DSS_CERTIFICATE_ADDITIONAL_NAMES",
-    "DSS_CERTIFICATE_VALIDATION",
-    "DSS_ZONE_NAME",
+    "DSS_GS_BUCKET_TEST_REGION",
+    "DSS_GS_CHECKOUT_BUCKET",
+    "DSS_GS_CHECKOUT_BUCKET_PROD",
+    "DSS_GS_CHECKOUT_BUCKET_STAGING",
+    "DSS_GS_CHECKOUT_BUCKET_TEST",
+    "DSS_S3_BUCKET",
+    "DSS_S3_BUCKET_INTEGRATION",
+    "DSS_S3_BUCKET_PROD",
+    "DSS_S3_BUCKET_STAGING",
+    "DSS_S3_BUCKET_TEST",
+    "DSS_S3_BUCKET_TEST_FIXTURES",
+    "DSS_S3_CHECKOUT_BUCKET",
+    "DSS_S3_CHECKOUT_BUCKET_INTEGRATION",
+    "DSS_S3_CHECKOUT_BUCKET_PROD",
+    "DSS_S3_CHECKOUT_BUCKET_STAGING",
+    "DSS_S3_CHECKOUT_BUCKET_TEST",
     "DSS_S3_CHECKOUT_BUCKET_UNWRITABLE",
+    "DSS_SECRETS_STORE",
+    "DSS_ZONE_NAME",
+    "ES_ALLOWED_SOURCE_IP_SECRETS_NAME",
+    "GCP_DEFAULT_REGION",
 ]
-
-terraform_variable_info = {'variable': dict()}
-for key in env_vars_to_infra:
-    terraform_variable_info['variable'][key] = {
-        'default': os.environ[key]
-    }
 
 with open(os.path.join(infra_root, args.component, "backend.tf"), "w") as fp:
     caller_info = boto3.client("sts").get_caller_identity()
@@ -102,7 +104,11 @@ with open(os.path.join(infra_root, args.component, "backend.tf"), "w") as fp:
     ))
 
 with open(os.path.join(infra_root, args.component, "variables.tf"), "w") as fp:
-    fp.write(json.dumps(terraform_variable_info, indent=2))
+    fp.write("# Auto-generated during infra build process." + os.linesep)
+    fp.write("# Please edit infra/build_deploy_config.py directly." + os.linesep)
+    for key in env_vars_to_infra:
+        val = os.environ[key]
+        fp.write(terraform_variable_template.format(name=key, val=val))
 
 with open(os.path.join(infra_root, args.component, "providers.tf"), "w") as fp:
     fp.write(terraform_providers_template.format(
