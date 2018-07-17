6fe9fa4144e8141788c8c22eca19bfe6325e6c95
Brian Hannafious <32105697+xbrianh@users.noreply.github.com>
2018-07-17 11:16:14 -0700

    TF scripting to deploy GCP service account (#1424)
    * TF scripting to deploy GCP service account

---
+++ infra/gcp_service_account/main.tf
@@ -0,0 +1,33 @@
+data "google_project" "project" {}
+
+resource "google_service_account" "dss" {
+  display_name = "${var.DSS_GCP_SERVICE_ACCOUNT_NAME}"
+  account_id = "${var.DSS_GCP_SERVICE_ACCOUNT_NAME}"
+}
+
+# Useful command to discover role names (Guessing based on console titles is difficult):
+# `gcloud iam list-grantable-roles //cloudresourcemanager.googleapis.com/projects/{project-id}`
+
+resource "google_project_iam_member" "serviceaccountactor" {
+  project = "${data.google_project.project.project_id}"
+  role    = "roles/iam.serviceAccountActor"
+  member  = "serviceAccount:${google_service_account.dss.email}"
+}
+
+resource "google_project_iam_member" "cloudruntimeconfiguratoradmin" {
+  project = "${data.google_project.project.project_id}"
+  role    = "roles/runtimeconfig.admin"
+  member  = "serviceAccount:${google_service_account.dss.email}"
+}
+
+resource "google_project_iam_member" "storageadmin" {
+  project = "${data.google_project.project.project_id}"
+  role    = "roles/storage.admin"
+  member  = "serviceAccount:${google_service_account.dss.email}"
+}
+
+resource "google_project_iam_member" "storageobjectadmin" {
+  project = "${data.google_project.project.project_id}"
+  role    = "roles/storage.objectAdmin"
+  member  = "serviceAccount:${google_service_account.dss.email}"
+}
