30ba1f81f389c248a8a1cbc4e18df4371f9e77c5
Brian Hannafious <32105697+xbrianh@users.noreply.github.com>
2018-08-06 12:36:37 -0700

    Check dcp-int status on GitLab (#1472)
    * Check dcp-int status on GitLab
    This allows us to check dcp-wide integration test status on GitLab.

--- scripts/status.sh
+++ scripts/status.sh
@@ -4,16 +4,17 @@ set -euo pipefail
 
 if [[ $# != 3 ]]; then
     echo "Given a owner, repo, and branch, output the combined check status."
+    echo "GITLAB_API and GITLAB_TOKEN are expected to be available in the environment."
     echo "Usage: $(basename $0) owner repo branch"
     echo "Example: $(basename $0) HumanCellAtlas dcp integration"
+    echo "Example: GITLAB_API=https://my_gitlab_domain/api/v4 $(basename $0) HumanCellAtlas dcp integration"
     exit 1
 fi
 
-GITHUB_API="https://api.github.com"
+owner=$1
+repo=$2
+branch=$3
+SLUG=$(python -c "import sys, urllib.parse; print(urllib.parse.quote_plus(sys.argv[1]))" "${owner}/${repo}")
 
-OWNER=$(python -c "import sys, urllib.parse; print(urllib.parse.quote_plus(sys.argv[1]))" "${1}")
-REPO=$(python -c "import sys, urllib.parse; print(urllib.parse.quote_plus(sys.argv[1]))" "${2}")
-BRANCH=$(python -c "import sys, urllib.parse; print(urllib.parse.quote_plus(sys.argv[1]))" "${3}")
-
-STATUS=$(http GET ${GITHUB_API}/repos/${OWNER}/${REPO}/commits/${BRANCH}/status Accept:application/vnd.github.full+json)
-echo "$STATUS" | jq -r .state
+STATUS=$(http GET ${GITLAB_API}/projects/${SLUG}/pipelines ref==${branch} "Private-Token: ${GITLAB_TOKEN}")
+echo ${STATUS} | jq -r .[0].status
