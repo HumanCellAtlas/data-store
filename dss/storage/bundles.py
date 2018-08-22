29a87537ca5c278248dcac93ebbb1ae7afa330a5
Calvin Nhieu <cnhieu@chanzuckerberg.com>
2018-08-21 17:24:01 -0700

    get_bundle only performs checkout when necessary (#1347)

--- dss/storage/bundles.py
+++ dss/storage/bundles.py
@@ -1,3 +1,4 @@
+from functools import lru_cache
 import json
 import typing
 
@@ -8,6 +9,7 @@ from dss.storage.identifiers import DSS_BUNDLE_KEY_REGEX, DSS_BUNDLE_TOMBSTONE_R
 from dss.storage.blobstore import test_object_exists
 
 
+@lru_cache(maxsize=32)
 def get_bundle_manifest(
         uuid: str,
         replica: Replica,
