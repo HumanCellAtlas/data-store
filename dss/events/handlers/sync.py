42f306807bf76100f2209a491d6f5951ce845406
Andrey Kislyuk <kislyuk@gmail.com>
2018-08-29 07:18:10 -0700

    [Easy] Remove dead GCSTS code (#1533)

--- dss/events/handlers/sync.py
+++ dss/events/handlers/sync.py
@@ -47,60 +47,6 @@ class GStorageTransferConnection(JSONConnection):
     API_VERSION = "v1"
     API_URL_TEMPLATE = "{api_base_url}/{api_version}{path}"
 
-
-# TODO akislyuk: access keys used here should be separate role credentials with need-based access
-# TODO akislyuk: schedule a lambda to check the status of the job, get it permissions to execute:
-#                storagetransfer.transferJobs().get(jobName=gsts_job["name"]).execute()
-def sync_s3_to_gcsts(project_id, s3_bucket_name, gs_bucket_name, source_key):
-    gsts_client = GStorageTransferClient()
-    gsts_conn = GStorageTransferConnection(client=gsts_client)
-    now = datetime.datetime.utcnow()
-    schedule_at = now + datetime.timedelta(minutes=gsts_sched_delay_minutes)
-    schedule_struct = dict(year=schedule_at.year, month=schedule_at.month, day=schedule_at.day)
-    gsts_job_def = {
-        "description": "hca-dss-{}-{}".format(int(now.timestamp()), hashlib.md5(source_key.encode()).hexdigest()),
-        "status": "ENABLED",
-        "projectId": project_id,
-        "schedule": {
-            "scheduleStartDate": schedule_struct,
-            "scheduleEndDate": schedule_struct,
-            "startTimeOfDay": dict(hours=schedule_at.hour, minutes=schedule_at.minute)
-        },
-        "transferSpec": {
-            "awsS3DataSource": {
-                "bucketName": s3_bucket_name,
-                "awsAccessKey": {
-                    "accessKeyId": botocore.session.get_session().get_credentials().access_key,
-                    "secretAccessKey": botocore.session.get_session().get_credentials().secret_key
-                }
-            },
-            "gcsDataSink": {
-                "bucketName": gs_bucket_name
-            },
-            "transferOptions": {
-                "overwriteObjectsAlreadyExistingInSink": False,
-                "deleteObjectsUniqueInSink": False,
-                "deleteObjectsFromSourceAfterTransfer": False,
-            },
-            "objectConditions": {
-                "includePrefixes": [source_key]
-            }
-        }
-    }
-    try:
-        gsts_job = gsts_conn.api_request("POST", "/transferJobs", data=gsts_job_def)
-        logger.info(gsts_job)
-    except Exception as e:
-        logger.error(f"FIXME: (akislyuk) GSTS job submission failed: {e}")
-    # FIXME akislyuk: the service account doesn't have permission to look at the
-    # status of the job, even though it has permission to create it.  I
-    # couldn't figure out what permission scope to give the principal in the
-    # IAM console, and the service definition at
-    # https://storagetransfer.googleapis.com/$discovery/rest?version=v1
-    # doesn't tell me either.
-    # gsts_job = gsts_conn.api_request("GET", "/" + gsts_job["name"])
-
-
 def sync_s3_to_gs_oneshot(source, dest):
     s3_blob_url = clients.s3.generate_presigned_url(
         ClientMethod='get_object',
@@ -164,9 +110,7 @@ def sync_blob(source_platform, source_key, dest_platform, context):
     else:
         raise NotImplementedError()
 
-    if source_platform == "s3" and dest_platform == "gs" and use_gsts:
-        sync_s3_to_gcsts(gs.project, source.bucket.name, dest.bucket.name, source_key)
-    elif source_platform == "s3" and dest_platform == "gs":
+    if source_platform == "s3" and dest_platform == "gs":
         if dest.blob.exists():
             logger.info(f"Key {source_key} already exists in GS")
             return
