5885371f1841afde09241ae11a7302db76e73016
DailyDreaming <lblauvel@ucsc.edu>
2018-11-27 15:20:06 -0800

    Proper script.

--- scripts/generate_swagger.py
+++ scripts/generate_swagger.py
@@ -1,4 +1,5 @@
 from collections import defaultdict
+import argparse
 import sys
 import os
 
@@ -7,12 +8,18 @@ sys.path.insert(0, pkg_root)  # noqa
 
 
 class SecureSwagger(object):
-    def __init__(self, config=None):
+    def __init__(self, infile=None, outfile=None, config=None):
+        """
+
+        :param infile:
+        :param outfile:
+        :param config:
+        """
         self.path_section = False
         self.call_section = None
-        self.infile = os.path.join(pkg_root, 'swagger_template')
-        self.outfile = os.path.join(pkg_root, 'dss-api.yml')
-        self.config = config if config else os.path.join(pkg_root, 'swagger_template')
+        self.infile = infile if infile else os.path.join(pkg_root, 'swagger_template')
+        self.outfile = outfile if outfile else os.path.join(pkg_root, 'dss-api.yml')
+        self.config = config if config else os.path.join(pkg_root, 'security.config')
         self.security_endpoints = self.security_from_config()
 
     def security_from_config(self):
@@ -35,24 +42,24 @@ class SecureSwagger(object):
 
     def insert_security_flag(self, line):
         """Checks the lines of a swagger/yaml file and determines if a security flag should be written in."""
-        # if not indented at all, we're in a new section, so reset
+        # If not indented at all, we're in a new section, so reset.
         if not line.startswith(' ') and self.path_section and line.strip() != '':
             self.path_section = False
 
-        # check if we're in the paths section
+        # Check if we're in the paths section.
         if line.startswith('paths:'):
             self.path_section = True
-        # check if we're in an api path section
+        # Check if we're in an api path section.
         elif line.startswith('  /'):
             self.call_section = None
             for api_path in self.security_endpoints:
-                # make sure it's one of the specified api paths, otherwise ignore
+                # Make sure it's one of the specified api paths, otherwise ignore.
                 if line.startswith(f'  {api_path}:'):
                     self.call_section = self.security_endpoints[api_path]
-        # if properly indented and we're in the correct (2) sections, this will be a call request
+        # If properly indented and we're in the correct (2) sections, this will be a call request.
         elif line.startswith('    ') and not line.startswith('     ') and self.path_section and self.call_section:
             for call in self.call_section:
-                # verify it's one of the specified calls we need to secure
+                # Verify it's one of the specified calls we need to secure.
                 if line.startswith(f'    {call}:'):
                     return True
         return False
@@ -61,9 +68,32 @@ class SecureSwagger(object):
         with open(self.outfile, 'w') as w:
             with open(self.infile, 'r') as f:
                 for line in f:
+
+                    if line.startswith('      security:') or line.startswith('        - dcpAuth: []'):
+                        # Should this raise or ignore these lines? Raise seems better here to avoid weird use-cases.
+                        raise RuntimeError('Invalid swagger template used.  File should not have security flags.')
+
                     w.write(line)
                     if self.insert_security_flag(line):
                         w.write('      security:\n')
                         w.write('        - dcpAuth: []\n')
 
-# SecureSwagger('/home/quokka/config_auth_dss/data-store/security.config').generate_swagger_with_secure_endpoints()
+
+def main(argv=sys.argv[1:]):
+    parser = argparse.ArgumentParser(description='Swagger Security Endpoint')
+    parser.add_argument('-i', '--input_swagger_template', dest="input_swagger_template", default=None,
+                        help='An input swagger template devoid of any security flags (to be filled in by this '
+                             'script from the configuration file).')
+    parser.add_argument('-o', '--output_swagger_yaml', dest="output_swagger_yaml", default=None,
+                        help='The name/path of the swagger output yaml file.')
+    parser.add_argument('-s', '--security_config', dest="security_config", default=None,
+                        help='A list of api requests that will be flagged for auth in the generated swagger '
+                             'yaml file.')
+    o = parser.parse_args(argv)
+
+    s = SecureSwagger(o.input_swagger_template, o.output_swagger_yaml, o.security_config)
+    s.generate_swagger_with_secure_endpoints()
+
+
+if __name__ == '__main__':
+    main(sys.argv[1:])
