c0a5c29bbbd4287344d3a0679006f76cb537e850
Lon Blauvelt <lblauvel@ucsc.edu>
2018-12-05 11:27:21 -0800

    Addressing comments except new tests.

--- tests/test_modify_swagger_auth.py
+++ tests/test_modify_swagger_auth.py
@@ -4,6 +4,7 @@ import os
 import sys
 import json
 import unittest
+from shutil import copyfile
 
 pkg_root = os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))  # noqa
 sys.path.insert(0, pkg_root)  # noqa
@@ -12,11 +13,18 @@ from scripts.swagger_auth import SecureSwagger
 
 
 class TestSecureSwagger(unittest.TestCase):
+    """Tests scripts/swagger_auth.py, that it parses and makes the new swagger file correctly."""
     def setUp(self):
+        # user test configs
         self.secure_auth = os.path.join(pkg_root, 'auth.secure_all.json')
-        self.hca_default_auth = os.path.join(pkg_root, 'auth.default.json')
-        self.swagger_path = os.path.join(pkg_root, 'dss-api.yml')
+        self.default_auth = os.path.join(pkg_root, 'auth.default.json')
 
+        # faulty test configs
+        self.duplicates_auth = os.path.join(pkg_root, 'tests/auth.duplicates.json')
+        self.nonexistentpaths_auth = os.path.join(pkg_root, 'tests/auth.nonexistentpaths.json')
+        self.empty_auth = os.path.join(pkg_root, 'tests/auth.empty.json')
+
+        self.swagger_path = os.path.join(pkg_root, 'dss-api.yml')
         # store the current swagger contents since we'll be mutating them in the tests
         with open(self.swagger_path, 'r') as f:
             self.original_contents = f.readlines()
@@ -26,6 +34,19 @@ class TestSecureSwagger(unittest.TestCase):
         with open(self.swagger_path, 'w') as f:
             for line in self.original_contents:
                 f.write(line)
+
+    def empty_config(self):
+        """An empty config should leave all endpoints open without auth."""
+        empty_config = self.set_and_return_current_config(self.empty_auth)
+        with open(self.secure_auth) as f:
+            expected_secure_config = json.loads(f.read())
+        assert empty_config == expected_secure_config
+
+    def config_with_duplicates(self):
+        pass
+
+    def config_with_nonexistent_paths(self):
+        pass
 
     def test_auth_can_be_determined_from_swagger(self):
         """
@@ -38,8 +59,8 @@ class TestSecureSwagger(unittest.TestCase):
         assert secure_config == expected_secure_config
 
         # do the same for the hca defaults
-        hca_config = self.set_and_return_current_config(self.hca_default_auth)
-        with open(self.hca_default_auth) as f:
+        hca_config = self.set_and_return_current_config(self.default_auth)
+        with open(self.default_auth) as f:
             expected_hca_config = json.loads(f.read())
         assert hca_config == expected_hca_config
 
@@ -54,7 +75,7 @@ class TestSecureSwagger(unittest.TestCase):
             secure_swagger_contents = f.readlines()
 
         # change swagger to the hca defaults
-        self.set_and_return_current_config(self.hca_default_auth)
+        self.set_and_return_current_config(self.default_auth)
         with open(self.swagger_path, 'r') as f:
             hca_default_swagger_contents = f.readlines()
 
@@ -64,7 +85,7 @@ class TestSecureSwagger(unittest.TestCase):
             assert secure_swagger_contents == f.readlines()
 
         # change back to the hca defaults and make sure it's the same file
-        self.set_and_return_current_config(self.hca_default_auth)
+        self.set_and_return_current_config(self.default_auth)
         with open(self.swagger_path, 'r') as f:
             assert hca_default_swagger_contents == f.readlines()
 
@@ -80,9 +101,10 @@ class TestSecureSwagger(unittest.TestCase):
             endpoints_from_config_file = json.loads(f.read())
         assert endpoints_from_swagger_file == endpoints_from_config_file
 
-    @staticmethod
-    def set_and_return_current_config(config_file):
-        SecureSwagger(config=config_file).make_swagger_from_authconfig()
+    def set_and_return_current_config(self, config_file):
+        SecureSwagger(infile=self.swagger_path,
+                      outfile=self.swagger_path,
+                      config=config_file).make_swagger_from_authconfig()
         return SecureSwagger().get_authconfig_from_swagger()
 
 
