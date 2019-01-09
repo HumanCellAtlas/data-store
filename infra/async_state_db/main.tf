2aa21abdae4594ec4265af44a918ef9b5de3fafa
Brian Hannafious <32105697+xbrianh@users.noreply.github.com>
2019-01-09 08:56:48 -0800

    Switch to on-demand capacity for async state db (#1827)
    This improves scaling

--- infra/async_state_db/main.tf
+++ infra/async_state_db/main.tf
@@ -1,13 +1,13 @@
 resource "aws_dynamodb_table" "sfn_state" {
-  name           = "dss-async-state-${var.DSS_DEPLOYMENT_STAGE}"
-  read_capacity  = 20
-  write_capacity = 20
-  hash_key       = "key"
+  name         = "dss-async-state-${var.DSS_DEPLOYMENT_STAGE}"
+  billing_mode = "PAY_PER_REQUEST"
+  hash_key     = "key"
 
   attribute {
     name = "key"
     type = "S"
   }
+
   tags {
     CreatedBy = "Terraform"
     Application = "DSS"
