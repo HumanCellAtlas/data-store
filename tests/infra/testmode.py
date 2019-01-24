83dcda9d4998327ef55f550de22ec73a501e74f9
Brian Hannafious <bhannafi@ucsc.edu>
2019-01-24 08:32:47 -0800

    remove always testmode

--- tests/infra/testmode.py
+++ tests/infra/testmode.py
@@ -3,24 +3,12 @@ import unittest
 
 
 def standalone(f):
-    return unittest.skipUnless(is_standalone(), "Skipping standalone test")(f)
-
-
-def is_standalone():
-    return "standalone" in _test_mode()
+    return unittest.skipUnless(
+        "standalone" in os.environ.get('DSS_TEST_MODE', "standalone"),
+        "Skipping standalone test")(f)
 
 
 def integration(f):
-    return unittest.skipUnless(is_integration(), "Skipping integration test")(f)
-
-
-def is_integration():
-    return "integration" in _test_mode()
-
-
-def always(f):
-    return f
-
-
-def _test_mode():
-    return os.environ.get('DSS_TEST_MODE', "standalone")
+    return unittest.skipUnless(
+        "integration" in os.environ.get('DSS_TEST_MODE', "integration"),
+        "Skipping integration test")(f)
