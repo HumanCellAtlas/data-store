0ecaa70960da51c1d4ab3c3e6c93a58ff7f851c7
amar jandu <ajandu@ucsc.edu>
2019-02-19 08:49:33 -0800

    simplified testing

--- dss/api/health.py
+++ dss/api/health.py
@@ -15,53 +15,54 @@ from googleapiclient import discovery
 from dss.util.aws.clients import dynamodb  # type: ignore
 
 
-def l2_health_checks(*args, **kwargs):
+def _get_es_status():
+    es_status = False
+    es_cleint = ElasticsearchClient().get()
+    es_res = es_cleint.cluster.health()
+    if es_res['status'] == 'green':
+        es_status = True
+    return es_status, es_res
 
-    def _get_es_status():
-        es_status = False
-        es_cleint = ElasticsearchClient().get()
-        es_res = es_cleint.cluster.health()
-        if es_res['status'] == 'green':
-            es_status = True
-        return es_status, es_res
 
-    def _get_dynamodb_status():
-        db_status = True
-        stage = os.environ['DSS_DEPLOYMENT_STAGE']
-        ddb_tables = ['dss-async-state-{}'.format(stage), 'dss-subscriptions-v2-aws-{}'.format(stage),
-                      'dss-subscriptions-v2-gcp-{}'.format(stage)]
-        ddb_table_data = dict.fromkeys(ddb_tables)
-        for table in ddb_tables:
-            try:
-                table_res = dynamodb.describe_table(TableName=table)['Table']
-                if table_res['TableStatus'] != 'ACTIVE':
-                    db_status = False
-                    ddb_table_data[table] = table_res.Table['TableStatus']
-            except errorfactory.BaseClientExceptions.ClientError:
-                db_status = False
-                ddb_table_data[table] = 'Error'
-        return db_status, ddb_table_data
-
-    def _get_event_relay_status():
-        er_status = False
+def _get_dynamodb_status():
+    db_status = True
+    stage = os.environ['DSS_DEPLOYMENT_STAGE']
+    ddb_tables = ['dss-async-state-{}'.format(stage), 'dss-subscriptions-v2-aws-{}'.format(stage),
+                  'dss-subscriptions-v2-gcp-{}'.format(stage)]
+    ddb_table_data = dict.fromkeys(ddb_tables)
+    for table in ddb_tables:
         try:
-            er_name = "projects/{}/locations/{}/functions/{}".format(
-                os.environ["GCP_PROJECT_NAME"], os.environ["GCP_DEFAULT_REGION"], os.environ["DSS_EVENT_RELAY_NAME"])
-            service = discovery.build('cloudfunctions', 'v1', cache_discovery=False)
-            er_res = service.projects().locations().functions().get(name=er_name).execute()
-            if er_res['status'] == 'ACTIVE':
-                er_status = True
-                er_data = er_res
-        except Exception:
-            er_status = False
-            er_data = "Error"
-        return er_status, er_data
+            table_res = dynamodb.describe_table(TableName=table)['Table']
+            if table_res['TableStatus'] != 'ACTIVE':
+                db_status = False
+                ddb_table_data[table] = table_res.Table['TableStatus']
+        except errorfactory.BaseClientExceptions.ClientError:
+            db_status = False
+            ddb_table_data[table] = 'Error'
+    return db_status, ddb_table_data
 
+
+def _get_event_relay_status():
+    er_status = False
+    try:
+        er_name = "projects/{}/locations/{}/functions/{}".format(
+            os.environ["GCP_PROJECT_NAME"], os.environ["GCP_DEFAULT_REGION"], os.environ["DSS_EVENT_RELAY_NAME"])
+        service = discovery.build('cloudfunctions', 'v1', cache_discovery=False)
+        er_res = service.projects().locations().functions().get(name=er_name).execute()
+        if er_res['status'] == 'ACTIVE':
+            er_status = True
+            er_data = er_res
+    except Exception:
+        er_status = False
+        er_data = "Error"
+    return er_status, er_data
+
+
+def l2_health_checks():
     health_status = {'Healthy': True}
-    # status_functions allow overriding // additional checks
-    status_functions = {"elasticSearch": kwargs.get('get_es_status', _get_es_status),
-                        "dynamoDB": kwargs.get("get_dynamodb_status", _get_dynamodb_status),
-                        "eventRelay": kwargs.get("get_er_status", _get_event_relay_status)}
+    status_functions = {"elasticSearch": _get_es_status,
+                        "dynamoDB": _get_dynamodb_status,
+                        "eventRelay": _get_event_relay_status}
     for key, functions in status_functions.items():
         try:
             status, payload = functions()
--- tests/test_health.py
+++ tests/test_health.py
@@ -10,30 +10,6 @@ from dss.api import health
 from tests.infra import testmode
 
 
-def _es_invalid():
-    return False, {"es": "error"}
-
-
-def _es_valid():
-    return True, None
-
-
-def _er_invalid():
-    return False, {"er": "Error"}
-
-
-def _er_valid():
-    return True, None
-
-
-def _dynamodb_invalid():
-    return False, {"ddb": "error"}
-
-
-def _dynamodb_valid():
-    return True, None
-
-
 class TestHealth(unittest.TestCase):
     @classmethod
     def setUpClass(cls):
@@ -45,32 +21,22 @@ class TestHealth(unittest.TestCase):
 
     @testmode.standalone
     def test_elastic_search(self):
-        test_es = json.loads(health.l2_health_checks(get_es_status=_es_valid))
-        self.assertNotIn("elasticSearch", test_es.keys())
-
-        test_es = json.loads(health.l2_health_checks(get_es_status=_es_invalid))
-        self.assertIn("elasticSearch", test_es.keys())
+        test_es = health._get_es_status()
+        self.assertIn(True, test_es)
 
     @testmode.standalone
     def test_dynamodb(self):
-        test_ddb = json.loads(health.l2_health_checks(get_dynamodb_status=_dynamodb_valid))
-        self.assertNotIn("dynamoDB", test_ddb.keys())
-
-        test_ddb = json.loads(health.l2_health_checks(get_dynamodb_status=_dynamodb_invalid))
-        self.assertIn("dynamoDB", test_ddb.keys())
+        test_ddb = health._get_dynamodb_status()
+        self.assertIn(True, test_ddb)
 
     @testmode.standalone
     def test_event_relay(self):
-        test_er = json.loads(health.l2_health_checks(get_er_status=_er_valid))
-        self.assertNotIn("eventRelay", test_er.keys())
-
-        test_er = json.loads(health.l2_health_checks(get_er_status=_er_invalid))
-        self.assertIn("eventRelay", test_er.keys())
+        test_er = health._get_event_relay_status()
+        self.assertIn(True, test_er)
 
     @testmode.standalone
     def test_healthy(self):
-        test_health = json.loads(health.l2_health_checks(get_es_status=_es_valid, get_dynamodb_status=_dynamodb_valid,
-                                                         get_er_status=_er_valid))
+        test_health = json.loads(health.l2_health_checks())
         self.assertDictEqual({"Healthy": True}, test_health)
 
 
