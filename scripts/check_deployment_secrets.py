19c706c6cd731e63649fb4a1082e6356203ac2a4
DailyDreaming <lblauvel@ucsc.edu>
2019-03-11 15:43:28 -0700

    Only check secrets if an official stage is set.

--- scripts/check_deployment_secrets.py
+++ scripts/check_deployment_secrets.py
@@ -8,19 +8,11 @@ import os
 import json
 
 
-stage = os.environ['DSS_DEPLOYMENT_STAGE']
-
-dev_auth_uri = 'https://auth.data.humancellatlas.org/authorize'
-dev_token_uri = 'https://auth.data.humancellatlas.org/oauth/token'
-dev_email = 'travis-test@human-cell-atlas-travis-test.iam.gserviceaccount.com'
-
-integration_auth_uri = 'https://auth.dev.data.humancellatlas.org/authorize'
-integration_token_uri = 'https://auth.data.humancellatlas.org/oauth/token'
-integration_email = 'org-humancellatlas-integration@human-cell-atlas-travis-test.iam.gserviceaccount.com'
-
-staging_auth_uri = 'https://auth.dev.data.humancellatlas.org/authorize'
-staging_token_uri = 'https://auth.data.humancellatlas.org/oauth/token'
-staging_email = 'org-humancellatlas-staging@human-cell-atlas-travis-test.iam.gserviceaccount.com'
+auth_uri = ['https://auth.data.humancellatlas.org/authorize', 'https://auth.dev.data.humancellatlas.org/authorize']
+token_uri = ['https://auth.data.humancellatlas.org/oauth/token','https://auth.dev.data.humancellatlas.org/oauth/token']
+dev_email = ['travis-test@human-cell-atlas-travis-test.iam.gserviceaccount.com']
+integration_email = ['org-humancellatlas-integration@human-cell-atlas-travis-test.iam.gserviceaccount.com']
+staging_email = ['org-humancellatlas-staging@human-cell-atlas-travis-test.iam.gserviceaccount.com']
 
 
 def fetch_secret(secret_name='application_secrets.json'):
@@ -31,27 +23,38 @@ def fetch_secret(secret_name='application_secrets.json'):
     return secret
 
 
-def main():
-    app_secret = fetch_secret('application_secrets.json')
-    gcp_cred_secret = fetch_secret('gcp-credentials.json')
+def check(current, expected, stage, secret):
+    if current not in expected:
+        raise ValueError(f'\n\nDeploying to {stage.upper()} could not be completed, because it looks like an AWS secret'
+                         f' has an unexpected value.  Please do not change AWS secrets for releases.\n'
+                         f'The following secret                : {secret}\n'
+                         f'Had the unexpected setting          : {current}\n'
+                         f'When one of these items was expected: {expected}\n')
+
+
+def main(stage=None):
+    if not stage:
+        stage = os.environ['DSS_DEPLOYMENT_STAGE']
+
+    app_secret_name = 'application_secrets.json'
+    gcp_cred_secret_name = 'gcp-credentials.json'
+    app_secret = fetch_secret(app_secret_name)
+    gcp_cred_secret = fetch_secret(gcp_cred_secret_name)
 
     # shared checks
-    assert gcp_cred_secret['type'] == 'service_account'
-    assert gcp_cred_secret['project_id'] == 'human-cell-atlas-travis-test'
+    if stage in ('dev', 'integration', 'staging'):
+        check(app_secret['installed']['auth_uri'], auth_uri, stage=stage, secret=app_secret_name)
+        check(app_secret['installed']['token_uri'], token_uri, stage=stage, secret=app_secret_name)
+        check(gcp_cred_secret['type'], ['service_account'], stage=stage, secret=gcp_cred_secret_name)
+        check(gcp_cred_secret['project_id'], ['human-cell-atlas-travis-test'], stage=stage, secret=app_secret_name)
 
     # stage-specific checks
     if stage == 'dev':
-        assert app_secret['installed']['auth_uri'] == dev_auth_uri
-        assert app_secret['installed']['token_uri'] == dev_token_uri
-        assert gcp_cred_secret['client_email'] == dev_email
+        check(gcp_cred_secret['client_email'], dev_email, stage=stage, secret=gcp_cred_secret_name)
     elif stage == 'integration':
-        assert app_secret['installed']['auth_uri'] == integration_auth_uri
-        assert app_secret['installed']['token_uri'] == integration_token_uri
-        assert gcp_cred_secret['client_email'] == integration_email
+        check(gcp_cred_secret['client_email'], integration_email, stage=stage, secret=gcp_cred_secret_name)
     elif stage == 'staging':
-        assert app_secret['installed']['auth_uri'] == staging_auth_uri
-        assert app_secret['installed']['token_uri'] == staging_token_uri
-        assert gcp_cred_secret['client_email'] == staging_email
+        check(gcp_cred_secret['client_email'], staging_email, stage=stage, secret=gcp_cred_secret_name)
 
 
 if __name__ == '__main__':
