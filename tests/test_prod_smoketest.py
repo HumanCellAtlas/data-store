70718c147a104e02f737dc194685c22342985d8c
amar jandu <ajandu@ucsc.edu>
2019-04-11 12:20:51 -0700

    working prod-tests

--- tests/test_prod_smoketest.py
+++ tests/test_prod_smoketest.py
@@ -41,48 +41,13 @@ def ENDC():
 
 @testmode.integration
 class ProdSmoketest(BaseSmokeTest):
-    params = [
-        {
-            'starting_replica': Replica.aws,
-            'checkout_bucket': os.environ['DSS_S3_CHECKOUT_BUCKET'],
-            'test_bucket': os.environ['DSS_S3_BUCKET_TEST']
-        },
-        {
-            'starting_replica': Replica.gcp,
-            'checkout_bucket': os.environ['DSS_GS_CHECKOUT_BUCKET'],
-            'test_bucket': os.environ['DSS_GS_BUCKET_TEST']
-        }
-    ]
-    notification_bucket = os.environ['DSS_S3_BUCKET_TEST']
+    params = BaseSmokeTest.params
 
     @classmethod
     def setUpClass(cls):
-        cls.replicas = {param['starting_replica'] for param in cls.params}
-        if os.path.exists("dcp-cli"):
-            run("git pull --recurse-submodules", cwd="dcp-cli")
-        else:
-            run("git clone --depth 1 --recurse-submodules https://github.com/HumanCellAtlas/dcp-cli")
-        cls.workdir = tempfile.TemporaryDirectory(dir=os.getcwd(), prefix="smoketest-", suffix=".tmp")
-        if not args.clean:
-            # Disable workdir destructor
-            cls.workdir._finalizer.detach()  # type: ignore
-
-        # Create a virtualenv and install the CLI
-        #
-        venv = os.path.join(cls.workdir.name, "venv")
-        run(f"virtualenv -p {sys.executable} {venv}")
-        cls.venv_bin = os.path.join(venv, "bin", "")
-        run(f"{cls.venv_bin}pip install --upgrade .", cwd="dcp-cli")
-
-        # Configure the CLI
-        #
-        cli_config = {"DSSClient": {"swagger_url": f"https://{os.environ['API_DOMAIN_NAME']}/v1/swagger.json"}}
-        cli_config_filename = f"{cls.workdir.name}/cli_config.json"
-        with open(cli_config_filename, "w") as fh2:
-            fh2.write(json.dumps(cli_config))
-        os.environ["HCA_CONFIG_FILE"] = f"{cls.workdir.name}/cli_config.json"
+        super().setUpClass()
+        cls.url = "https://www.example.com"
         cls.query = {"query": {"bool": {"must": [{"term": {"admin_deleted": "true"}}]}}}
-        cls.url = 'https://www.example.com'
 
     @classmethod
     def tearDownClass(cls):
@@ -91,73 +56,6 @@ class ProdSmoketest(BaseSmokeTest):
         else:
             print(f"Leaving temporary working directory at {cls.workdir}.", file=sys.stderr)
 
-    def _test_query_es(self, starting_replica):
-        es_res = run_for_json(f'{self.venv_bin}hca dss post-search  --es-query {{}} --replica aws')
-        bundle_fqid = es_res['results'][0]['bundle_fqid']
-        return bundle_fqid.split('.')[0], bundle_fqid.split('.')[1]
-
-    def _test_checkout_start(self, starting_replica, bundle_uuid):
-        res = run_for_json(f"{self.venv_bin}hca dss post-bundles-checkout --uuid {bundle_uuid} "
-                           f"--replica {starting_replica.name}")
-        checkout_job_id = res['checkout_job_id']
-        print(f"Checkout jobId: {checkout_job_id}")
-        self.assertTrue(checkout_job_id)
-        return checkout_job_id
-
-    def _test_get_bundle(self, starting_replica, bundle_uuid):
-        download_res = run_for_json(f"{self.venv_bin}hca dss get-bundle --replica {starting_replica.name}"
-                                    f" --uuid {bundle_uuid}")
-        prod_bundle_file_count = len(download_res['bundle']['files'])
-        self.assertEqual(bundle_uuid, download_res['bundle']['uuid'])
-        return prod_bundle_file_count
-
-    def _test_checkout(self, starting_replica, bundle_uuid, bundle_version,
-                       checkout_job_id, checkout_bucket, file_count):
-        for i in range(10):
-            res = run_for_json(f"{self.venv_bin}hca dss get-bundles-checkout --checkout-job-id {checkout_job_id} "
-                               f"--replica {starting_replica.name}")
-            status = res['status']
-            self.assertGreater(len(status), 0)
-            if status == 'RUNNING':
-                time.sleep(6)
-            else:
-                self.assertEqual(status, 'SUCCEEDED')
-                blob_handle = self.get_blobstore(starting_replica)
-                object_key = get_dst_bundle_prefix(bundle_uuid, bundle_version)
-                print(f"Checking bucket {checkout_bucket} "
-                      f"object key: {object_key}")
-                files = list(blob_handle.list(checkout_bucket, object_key))
-                self.assertEqual(len(files), file_count)
-                break
-        else:
-            self.fail("Timed out waiting for checkout job to succeed")
-
-    def _test_subscription_create(self, starting_replica):
-        url = 'https://www.example.com'
-        query = {"query": {"bool": {"must": [{"term": {"admin_deleted": "true"}}]}}}
-        print(json.dumps(query))
-        put_response = run_for_json([f'{self.venv_bin}hca', 'dss', 'put-subscription',
-                                     '--callback-url', url,
-                                     '--method', 'PUT',
-                                     '--es-query', json.dumps(query),
-                                     '--replica', starting_replica.name])
-        subscription_id = put_response['uuid']
-        return subscription_id
-
-    def _test_subscription_fetch(self, starting_replica, subscription_id):
-        get_response = run_for_json(f"{self.venv_bin}hca dss get-subscription "
-                                    f"--replica {starting_replica.name} "
-                                    f"--uuid {subscription_id} "
-                                    "--subscription-type elasticsearch")
-        self.assertEquals(subscription_id, get_response['uuid'])
-
-    def _test_subscription_delete(self, starting_replica, subscription_id):
-        delete_res = run_for_json(f"{self.venv_bin}hca dss delete-subscription --replica {starting_replica.name} "
-                                  f"--uuid {subscription_id} "
-                                  "--subscription-type elasticsearch")
-        self.assertIn('timeDeleted', delete_res.keys())
-
-
     def prod_smokeTest(self):
         os.chdir(self.workdir.name)
         for param in self.params:
@@ -165,51 +63,32 @@ class ProdSmoketest(BaseSmokeTest):
             checkout_bucket = param['checkout_bucket']
 
             query_res = BaseSmokeTest.post_search_es(self, replica, "{}")
-            bundle_uuid = query_res['results'][0]['bundle_fqid'].split(':')[0]
-            bundle_version = query_res['results'][0]['bundle_fqid'].split(':')[1]
+            bundle_uuid = query_res['results'][0]['bundle_fqid'].split('.')[0]
+            bundle_version = query_res['results'][0]['bundle_fqid'].split('.')[1]
+
             checkout_id = BaseSmokeTest.checkout_initiate(self, replica, bundle_uuid)
             self.assertTrue(checkout_id)
-            bundle_file_count = BaseSmokeTest.get_bundle(self, replica, bundle_uuid)
+
+            bundle_res = BaseSmokeTest.get_bundle(self, replica, bundle_uuid)
+            bundle_file_count = len(bundle_res['bundle']['files'])
 
             subscription_id = BaseSmokeTest.subscription_create_es(self, replica, self.query, self.url)
-            BaseSmokeTest._test_subscription_get_es(self, replica, subscription_id, )
+            BaseSmokeTest._test_subscription_get_es(self, replica, subscription_id, self.url)
 
-            self._test_subscription_fetch(replica, subscription_id)
-            self._test_subscription_delete(replica, subscription_id)
-            self._test_checkout(replica, bundle_uuid, bundle_version, checkout_id, checkout_bucket, bundle_file_count)
+            BaseSmokeTest._test_subscription_get_es(self, replica, subscription_id, self.url)
+            BaseSmokeTest.subscription_delete(self, replica, subscription_id)
 
-    @classmethod
-    def test_prod_smoketest(cls):
-        os.chdir(cls.workdir.name)
-        for param in cls.params:
-            replica = param['starting_replica']
-            checkout_bucket = param['checkout_bucket']
+            BaseSmokeTest._test_checkout(self, replica, bundle_uuid, bundle_version, checkout_id,
+                                         checkout_bucket, bundle_file_count)
 
-            bundle_uuid, bundle_version = self._test_query_es(replica)
-            query_res = BaseSmokeTest.post_search_es(self, replica, "{}")
-            checkout_id = self._test_checkout_start(replica, bundle_uuid)
-            bundle_file_count = self._test_get_bundle(replica, bundle_uuid)
-
-            query = {"query": {"bool": {"must": [{"term": {"admin_deleted": "true"}}]}}}
-            subscription_id = BaseSmokeTest.subscription_create_es(self, replica, query, url='https://www.example.com')
-            self._test_subscription_fetch(replica, subscription_id)
-            self._test_subscription_delete(replica, subscription_id)
-            self._test_checkout(replica, bundle_uuid, bundle_version, checkout_id, checkout_bucket, bundle_file_count)
-
-    def get_blobstore(self, replica: Replica) -> BlobStore:
-        if replica is Replica.aws:
-            return replica.blobstore_class.from_environment()
-        elif replica is Replica.gcp:
-            return replica.blobstore_class.from_auth_credentials(os.environ['GOOGLE_APPLICATION_CREDENTIALS'])
-        else:
-            raise NameError("Replica not found")
+    def test_prod_smoketest(self):
+        self.prod_smokeTest()
 
 
 if __name__ == "__main__":
     if os.environ.get("DSS_DEPLOYMENT_STAGE") is not "prod":
         print("prod_smoketest is not applicable to stage: {}".format(os.environ.get("DSS_DEPLOYMENT_STAGE")))
-    # TODO
-    # exit(0)
-    # else:
+        exit(0)
+    else:
         args, sys.argv[1:] = parser.parse_known_args()
         unittest.main()
