7a65072646110f57c1f94abcbe5cab3772bb85de
amar jandu <ajandu@ucsc.edu>
2019-05-09 09:33:06 -0700

    have terraforming, going to backtrack

--- scripts/generage_requirements_layer.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-# TODO make this description not suck
-# This script is used to generate a requirements layer for chalice to use
-
-pip install --install-option="--prefix=$PREFIX_PATH" package_name
