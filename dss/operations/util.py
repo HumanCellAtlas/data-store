5e80bc8a457662ecfee82b2e2fe9b5d564bc0b15
Brian Hannafious <32105697+xbrianh@users.noreply.github.com>
2019-05-08 21:39:02 -0700

    Operations utils (#2115)

---
+++ dss/operations/util.py
@@ -0,0 +1,59 @@
+import os
+import traceback
+import typing
+from uuid import uuid4
+from contextlib import AbstractContextManager
+
+from cloud_blobstore import BlobStore
+
+from dss.util.aws.clients import sqs, sts  # type: ignore
+from concurrent.futures import ThreadPoolExecutor, as_completed
+
+
+_account_id = sts.get_caller_identity()['Account']
+_queue_url = "https://sqs.{}.amazonaws.com/{}/dss-operations-{}".format(os.environ['AWS_DEFAULT_REGION'],
+                                                                        _account_id,
+                                                                        os.environ['DSS_DEPLOYMENT_STAGE'])
+
+
+class CommandForwarder(AbstractContextManager):
+    """
+    Context manager for forwarding commands for Lambda execution through batch SQS queuing
+    """
+    def __init__(self):
+        self.chunk = list()
+
+    def forward(self, msg: str):
+        self.chunk.append(msg)
+        if 10 == len(self.chunk):
+            _enqueue_command_batch(self.chunk)
+            self.chunk = list()
+
+    def __exit__(self, *args, **kwargs):
+        if len(self.chunk):
+            _enqueue_command_batch(self.chunk)
+
+def map_bucket(func: typing.Callable, handle: BlobStore, bucket: str, base_pfx: str, parallelization=16):
+    """
+    Call `func` on an iterable of keys
+    func is expected to be thread safe.
+    """
+    with ThreadPoolExecutor(max_workers=parallelization) as e:
+        futures = list()
+        for pfx in "0123456789abcdef":
+            f = e.submit(func, handle.list(bucket, prefix=f"{base_pfx}{pfx}"))
+            futures.append(f)
+        for f in as_completed(futures):
+            try:
+                yield f.result()
+            except Exception:
+                traceback.print_exc()
+
+def _enqueue_command_batch(commands: typing.List[str]):
+    assert 10 >= len(commands)
+    resp = sqs.send_message_batch(
+        QueueUrl=_queue_url,
+        Entries=[dict(Id=str(uuid4()), MessageBody=command)
+                 for command in commands]
+    )
+    return resp
--- tests/test_operations.py
+++ tests/test_operations.py
@@ -5,6 +5,7 @@ import os
 import sys
 import uuid
 import unittest
+from unittest import mock
 
 pkg_root = os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))  # noqa
 sys.path.insert(0, pkg_root)  # noqa
@@ -12,7 +13,9 @@ sys.path.insert(0, pkg_root)  # noqa
 import dss
 from tests.infra import testmode
 from dss.operations import DSSOperationsCommandDispatch
+from dss.operations.util import map_bucket, CommandForwarder
 from dss.logging import configure_test_logging
+from dss.config import BucketConfig, Config, Replica, override_bucket_config
 
 def setUpModule():
     configure_test_logging()
@@ -54,6 +57,44 @@ class TestOperations(unittest.TestCase):
                 self.assertEqual(args.foo, 24)
 
         dispatch(["my_target", "my_action", "24", "--argument-b", "LSDKFJ"])
+
+    def test_map_bucket(self):
+        with override_bucket_config(BucketConfig.TEST_FIXTURE):
+            for replica in Replica:
+                with self.subTest(replica=replica):
+                    handle = Config.get_blobstore_handle(replica)
+
+                    count_list = 0
+                    for key in handle.list(replica.bucket, prefix="bundles/"):
+                        count_list += 1
+
+                    def counter(keys):
+                        count = 0
+                        for key in keys:
+                            count += 1
+                        return count
+
+                    total = 0
+                    for count in map_bucket(counter, handle, replica.bucket, "bundles/", 2):
+                        total += count
+
+                    self.assertGreater(count_list, 0)
+                    self.assertEqual(count_list, total)
+
+    def test_command_forwarder(self):
+        batches = list()
+
+        def mock_enqueue(commands):
+            batches.append(commands)
+
+        with mock.patch("dss.operations.util._enqueue_command_batch", mock_enqueue):
+            with CommandForwarder() as f:
+                for i in range(21):
+                    f.forward(str(i))
+
+        self.assertEqual(batches[0], [str(i) for i in range(10)])
+        self.assertEqual(batches[1], [str(i) for i in range(10, 20)])
+        self.assertEqual(batches[2], [str(i) for i in range(20, 21)])
 
 
 if __name__ == '__main__':
