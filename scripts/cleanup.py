beb5fe4802890afbaf244132c6108ed4e5dceeb6
Trent Smith <1429913+Bento007@users.noreply.github.com>
2019-05-09 17:38:23 -0700

    Fixing cleanup script (#102)

--- scripts/cleanup.py
+++ scripts/cleanup.py
@@ -1,34 +1,40 @@
-import boto3
-from fusillade.clouddirectory import cleanup_directory, cleanup_schema
+#!/usr/bin/env python
 """
 This script is used to clean up test directories and schemas from aws clouddirectory
 """
+import os
+import sys
 
-client = boto3.client("clouddirectory")
+pkg_root = os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))  # noqa
+sys.path.insert(0, pkg_root)  # noqa
 
-response = client.list_directories(
-    MaxResults=30,
-    state='ENABLED'
-)
+from fusillade.clouddirectory import cleanup_directory, cleanup_schema, cd_client
 
-for directory in response['Directories']:
-    if 'test' in directory['Name']:
-        cleanup_directory['DirectoryArn']
 
-directories = [ i['Name'] for i in client.list_directories(
-    MaxResults=30,
-    state='ENABLED'
-)['Directories']]
-print('DIRECTORIES:', directories)
+if __name__ == "__main__":
+    for response in cd_client.get_paginator('list_directories').paginate(MaxResults=30, state='ENABLED'):
+        for directory in response['Directories']:
+            if 'test' in directory['Name']:
+                cleanup_directory(directory['DirectoryArn'])
 
-response = client.list_published_schema_arns(
-    MaxResults = 20
-)
-for schema in response['SchemaArns']:
-    if "authz/T" in schema:
-        cleanup_schema(schema)
+    directories = [
+        i['Name'] for i in cd_client.list_directories(
+            MaxResults=30,
+            state='ENABLED'
+        )['Directories']
+    ]
+    print('DIRECTORIES:')
+    for i in directories:
+        print('\t', i)
 
-response = client.list_published_schema_arns(
-    MaxResults = 20
-)['SchemaArns']
-print('Schemas:',response)
+    for response in cd_client.get_paginator('list_published_schema_arns').paginate(MaxResults=30):
+        for schema in response['SchemaArns']:
+            if "authz/T" in schema:
+                cleanup_schema(schema)
+
+    schemas = cd_client.list_published_schema_arns(
+        MaxResults=30
+    )['SchemaArns']
+    print('Schemas:')
+    for i in schemas:
+        print('\t', i)
