922d7ee4939fb1b4a482037586e14964c99d4930
Brian Hannafious <32105697+xbrianh@users.noreply.github.com>
2019-05-14 13:18:47 -0700

    Pull logging into copy clients (#2134)

--- daemons/dss-gs-copy-write-metadata-sfn/app.py
+++ daemons/dss-gs-copy-write-metadata-sfn/app.py
@@ -6,8 +6,11 @@ import domovoi
 pkg_root = os.path.abspath(os.path.join(os.path.dirname(__file__), 'domovoilib'))  # noqa
 sys.path.insert(0, pkg_root)  # noqa
 
+from dss.logging import configure_lambda_logging
 import dss.stepfunctions.gscopyclient as gscopyclient
 from dss.util import tracing
+
+configure_lambda_logging()
 
 app = domovoi.Domovoi()
 app.register_state_machine(gscopyclient.copy_write_metadata_sfn)
