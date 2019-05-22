527e6a5b2b3526f98b5de4a4bed4888a1a3c26ca
amar jandu <ajandu@ucsc.edu>
2019-05-22 09:46:09 -0700

    Lambda Tagging (#2150)

--- chalice/build_deploy_config.sh
+++ chalice/build_deploy_config.sh
@@ -24,7 +24,6 @@ if ! aws es describe-elasticsearch-domain --domain-name $dss_es_domain; then
 fi
 
 cat "$config_json" | jq ".stages.$stage.api_gateway_stage=env.stage" | sponge "$config_json"
-
 export lambda_arn=$(aws lambda list-functions | jq -r '.Functions[] | select(.FunctionName==env.lambda_name) | .FunctionArn')
 if [[ -z $lambda_arn ]]; then
     echo "Lambda function $lambda_name not found, resetting Chalice config"
@@ -42,8 +41,14 @@ else
            .$stage.lambda_functions = {}" > "$deployed_json"
 fi
 
+
 export DEPLOY_ORIGIN="$(whoami)-$(hostname)-$(git describe --tags --always)-$(date -u +'%Y-%m-%d-%H-%M-%S').deploy"
-cat "$config_json" | jq .stages.$stage.tags.DSS_DEPLOY_ORIGIN=env.DEPLOY_ORIGIN | sponge "$config_json"
+cat "$config_json" | jq ".stages.$stage.tags.DSS_DEPLOY_ORIGIN=\"$DEPLOY_ORIGIN\" | \
+	.stages.$stage.tags.Name=\"${DSS_INFRA_TAG_PROJECT}-${DSS_DEPLOYMENT_STAGE}-${DSS_INFRA_TAG_SERVICE}\" | \
+	.stages.$stage.tags.service=\"${DSS_INFRA_TAG_SERVICE}\"  | \
+	.stages.$stage.tags.project=\"$DSS_INFRA_TAG_PROJECT\" | \
+	.stages.$stage.tags.owner=\"${DSS_INFRA_TAG_OWNER}\" | \
+	.stages.$stage.tags.env=\"${DSS_DEPLOYMENT_STAGE}\""  | sponge "$config_json"
 
 env_json=$(aws ssm get-parameter --name /dcp/dss/${DSS_DEPLOYMENT_STAGE}/environment | jq -r .Parameter.Value)
 for var in $(echo $env_json | jq -r keys[]); do
