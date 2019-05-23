d922621f2025326ff600797cd8c62133119fd72e
amar jandu <ajandu@ucsc.edu>
2019-05-23 12:47:43 -0700

    working layer

--- scripts/generate_upload_requirements_layer.sh
+++ scripts/generate_upload_requirements_layer.sh
@@ -16,10 +16,10 @@ then
 fi
 
 build_path="$DSS_HOME/dependencies/python/lib/python3.6/site-packages"
-docker_download_path="/mnt/dependencies/python/lib/python3.6/site-packages"
 dependency_dir="$DSS_HOME/dependencies"
 aws_req_key=$DSS_DEPLOYMENT_STAGE/requirements.txt
 aws_zip_key=$DSS_DEPLOYMENT_STAGE/dss-dependencies-$DSS_DEPLOYMENT_STAGE.zip
+local_temp_chalice=${DSS_HOME}/temp_chalice
 local_req=$DSS_HOME/requirements.txt
 local_zip=$DSS_HOME/dss-dependencies-$DSS_DEPLOYMENT_STAGE.zip
 layer_name=dss-dependencies-${DSS_DEPLOYMENT_STAGE}
@@ -29,42 +29,9 @@ function create_layer(){
     echo "created layer/-version_arn: ${layer_version_arn}"
 }
 
-function check() {
-     aws_req_hash=$(aws s3api head-object --bucket ${populated_dss_tf_backend} --key ${aws_req_key} | jq -r .ETag | xxd -r -p | base64)
-     local_req_hash=$(openssl md5 -binary ${local_req} | base64)
-if [[ $aws_req_hash != $local_req_hash ]]; then
-    echo "checksum missmatch, uploading new ${aws_zip_key}"
-    upload
-else
-    echo "requirements have not changed, no need to update"
-    exit 0
-fi
-}
-
-function build_zip(){
-	# if this docker fails with `pull access denied` your credentials might be expired, perform a `docker logout`
-	docker pull humancellatlas/dss-lambda-layer
-	echo "downloading requirements to ${dependency_dir}"
-	# this command can get simplifed to remove `mv`, the issue is inconsistent `pip install -t` https://github.com/googleapis/google-cloud-python/issues/3806
-	docker run --rm -v "${DSS_HOME}":/mnt humancellatlas/dss-lambda-layer /bin/bash -c \
-	"cd /mnt/ ; python3 -m virtualenv tempvenv; source tempvenv/bin/activate; \
-	 python3 -m pip -q --no-cache-dir install -r requirements.txt; \
-	 mkdir -p ${docker_download_path}; mv tempvenv/lib/python3.6/site-packages/* ${docker_download_path}; \
-	 deactivate; \
-	 rm -rf tempvenv"
-	if [[ ! -d $dependency_dir ]]
-	then
-		echo "was not able to download directories from docker image"
-		exit 1
-	fi
-     # target looks like site-package
-    echo "compressing......"
-    cd ${dependency_dir}
-    zip -qq -r -o $local_zip ./
-    cd ..
-}
 
 function build_clean_zip(){
+	mkdir -p $build_path
 	unzip -q -o -d $build_path deployment.zip
 	cd $dependency_dir
 	zip -qq -r -o $local_zip ./
@@ -75,12 +42,13 @@ function build_chalice(){
 	# utilize challice to build out dependencies
 	# chalice requires a a configured chalice application to package.
 	# copy chalice folder TODO verify that in process this folder will be clean....
-	cp -R ${DSS_HOME}/chalice ${DSS_HOME}/temp_chalice
-	mkdir ${DSS_HOME}/temp_chalice/vendor
-	cp -R ${DSS_HOME}/vendor.in temp_chalice; ln -s ${DSS_HOME}/requirements.txt temp_chalice/requirements.txt;
-	cd temp_chalice
+	cp -R ${DSS_HOME}/chalice ${local_temp_chalice}
+	mkdir ${local_temp_chalice}/vendor
+	cp -R ${DSS_HOME}/vendor.in ${local_temp_chalice}; ln -s ${DSS_HOME}/requirements.txt ${local_temp_chalice}/requirements.txt;
+	cd ${local_temp_chalice}
 	shopt -s nullglob;for wheel in vendor.in/*/*.whl; do unzip -q -o -d vendor $wheel; done
 	chalice package .
+	echo "removing non-dependencies from zip"
 	zip -d deployment.zip chalicelib/\*
 	zip -d deployment.zip app.py
 }
@@ -96,24 +64,51 @@ function upload() {
     aws s3 cp $local_req s3://${populated_dss_tf_backend}/$aws_req_key
     create_layer
 }
+
 function clean (){
 	echo "deleting $1"
     rm -rf $1
+}
+
+function clean_all(){
+	clean $dependency_dir
+	clean $local_temp_chalice
+	clean $local_zip
+}
+
+function build_upload_clean(){
+	build_chalice
+    build_clean_zip
+    upload
+	clean_all
+}
+
+function check() {
+    aws_req_hash=$(aws s3api head-object --bucket ${populated_dss_tf_backend} --key ${aws_req_key} | jq -r .ETag | xxd -r -p | base64)
+    local_req_hash=$(openssl md5 -binary ${local_req} | base64)
+	if [[ $aws_req_hash != $local_req_hash ]]; then
+    	echo "checksum missmatch, uploading new ${aws_zip_key}"
+    	build_upload_clean
+	else
+    	echo "requirements have not changed, no need to update"
+    	exit 0
+	fi
 }
 
 optspec=":hbcd"
 while getopts "$optspec" optchar; do
     case "${optchar}" in
         b)
+        	clean_all
             build_chalice
             build_clean_zip
             echo "created: $local_zip"
-            # clean $dependency_dir
+            clean $dependency_dir
+            clean $local_temp_chalice
             exit 0
             ;;
        	c)
-       		clean $dependency_dir
-       		clean $local_zip
+			clean_all
        		exit 0
        		;;
        	d)
@@ -131,15 +126,11 @@ while getopts "$optspec" optchar; do
 esac
 done
 
-
-# if no arguments then
+# if no arguments then check and run
 if [[ $(aws s3api head-object --bucket ${populated_dss_tf_backend} --key $aws_zip_key &>/dev/null; echo $?) -eq 255 ]]
 then
     echo "Could not locate $aws_zip_key in aws, starting upload"
-    build_zip
-    upload
-   	clean $dependency_dir
-    clean $local_zip
+	build_upload_clean
     exit 0
 else
     echo "$aws_zip_key found in aws"
