74b216e20ba34156dcd2d1c17469296200dadb6e
Lon Blauvelt <lblauvel@ucsc.edu>
2019-05-31 17:52:49 -0700

    [easy] Use the same regex for all uuids. (#2185)

--- dss/storage/identifiers.py
+++ dss/storage/identifiers.py
@@ -10,7 +10,9 @@ COLLECTION_PREFIX = "collections"
 # versioned tombstones are indexed after all bundles during a reindex operation.
 TOMBSTONE_SUFFIX = "dead"
 
-UUID_PATTERN = "[0-9A-Fa-f]{8}-[0-9A-Fa-f]{4}-[0-9A-Fa-f]{4}-[0-9A-Fa-f]{4}-[0-9A-Fa-f]{12}"
+# does not allow caps
+# (though our swagger params allow users to input this, s3 keys are changed to lowercase after ingestion)
+UUID_PATTERN = "[a-z0-9]{8}-[a-z0-9]{4}-[a-z0-9]{4}-[a-z0-9]{4}-[a-z0-9]{12}"
 UUID_REGEX = re.compile(UUID_PATTERN)
 
 VERSION_PATTERN = "\d{4}-\d{2}-\d{2}T\d{2}\d{2}\d{2}[.]\d{6}Z"
