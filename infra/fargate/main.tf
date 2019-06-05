96c2943154897e8d7f54ab6a03108be5d21a98f7
amar jandu <ajandu@ucsc.edu>
2019-06-05 11:28:35 -0700

    updated from feedback

--- infra/fargate/main.tf
+++ infra/fargate/main.tf
@@ -1,4 +1,6 @@
 data "aws_caller_identity" "current" {}
+
+
 locals {
   common_tags = "${map(
     "managedBy" , "terraform",
@@ -10,7 +12,7 @@ locals {
   )}",
   availability_zones = "${split(" ", "${var.DSS_AVAILABILITY_ZONES}")}"
 }
-resource "aws_iam_role" "task_executor" {
+resource "aws_iam_role" "task-executor" {
   name = "dss-monitor-${var.DSS_DEPLOYMENT_STAGE}"
   assume_role_policy = <<EOF
 {
@@ -31,13 +33,14 @@ resource "aws_iam_role" "task_executor" {
 EOF
 }
 
-resource "aws_iam_role_policy_attachment" "task_executor_ecs" {
-  role = "${aws_iam_role.task_executor.name}"
+resource "aws_iam_role_policy_attachment" "task-executor_ecs" {
+  role = "${aws_iam_role.task-executor.name}"
   policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy"
 }
 
-resource "aws_iam_role" "query_runner" {
-  name = "dss-monitor-query-runner-${var.DSS_DEPLOYMENT_STAGE}"
+resource "aws_iam_role" "task-performer" {
+  name = "dss-monitor-task-performer-${var.DSS_DEPLOYMENT_STAGE}"
+  tags = "${local.common_tags}"
   assume_role_policy = <<EOF
 {
   "Version": "2012-10-17",
@@ -56,9 +59,9 @@ resource "aws_iam_role" "query_runner" {
 EOF
 }
 
-resource "aws_iam_role_policy" "query_runner" {
-  name = "dss-monitor-query-runner-${var.DSS_DEPLOYMENT_STAGE}"
-  role = "${aws_iam_role.query_runner.id}"
+resource "aws_iam_role_policy" "task-performer" {
+  name = "dss-monitor-task-performer-${var.DSS_DEPLOYMENT_STAGE}"
+  role = "${aws_iam_role.task-performer.id}"
   policy = <<EOF
 {
   "Version": "2012-10-17",
@@ -87,21 +90,19 @@ EOF
 
 resource "aws_ecs_task_definition" "monitor" {
   family = "dss-monitor-${var.DSS_DEPLOYMENT_STAGE}"
-  execution_role_arn = "${aws_iam_role.task_executor.arn}"
-  task_role_arn = "${aws_iam_role.query_runner.arn}"
+  execution_role_arn = "${aws_iam_role.task-executor.arn}"
+  task_role_arn = "${aws_iam_role.task-performer.arn}"
   requires_compatibilities = ["FARGATE"]
   network_mode = "awsvpc"
   cpu = "256"
   memory = "512"
-  # TODO add logging below.
+  # TODO add logging below. 
   container_definitions = <<DEFINITION
 [
   {
     "family": "dss-monitor",
     "name": "dss-monitor-lambda",
     "image": "humancellatlas/dss-monitor-image",
-    "cpu": 256,
-    "memory": 512,
     "essential": true,
     "logConfiguration": {
         "logDriver": "awslogs",
@@ -110,17 +111,11 @@ resource "aws_ecs_task_definition" "monitor" {
           "awslogs-region": "us-east-1",
           "awslogs-stream-prefix": "ecs"
         }
-    },
-    "portMappings": [
-      {
-        "containerPort": 80,
-        "hostPort": 80
-      }
-    ]
+    }
   }
 ]
 DEFINITION
-  # tags = "${local.common_tags}"
+  tags = "${local.common_tags}"
 }
 
 
@@ -130,63 +125,26 @@ resource "aws_cloudwatch_log_group" "query_runner" {
 }
 
 
-resource "aws_vpc" "dss_fargate" {
-  cidr_block = "10.0.0.0/16"
-  enable_dns_support = true
-  enable_dns_hostnames = true
-  tags = "${local.common_tags}"
+data "aws_vpc" "default" {
+  default = true
 }
 
-# These subnets need to be associated with a route table providing
-# internet access.
-resource "aws_subnet" "dss_monitor" {
-  count 		    = "${length(local.availability_zones)}"
-  vpc_id 			= "${aws_vpc.dss_fargate.id}"
-  availability_zone = "${local.availability_zones[count.index]}"
-  cidr_block = "${cidrsubnet("10.0.0.0/16", "4", count.index)}"
+data "aws_availability_zones" "available" {}
+
+data "aws_subnet" "default" {
+  count             = 3
+  vpc_id            = "${data.aws_vpc.default.id}"
+  availability_zone = "${data.aws_availability_zones.available.names[count.index]}"
+  default_for_az    = true
 }
 
-resource "aws_internet_gateway" "gw" {
-  vpc_id = "${aws_vpc.dss_fargate.id}"
-}
-
-# Route the public subnet trafic through the IGW
-resource "aws_route" "internet_access" {
-  route_table_id         = "${aws_vpc.dss_fargate.main_route_table_id}"
-  destination_cidr_block = "0.0.0.0/0"
-  gateway_id             = "${aws_internet_gateway.gw.id}"
-}
-
-
-
-resource "aws_ecs_cluster" "dss_monitor" {
-  name = "dss-monitor-${var.DSS_DEPLOYMENT_STAGE}"
-  # tags = "${local.common_tags}"
-}
-
-resource "aws_ecs_service" "notification-builder" {
-  name            = "dss-monitor-${var.DSS_DEPLOYMENT_STAGE}"
-  cluster         = "${aws_ecs_cluster.dss_monitor.id}"
-  task_definition = "${aws_ecs_task_definition.monitor.arn}"
-  desired_count   = 0
-  launch_type = "FARGATE"
-  # depends_on      = ["aws_subnet.dss-monitor"]
-  # tags = "${local.common_tags}"
-
-  lifecycle {
-    ignore_changes = ["desired_count"]
-  }
-
-  network_configuration {
-    # security_groups = ["${aws_vpc.vpc.default_security_group_id}"]
-    subnets         = ["${aws_subnet.dss_monitor.*.id}"]
-    assign_public_ip = true
-  }
+data "aws_ecs_cluster" "default"{
+  cluster_name = "default"
 }
 
 resource "aws_cloudwatch_event_rule" "dss-monitor" {
-  name = "dss-monitor-lambda-trigger"
-  schedule_expression = "cron(* 1 * * ? *)"
+  name = "dss-monitor-trigger"
+  schedule_expression = "cron(* 0 * * ? *)"
   description = "daily event trigger for dss-monitor notifications"
   tags = "${local.common_tags}"
 
@@ -194,8 +152,8 @@ resource "aws_cloudwatch_event_rule" "dss-monitor" {
 
 resource "aws_cloudwatch_event_target" "scheduled_task" {
   rule      = "${aws_cloudwatch_event_rule.dss-monitor.name}"
-  arn       = "${aws_ecs_cluster.dss_monitor.arn}"
-  role_arn = "${aws_iam_role.query_runner.arn}"
+  arn       = "${data.aws_ecs_cluster.default.arn}"
+  role_arn = "${aws_iam_role.task-performer.arn}"
   ecs_target = {
     task_count          = 1
     task_definition_arn = "${aws_ecs_task_definition.monitor.arn}"
@@ -204,7 +162,8 @@ resource "aws_cloudwatch_event_target" "scheduled_task" {
 
     network_configuration {
       assign_public_ip = true
-      subnets         = ["${aws_subnet.dss_monitor.*.id}"]
+      subnets         = ["${data.aws_subnet.default.*.id}"]
     }
   }
 }
+
