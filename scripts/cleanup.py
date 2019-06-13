be6a25da0b1980111fdfedd31a8dbd69aa4d4650
Trent Smith <1429913+Bento007@users.noreply.github.com>
2019-06-13 16:23:41 -0700

    update cleanup script to handle minor versions. (#190)

--- scripts/cleanup.py
+++ scripts/cleanup.py
@@ -27,10 +27,14 @@ if __name__ == "__main__":
     for i in directories:
         print('\t', i)
 
-    for response in cd_client.get_paginator('list_published_schema_arns').paginate(MaxResults=30):
-        for schema in response['SchemaArns']:
-            if "authz/T" in schema:
-                cleanup_schema(schema)
+    for response_0 in cd_client.get_paginator('list_published_schema_arns').paginate(MaxResults=30):
+        for schema_0 in response_0['SchemaArns']:
+            if "authz/T" in schema_0:
+                for response_1 in cd_client.get_paginator('list_published_schema_arns').paginate(
+                        SchemaArn=schema_0, MaxResults=30):
+                    for schema_1 in response_1['SchemaArns']:
+                        cleanup_schema(schema_1)
+
 
     schemas = cd_client.list_published_schema_arns(
         MaxResults=30
