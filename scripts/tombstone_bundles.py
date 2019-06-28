92845fd9e96bdd1457fd13b4af131478d23a71d8
Brian Hannafious <bhannafi@ucsc.edu>
2019-06-28 21:10:03 +0000

    use batching to increase reliability

--- scripts/tombstone_bundles.py
+++ scripts/tombstone_bundles.py
@@ -12,6 +12,8 @@ Usage:
 """
 import sys
 import time
+import json
+from random import randint
 from traceback import format_exc
 from concurrent.futures import ThreadPoolExecutor, as_completed
 
@@ -23,15 +25,20 @@ tombstone_reason = ""
 assert stage
 assert tombstone_reason
 
-if stage == "prod":
-    dss_client = DSSClient(swagger_url="https://dss.data.humancellatlas.org/v1/swagger.json")
-else:
-    dss_client = DSSClient(swagger_url=f"https://dss.{stage}.data.humancellatlas.org/v1/swagger.json")
+BATCH_SIZE = 1000
+
+def _get_dss_client():
+    if stage == "prod":
+        dss_client = DSSClient(swagger_url="https://dss.data.humancellatlas.org/v1/swagger.json")
+    else:
+        dss_client = DSSClient(swagger_url=f"https://dss.{stage}.data.humancellatlas.org/v1/swagger.json")
+    return dss_client
+dss_client = _get_dss_client()
 
 def tombstone_bundle(fqid):
     uuid, version = fqid.split(".", 1)
-    bundle_found = _bundle_found(uuid, version)
-    bundle_already_tombstoned = _bundle_tombstoned(uuid, version)
+    bundle_found = bundle_is_found(uuid, version)
+    bundle_already_tombstoned = bundle_is_tombstoned(uuid, version)
     if bundle_found and not bundle_already_tombstoned:
         resp = dss_client.delete_bundle(
             replica="aws",
@@ -39,16 +46,16 @@ def tombstone_bundle(fqid):
             version=version,
             reason=tombstone_reason,
         )
-        for _ in range(30):
-            if _bundle_tombstoned(uuid, version):
+        for _ in range(20):
+            if bundle_is_tombstoned(uuid, version):
                 break
             else:
-                time.sleep(1)
+                time.sleep(randint(1,3))
         else:
             raise Exception(f"Unable to verity tombstone {uuid}")
     return bundle_found, bundle_already_tombstoned, fqid
 
-def _bundle_found(uuid, version):
+def bundle_is_found(uuid, version):
     try:
         resp = dss_client.get_bundle(replica="aws", uuid=uuid, version=version)
         return True
@@ -58,7 +65,7 @@ def _bundle_found(uuid, version):
         else:
             raise
 
-def _bundle_tombstoned(uuid, version):
+def bundle_is_tombstoned(uuid, version):
     bundle_query_with_version = {"query":{"bool":{"must":[{"match":{"uuid":uuid}},{"match":{"version":version}},]}}}
     resp = dss_client.post_search(replica="aws", es_query=bundle_query_with_version, output_format="raw")
     if 0 == resp['total_hits']:
@@ -68,27 +75,34 @@ def _bundle_tombstoned(uuid, version):
     else:
         return False
 
-bundles_to_tombstone = [
-    # bundles fqid list
-]
+def bundle_status_string(bundle_found, bundle_already_tombstoned):
+    if bundle_already_tombstoned:
+        return "ALLREADY_TOMBSTONED"
+    elif not bundle_found:
+        return "NOT_FOUND"
+    else:
+        return "TOMBSTONED"
 
-with ThreadPoolExecutor(max_workers=10) as e:
-    futures = list()
-    for fqid in bundles_to_tombstone:
-        futures.append(e.submit(tombstone_bundle, fqid))
-    for f in as_completed(futures):
-        try:
-            bundle_found, bundle_already_tombstoned, fqid = f.result()
-            if bundle_already_tombstoned:
-                bundle_status = "ALLREADY_TOMBSTONED"            
-            elif not bundle_found:
-                bundle_status = "NOT_FOUND"
-            else:
-                bundle_status = "TOMBSTONED"            
-            print(bundle_status, fqid)
-        except SwaggerAPIException as e:
-            if 403 == e.code: # bail out on the first credentials denial
-                print(e)
-                sys.exit(1)
-        except Exception as e:
-            sys.stderr.write(format_exc())
+if __name__ == "__main__":
+    bundles_to_tombstone = [
+        # bundles fqid list
+    ]
+    
+    for i in range(0, len(bundles_to_tombstone), BATCH_SIZE):
+        dss_client = _get_dss_client()
+        fqids_batch = bundles_to_tombstone[i:i+BATCH_SIZE]
+        with ThreadPoolExecutor(max_workers=20) as e:
+            futures = [e.submit(tombstone_bundle, fqid) for fqid in fqids_batch]
+            for f in as_completed(futures):
+                try:
+                    bundle_found, bundle_already_tombstoned, fqid = f.result()
+                except SwaggerAPIException as e:
+                    if 403 == e.code: # bail out on the first credentials denial
+                        sys.stderr.write(format_exc())
+                        sys.exit(1)
+                except Exception:
+                    sys.stderr.write(format_exc())
+                else:
+                    status_string = bundle_status_string(bundle_found, bundle_already_tombstoned)
+                    print(status_string, fqid)
+                    sys.stdout.flush()
