71d2ec9388c7b3c85ffa6c3f104601ea3eaad9f2
Lon Blauvelt <lblauvel@ucsc.edu>
2019-07-10 17:05:09 -0700

    Adjust smoketest to wait a bit longer when downloading after a sync. (#2243)

--- tests/infra/base_smoketest.py
+++ tests/infra/base_smoketest.py
@@ -198,7 +198,15 @@ class BaseSmokeTest(unittest.TestCase):
                 else:
                     parser.exit(RED(f"Failed to replicate bundle from {current_replica.name} to {replica.name}"))
             with self.subTest(f"{current_replica.name}: Download bundle from {replica}"):
-                run(f"{self.venv_bin}hca dss download --replica {replica.name} --bundle-uuid {bundle_uuid}")
+                for i in range(3):
+                    try:
+                        run(f"{self.venv_bin}hca dss download --replica {replica.name} --bundle-uuid {bundle_uuid}")
+                        break
+                    except:  # noqa
+                        print(f'Waiting for {replica.name} bundle uuid: {bundle_uuid}')
+                        time.sleep(1)
+                else:
+                    run(f"{self.venv_bin}hca dss download --replica {replica.name} --bundle-uuid {bundle_uuid}")
 
     def generate_presigned_url(self, bucket, key):
         s3 = boto3.client('s3', config=botocore.client.Config(signature_version='s3v4'))
