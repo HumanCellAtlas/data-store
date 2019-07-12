4e5c31fcfd68008734575cecc727c8c9190d663d
Brian Hannafious <bhannafi@ucsc.edu>
2019-07-12 17:35:23 +0000

    linting

--- dss/operations/events.py
+++ dss/operations/events.py
@@ -9,15 +9,13 @@ from dss.config import Replica
 from dss.operations import dispatch
 from dss.events.handlers.notify_v2 import build_bundle_metadata_document
 
-events = dispatch.target("events",
-    help=__doc__
-)
+events = dispatch.target("events", help=__doc__)
 
 @events.action("bundle-metadata-document",
                arguments={"--replica": dict(choices=[r.name for r in Replica], required=True),
                           "--keys": dict(default=None,
-                                    nargs="*",
-                                    help="bundle keys to generate documents.")})
+                                         nargs="*",
+                                         help="bundle keys to generate documents.")})
 def bundle_metadata_document(argv: typing.List[str], args: argparse.Namespace):
     for key in args.keys:
         md = build_bundle_metadata_document(Replica[args.replica], key)
