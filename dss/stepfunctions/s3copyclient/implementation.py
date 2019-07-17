93670243e44a7b82b933e7fcb78e1abe6f4142d2
Lon Blauvelt <lblauvel@ucsc.edu>
2019-07-16 17:22:17 -0700

    Verify content type. (#2258)

--- dss/stepfunctions/s3copyclient/implementation.py
+++ dss/stepfunctions/s3copyclient/implementation.py
@@ -63,7 +63,10 @@ def setup_copy_task(event, lambda_context):
     if part_count * part_size < source_size:
         part_count += 1
     if part_count > 1:
-        mpu = s3_blobstore.s3_client.create_multipart_upload(Bucket=destination_bucket, Key=destination_key)
+        mpu = s3_blobstore.s3_client.create_multipart_upload(
+            Bucket=destination_bucket,
+            Key=destination_key,
+            ContentType=blobinfo['ContentType'])
         event[_Key.UPLOAD_ID] = mpu['UploadId']
         event[Key.FINISHED] = False
     else:
