fa3b6012b0c9154c2bc774276c84bc62fbc4a86f
Brian Hannafious <bhannafi@ucsc.edu>
2019-07-18 15:45:08 +0000

    improve output

--- dss/operations/events.py
+++ dss/operations/events.py
@@ -1,15 +1,23 @@
 """
-JMESPath Eventing/subscription admin tooling
+JMESPath eventing and subscription admin tooling
 """
 import typing
 import argparse
 import json
+import logging
+
+from cloud_blobstore import BlobNotFoundError
 
 from dss.config import Replica
 from dss.operations import dispatch
 from dss.events.handlers.notify_v2 import build_bundle_metadata_document
 
+
+logger = logging.getLogger(__name__)
+
+
 events = dispatch.target("events", help=__doc__)
+
 
 @events.action("bundle-metadata-document",
                arguments={"--replica": dict(choices=[r.name for r in Replica], required=True),
@@ -18,5 +26,11 @@ events = dispatch.target("events", help=__doc__)
                                          help="bundle keys to generate documents.")})
 def bundle_metadata_document(argv: typing.List[str], args: argparse.Namespace):
     for key in args.keys:
-        md = build_bundle_metadata_document(Replica[args.replica], key)
-        print(json.dumps(md))
+        if not key.startswith("bundles/"):
+            logger.warning(f"Unable to produce bundle metadata document for '{key}'")
+            continue
+        try:
+            md = build_bundle_metadata_document(Replica[args.replica], key)
+            print(json.dumps(md))
+        except BlobNotFoundError:
+            logger.error(f"Bundle not found for '{key}'")
