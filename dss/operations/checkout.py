af3091375186c191387b09bb91bc29bb16aaf98f
amar jandu <ajandu@ucsc.edu>
2019-07-22 14:33:01 -0700

    need to add functionality to remove blobs

--- dss/operations/checkout.py
+++ dss/operations/checkout.py
@@ -9,6 +9,9 @@ from dss import Replica
 from dss.operations import dispatch
 from dss import Config
 from cloud_blobstore import BlobNotFoundError
+from dss.api.bundles import get_bundle_manifest
+from dss.storage.hcablobstore import compose_blob_key
+from dss.storage.hcablobstore import compose_blob_key
 
 
 logger = logging.getLogger(__name__)
@@ -27,7 +30,8 @@ def verify_delete(handler, bucket, key):
 
 @checkout.action("remove_checkout",
                  arguments={"--replica": dict(choices=[r.name for r in Replica], required=True),
-                            "--keys": dict(nargs="+", help="keys from checkout bucket to remove", required=True)})
+                            "--keys": dict(nargs="+", help="keys from checkout bucket to remove", required=True),
+                            "--bundle-fqid": dict(nargs="+", help="fqid checkout bucket to remove", required=False)})
 def remove_checkout(argv: typing.List[str], args: argparse.Namespace):
     """
     Remove a bundle from the checkout bucket
@@ -35,16 +39,29 @@ def remove_checkout(argv: typing.List[str], args: argparse.Namespace):
     replica = Replica[args.replica]
     handler = Config.get_blobstore_handle(replica)
     bucket = replica.checkout_bucket
-    for _key in args.keys:
-        if 'bundles/' in _key:
-            for key in handler.list(bucket, _key):
+    if args.keys:
+        for _key in args.keys:
+            if 'bundles/' in _key:
+                print(handler.list(bucket, _key))
+                for key in handler.list(bucket, _key):
+                    try:
+                        verify_delete(handler, bucket, key)
+                    except BlobNotFoundError:
+                        continue
+            else:
+                # should handle other keys, files/blobs
                 try:
                     verify_delete(handler, bucket, key)
                 except BlobNotFoundError:
                     continue
-        else:
-            # should handle other keys, files/blobs
-            try:
-                verify_delete(handler, bucket, key)
-            except BlobNotFoundError:
+    elif args.bundle_fqid:
+        for _fqid in args.bundle_fqid:
+            uuid, version = _fqid.split('.', 1)
+            manifest = get_bundle_manifest(replica=replica, uuid=uuid, version=version)
+            if manifest is None:
+                logger.warning(f"unable to locate manifest for fqid: {'fqid'}")
                 continue
+            for _files in manifest['files']:
+                key = compose_blob_key(_files)
+                logger.info(f'attempting removal of file: {_files["uuid"]}:{_files["version"]}')
+                verify_delete(handler,bucket,key)
