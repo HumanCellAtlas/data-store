4797c015bc3f8228c340d00e755b32c7449c4150
Brian Hannafious <32105697+xbrianh@users.noreply.github.com>
2019-07-24 13:52:49 -0700

    Operations for managing the ES index and subscriptions (#2269)

--- dss/operations/elasticsearch.py
+++ dss/operations/elasticsearch.py
@@ -6,11 +6,18 @@ import json
 import typing
 import argparse
 import logging
+import functools
+from concurrent.futures import ThreadPoolExecutor, as_completed
+from threading import Lock
 
+import boto3
+from elasticsearch_dsl import Search
 from dcplib.aws.sqs import SQSMessenger, get_queue_url
 
 from dss import Replica
 from dss.operations import dispatch
+from dss.api.subscriptions_v1 import _delete_subscription
+from dss.index.es import ElasticsearchClient
 
 
 logger = logging.getLogger(__name__)
@@ -30,3 +37,87 @@ def index(argv: typing.List[str], args: argparse.Namespace):
     with SQSMessenger(index_queue_url) as sqsm:
         for key in args.keys:
             sqsm.send(json.dumps(dict(replica=args.replica, key=key)))
+
+@elasticsearch.action("list-subscriptions", arguments={"--owner": dict(type=str)})
+def list_subscriptions(argv: typing.List[str], args: argparse.Namespace):
+    """
+    List ES subscriptions, optionally filtered by owner
+    """
+    es = get_es_client()
+    for hit in get(es, doc_type="subscription"):
+        if args.owner is None or hit['owner'] == args.owner:
+            print(hit.meta.id, hit['owner'])
+
+@elasticsearch.action("get-subscriptions", arguments={"--uuids": dict(required=True, nargs="*")})
+def get_subscriptions(argv: typing.List[str], args: argparse.Namespace):
+    """
+    For each uuid, output a subscription and associated percolate queries.
+    """
+    es = get_es_client()
+    lock = Lock()
+
+    def _get(uuid):
+        for hit in get_by_id(es, uuid):
+            with lock:
+                print(hit.meta.to_dict(), hit.to_dict())
+
+    with ThreadPoolExecutor(max_workers=2) as e:
+        for uuid in args.uuids:
+            e.submit(_get, uuid)
+
+@elasticsearch.action("delete-subscriptions", arguments={"--uuids": dict(required=True, nargs="*")})
+def delete_subscriptions(argv: typing.List[str], args: argparse.Namespace):
+    """
+    For each uuid, delete a subscription and associated percolate queries
+    """
+    es = get_es_client()
+    lock = Lock()
+
+    def _delete(uuid):
+        count = es.search(index="_all",
+                          doc_type="subscription",
+                          body={"query": {"terms": {"_id": [uuid]}}})['hits']['total']
+        if count > 0:
+            _delete_subscription(es, uuid)
+            with lock:
+                print(f"Removed subscription {uuid}")
+        else:
+            with lock:
+                print(f"No subscriptions found for {uuid}")
+
+    with ThreadPoolExecutor(max_workers=2) as e:
+        futures = [e.submit(_delete, uuid) for uuid in args.uuids]
+        for f in as_completed(futures):
+            f.result()
+
+@elasticsearch.action("get-by-id", arguments={"--ids": dict(required=True, nargs="*")})
+def get_doc_by_id(argv: typing.List[str], args: argparse.Namespace):
+    es = get_es_client()
+    for id_ in args.ids:
+        for hit in get_by_id(es, id_):
+            print(hit.to_dict())
+
+@functools.lru_cache()
+def get_es_client():
+    domain_name = "dss-index-" + os.environ['DSS_DEPLOYMENT_STAGE']
+    host = boto3.client("es").describe_elasticsearch_domain(DomainName=domain_name)['DomainStatus']['Endpoint']
+    os.environ['DSS_ES_ENDPOINT'] = host
+    return ElasticsearchClient.get()
+
+def get_by_id(es, doc_id, doc_type=None):
+    kwargs = dict(using=es, index="_all")
+    if doc_type is not None:
+        kwargs['doc_type'] = doc_type
+    for hit in Search(**kwargs).query({"terms": {"_id": [doc_id]}}).scan():
+        yield hit
+
+def get(es, index="_all", doc_type=None, owner=None):
+    kwargs = dict(using=es, index=index)
+    if doc_type is not None:
+        kwargs['doc_type'] = doc_type
+    if owner is not None:
+        search = Search(**kwargs).query({'bool': {'must': [{'term': {'owner': owner}}]}})
+    else:
+        search = Search(**kwargs)
+    for hit in search.scan():
+        yield hit
