42d50b44f8200f532e62ecc20ef3350dc626d74d
amar jandu <ajandu@ucsc.edu>
2019-08-05 16:27:49 -0700

    added lons review comments

--- dss/operations/checkout.py
+++ dss/operations/checkout.py
@@ -4,6 +4,7 @@ Tools for managing checkout buckets
 import typing
 import argparse
 import logging
+import os
 
 from dss import Replica
 from dss.operations import dispatch
@@ -11,8 +12,7 @@ from dss import Config
 from cloud_blobstore import BlobNotFoundError
 from dss.api.bundles import get_bundle_manifest
 from dss.storage.hcablobstore import compose_blob_key
-from dss.storage.hcablobstore import compose_blob_key
-
+from dss.storage.identifiers import BUNDLE_PREFIX
 
 logger = logging.getLogger(__name__)
 
@@ -20,12 +20,15 @@ checkout = dispatch.target("checkout", help=__doc__)
 
 
 def verify_delete(handler, bucket, key):
-    logger.warning(f'attempting removal of key: {key}')
-    handler.delete(bucket, key)
+    logger.warning(f'attempting removal of key: {bucket}/{key}')
     try:
+        handler.delete(bucket=bucket, key=key)
         handler.get(bucket=bucket, key=key)
     except BlobNotFoundError:
-        logger.warning(f'Success! unable to locate key {key} in bucket: {bucket} ')
+        logger.warning(f'Success! unable to locate key {bucket}/{key} ')
+    else:
+        logger.warning(f'Unable to delete key: {bucket}/{key}')
+        exit(1)
 
 
 @checkout.action("remove_checkout",
@@ -39,31 +42,26 @@ def remove_checkout(argv: typing.List[str], args: argparse.Namespace):
     replica = Replica[args.replica]
     handler = Config.get_blobstore_handle(replica)
     bucket = replica.checkout_bucket
+    stage = os.getenv('DSS_DEPLOYMENT_STAGE')
+    logger.warning(f'Remove Checkout Started for stage: {stage}, bucket: {bucket}')
     if args.keys:
         for _key in args.keys:
-            if 'bundles/' in _key:
+            if BUNDLE_PREFIX in _key:
                 # get-bundle presignedurl uses key based prefix
-                print(handler.list(bucket, _key))
                 for key in handler.list(bucket, _key):
-                    try:
-                        verify_delete(handler, bucket, key)
-                    except BlobNotFoundError:
-                        continue
+                    verify_delete(handler, bucket, key)
             else:
                 # should handle other keys, files/blobs
-                try:
                     verify_delete(handler, bucket, key)
-                except BlobNotFoundError:
-                    continue
     elif args.bundle_fqid:
         # get-bundle that has a manifest context does is under /blob prefix
         for _fqid in args.bundle_fqid:
             uuid, version = _fqid.split('.', 1)
             manifest = get_bundle_manifest(replica=replica, uuid=uuid, version=version)
             if manifest is None:
-                logger.warning(f"unable to locate manifest for fqid: {'fqid'}")
+                logger.warning(f"unable to locate manifest for fqid: {_fqid}")
                 continue
             for _files in manifest['files']:
                 key = compose_blob_key(_files)
-                logger.info(f'attempting removal of key: {key}')
+                logger.info(f'attempting removal of key: {bucket}/{key}')
                 verify_delete(handler, bucket, key)
