6be4e5792b7586722c98cd2dd565cddfc1acb404
amar jandu <ajandu@ucsc.edu>
2019-08-05 13:24:39 -0700

    Tagging Updates: Name (#2322)

--- infra/async_state_db/main.tf
+++ infra/async_state_db/main.tf
@@ -3,7 +3,7 @@ data "aws_caller_identity" "current" {}
 locals {
   common_tags = "${map(
     "managedBy" , "terraform",
-    "Name"      , "${var.DSS_INFRA_TAG_PROJECT}-${var.DSS_DEPLOYMENT_STAGE}-${var.DSS_INFRA_TAG_SERVICE}",
+    "Name"      , "${var.DSS_INFRA_TAG_SERVICE}-asyncdynamodb",
     "project"   , "${var.DSS_INFRA_TAG_PROJECT}",
     "env"       , "${var.DSS_DEPLOYMENT_STAGE}",
     "service"   , "${var.DSS_INFRA_TAG_SERVICE}",
