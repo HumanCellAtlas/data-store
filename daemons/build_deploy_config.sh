0d42cc493cee7b0d36f542404d6927c3ecbc98ba
amar jandu <ajandu@ucsc.edu>
2019-08-05 07:33:31 -0700

    modifed name tag to represent subcomponent of the dss

--- daemons/build_deploy_config.sh
+++ daemons/build_deploy_config.sh
@@ -37,7 +37,7 @@ cat "$config_json" | jq ".stages.$stage.layers=[env.layer_version_arn]" | sponge
 
 export DEPLOY_ORIGIN="$(whoami)-$(hostname)-$(git describe --tags --always)-$(date -u +'%Y-%m-%d-%H-%M-%S').deploy"
 cat "$config_json" | jq ".stages.$stage.tags.DSS_DEPLOY_ORIGIN=\"$DEPLOY_ORIGIN\" | \
-	.stages.$stage.tags.Name=\"${DSS_INFRA_TAG_PROJECT}-${DSS_DEPLOYMENT_STAGE}-${DSS_INFRA_TAG_SERVICE}\" | \
+	.stages.$stage.tags.Name=\"${DSS_INFRA_TAG_SERVICE}-$lambda_name-${DSS_DEPLOYMENT_STAGE}\" | \
 	.stages.$stage.tags.service=\"${DSS_INFRA_TAG_SERVICE}\"  | \
 	.stages.$stage.tags.project=\"$DSS_INFRA_TAG_PROJECT\" | \
 	.stages.$stage.tags.owner=\"${DSS_INFRA_TAG_OWNER}\" | \
