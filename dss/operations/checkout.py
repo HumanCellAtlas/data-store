73d62a26efb5f7aca4b4c1a5e97593b55235d55f
amar jandu <ajandu@ucsc.edu>
2019-08-06 11:59:43 -0700

    moar fixes

--- dss/operations/checkout.py
+++ dss/operations/checkout.py
@@ -19,16 +19,15 @@ logger = logging.getLogger(__name__)
 checkout = dispatch.target("checkout", help=__doc__)
 
 
-def verify_delete(handler, bucket, key):
+def verify_delete(handle, bucket, key):
     logger.warning(f'attempting removal of key: {bucket}/{key}')
     try:
-        handler.delete(bucket=bucket, key=key)
-        handler.get(bucket=bucket, key=key)
+        handle.delete(bucket=bucket, key=key)
+        handle.get(bucket=bucket, key=key)
     except BlobNotFoundError:
         logger.warning(f'Success! unable to locate key {bucket}/{key} ')
     else:
-        logger.warning(f'Unable to delete key: {bucket}/{key}')
-        exit(1)
+        raise RuntimeError(f'attempted to delete {bucket}/{key} but it did not work ;(')
 
 
 @checkout.action("remove_checkout",
@@ -39,13 +38,13 @@ def remove_checkout(argv: typing.List[str], args: argparse.Namespace):
     Remove a bundle from the checkout bucket
     """
     replica = Replica[args.replica]
-    handler = Config.get_blobstore_handle(replica)
+    handle = Config.get_blobstore_handle(replica)
     bucket = replica.checkout_bucket
     if args.keys:
         for _key in args.keys:
             if DSS_BUNDLE_KEY_REGEX.match(_key):
-                for key in handler.list(bucket, _key):
-                    verify_delete(handler, bucket, key)
+                for key in handle.list(bucket, _key):
+                    verify_delete(handle, bucket, key)
             elif DSS_BUNDLE_FQID_REGEX.match(_key):
                 uuid, version = _key.split('.', 1)
                 manifest = get_bundle_manifest(replica=replica, uuid=uuid, version=version)
@@ -54,7 +53,7 @@ def remove_checkout(argv: typing.List[str], args: argparse.Namespace):
                     continue
                 for _files in manifest['files']:
                     key = compose_blob_key(_files)
-                    verify_delete(handler, bucket, key)
+                    verify_delete(handle, bucket, key)
             else:
                 # should handle other keys, files/blobs
-                    verify_delete(handler, bucket, _key)
+                    verify_delete(handle, bucket, _key)
