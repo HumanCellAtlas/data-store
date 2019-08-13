7f034a57225f94eb6824a15c4f8e45fe8bb6d6a3
DailyDreaming <lblauvel@ucsc.edu>
2019-08-13 12:13:39 -0700

    Update defualt runner to python 3.7.

--- common.mk
+++ common.mk
@@ -16,6 +16,6 @@ ifeq ($(shell which envsubst),)
 $(error Please install envsubst using "apt-get install gettext" or "brew install gettext; brew link gettext")
 endif
 
-ifeq ($(findstring Python 3.6, $(shell python --version 2>&1)),)
-$(error Please run make commands from a Python 3.6 virtualenv)
+ifeq ($(findstring Python 3.7, $(shell python --version 2>&1)),)
+$(error Please run make commands from a Python 3.7 virtualenv)
 endif
