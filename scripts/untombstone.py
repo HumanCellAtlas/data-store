fc04acaba32e29602058e1ae4e7345e081db2168
Jeffrey <wujeffrey567@gmail.com>
2019-09-11 13:58:30 -0700

    incorperated sns to untombstone

--- scripts/untombstone.py
+++ scripts/untombstone.py
@@ -1,150 +1,101 @@
 import os, re
 import logging as logger
 
-from dss import Config, Replica, dynamodb
+from dss import Replica, dynamodb
 from dss.dynamodb import get_item, put_item, delete_item, DynamoDBItemNotFound
 from dss.index.es import ElasticsearchClient
-from cloud_blobstore import BlobNotFoundError
-from dss.index.es.backend.ElasticsearchIndexBackend import index_bundle
-from dss import DSSException, dss_handler, DSSForbiddenException
 from dss.collections import owner_lookup
-
-from hca.dss import DSSClient
-from hca.util.exceptions import SwaggerAPIException
+from dss.events.handlers.sync import dependencies_exist
+from dcplib.aws.sqs import SQSMessenger, get_queue_url
 from dss.storage.identifiers import DSS_BUNDLE_TOMBSTONE_REGEX as dead_template
 
+
+
+sns = boto3.client('sns')
+sns.meta.config.max_pool_connections = 100
+sns_topic_run = "domovoi-s3-events-org-humancellatlas-dss-dev"
+gs_topic_run = "dss-gs-events-org-humancellatlas-dss-dev"
 
 # --------------------------------------------------------------
 # untombstone bundles
 # --------------------------------------------------------------
 
-
-def untombstone_bundle(uuid, replica, version=None):
-    """
-    deletes dead bundle and brings back original bundle 
-    """
-    if version is not None:
-        key = f"{uuid}.{version}.dead"
-    else:
-        key = f"{uuid}.dead"
-    if tombstoned_or_not_bundle(key, replica) is False:
-        pass
-    else:
-        deindex_dead_bundle(key)
-        update_og_bundle(key)
-        # indexs and notifys subs
-        index_bundle(key)
-
-
+# revision of untombstone bundle 
+def untombstone_bundle(uuid , version = None):
+    sqs = boto3.resource('sqs')
+    
+    for replica in Repclia:
+        if replica is Replica.aws:
+            topic = sns_topic_run
+        else:
+            topic  = gs_topic_run
+        if version is not None:
+            key = f"bundle/{uuid}.{version}.dead"
+        else:
+            key = f"bundle/{uuid}.dead"
+        if dependencies_exist(replica,replica,key) is False:
+            pass
+        else:
+            deindex_dead_bundle(key)
+            index_queue_url = get_queue_url(topic)
+            with SQSMessenger(index_queue_url) as sqsm:
+                sqsm.send(json.dumps(dict(replica=replica, key=key)))
+            
 # --------------------------------------------------------------
 # bundle
 # --------------------------------------------------------------
 
 
-def tombstoned_or_not_bundle(key, replica):
-    """
-    Return :: boolean :: True/False
-    Checks if bundle is  tombstoned or not 
-    """
-
-    try:
-        handle = Config.get_blobstore_handle(replica)
-        handle.get(replica.bucket, key)
-    except BlobNotFoundError:
-        raise DSSException(404, "not_found", "Cannot find bundle!")
-
 
 def deindex_dead_bundle(key):
     """
-    Param :: key :: string :: {uuid}.{version}
     Removes dead bundle and removes from es
     """
 
     es_client = ElasticsearchClient.get()
-
+    fqid = key.split("/")[1]
     if re.match(dead_template, key):
         # matches dead fqid and deletes it from es
         es_client.delete_by_query(
-            index="_all", body={"query": {"terms": {"_id": [key]}}}
+            index="_all", body={"query": {"terms": {"_id": [fqid]}}}
         )
-    logger.debug(f"removed dead bundle {key} from es")
-
-
-def update_og_bundle(fqid):
-    """
-    Param :: fqid :: string :: {uuid}.{version}
-    Finds original bundle and updates it
-    """
-    es_client = ElasticsearchClient.get()
-    if re.match(dead_template, fqid):
-        uuid, version, dead_tag = fqid.split(".")
-        # brings back the version that was labeled dead
-        es_client.update_by_query(
-            index="_all",
-            body={"query": {"terms": {"_id": ["{}.{}".format(uuid, version)]}}},
-        )
-        logger.debug("Untombstoned original bundle {uuid}.{version}")
-
-    else:
-        uuid, dead_tag = fqid.split(".")
-        # brings back all the versions
-        es_client.update_by_query(
-            index="_all", body={"query": {"terms": {"_id": [uuid]}}}
-        )
-
-        logger.debug("Untombstoned original bundles {uuid}")
+    logger.debug(f"removed dead bundle {fqid} from es")
 
 
 # --------------------------------------------------------------
 # collection
 # --------------------------------------------------------------
 
+def untombstone_collection(uuid, version=None):
+    sqs = boto3.resource('sqs')
+    
+    for replica in Repclia:
+        if replica is Replica.aws:
+            topic = sns_topic_run
+        else:
+            topic  = gs_topic_run
+        if version is not None:
+            key = f"collections/{uuid}.{version}.dead"
+        else:
+            key = f"collections/{uuid}.dead"
+        
+        if dependencies_exist(replica,replica,key) is False:
+            pass
+        else:
+            deindex_dead_collection(key)
+            index_queue_url = get_queue_url(topic)
+            with SQSMessenger(index_queue_url) as sqsm:
+                sqsm.send(json.dumps(dict(replica=replica, key=key)))
 
-def untombstone_colelction(uuid, replica, version=None):
-    if version is not None:
-        key = f"{uuid}.{version}.dead"
-    else:
-        key = f"{uuid}.dead"
-
-    if tombstoned_or_not_collection(key) is False:
-        pass
-    else:
-        deindex_dead_reindex_collection(key)
-
-
-def tombstoned_or_not_collection(key):
-    """
-    Return :: boolean :: True/False
-    Checks for exiting collection 
-    """
-
-    if re.match(dead_template, key):
-        uuid, version = key.split(".", 1)
-    else:
-        # Just dead tag
-        uuid, version = key.split(".")
-
-
-def deindex_dead_reindex_collection(fqid):
+def deindex_dead_collection(key):
     """
     Finds and deletes dead colletion and restores old collection
     """
-
-    if re.match(dead_template, fqid):
-        uuid, version = fqid.split(".", 1)
-    else:
-        uuid, version = fqid.split(".")
+    fqid = key.split("/")[1]
 
     dead_query = {
         "TableName": owner_lookup.collection_db_table,
-        "Key": _format_dynamodb_item(),
+        "Key": _format_dynamodb_item(sort_key=key,)
     }
-    og_query = {
-        "TableName": owner_lookup.collection_db_table,
-        "Key": _format_dynamodb_item(),
-    }
-
     dynamodb.delete_item(**dead_query)
     logger.debug(f"removed collection {fqid} from dynamodb")
-    dynamodb.put_item(**og_query)
-    logger.debug(f"restored original collection {uuid} {version}")
