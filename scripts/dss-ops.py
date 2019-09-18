c6c60297d25449950e608f97073d63c521717b83
Charles Reid <chmreid@ucsc.edu>
2019-09-17 17:54:15 -0700

    add import of lambda_params.py in dss-ops.py

--- scripts/dss-ops.py
+++ scripts/dss-ops.py
@@ -14,6 +14,7 @@ import dss.operations.checkout
 import dss.operations.storage
 import dss.operations.sync
 import dss.operations.ssm_params
+import dss.operations.lambda_params
 import dss.operations.elasticsearch
 import dss.operations.events
 import dss.operations.secrets
