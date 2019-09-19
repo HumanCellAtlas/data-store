9b69d3d2d4642831428f4031cd098e5683123902
Charles Reid <53452777+chmreid@users.noreply.github.com>
2019-09-18 17:38:37 -0700

    Add SSM parameter store functionality to dss-ops (#2432)
    * add initial version of ssm_params
    
    - adds two utility functions for manipulating
      ssm parameter store variable names
    
    * add get/set methods for ssm environment and ssm parameters, fix docs
    
    * add functions to list, set, unset vars  in ssm param store
    
    * improve description of ssm_params operations component
    
    * fix function names and parameter name check
    
    * add SwapStdin (context manager) object for tests
    
    * fix function signatures for set/unset ssm parameter functions
    
    * add ssm parameter store tests to test_operations.py
    
    * fix how we handle stdin so script won't hang if no input provided
    
    * add new ssm_params functionality to dss-ops script
    
    * improve printed messages
    
    * linting
    
    * fix type var
    
    * Remove superfluous secretsmanager imports
    
    * open multiple context managers in one line
    
    * change kwarg name, update how we restore stdin
    
    * use dict get() method
    
    * improve docstrings and printed messages
    
    improve docstrings and printed messages
    
    forgot one docstring
    
    * inline set/get_ssm_param methods
    
    * predefine repeated flag options

--- tests/__init__.py
+++ tests/__init__.py
@@ -160,3 +160,29 @@ class CaptureStdout(list):
         # Clean up by setting sys.stdout back to what it was before we opened
         # up this context
         sys.stdout = self._stdout
+
+class SwapStdin(object):
+    """Utility object using a context manager to swap out stdin with user-provided data."""
+    def __init__(self, swap_with):
+        if swap_with is None:
+            raise RuntimeError("Error: SwapStdin constructor must be provided with a value to substitute for stdin!")
+        elif isinstance(swap_with, type("")):
+            swap_with = bytes(swap_with, 'utf-8')
+        self.swap_with = swap_with
+
+    def __enter__(self, *args, **kwargs):
+        """
+        To swap out stdin properly (so it still works with the select module) requires a "real" file
+        with an os-level file descriptor. Fortunately os.pipe() will create a file descriptor for
+        the pipe, so we create a pipe and fill it with mock data, then swap it out with stdin.
+        """
+        fdr, fdw = os.pipe()
+        os.write(fdw, self.swap_with)
+        os.close(fdw)
+        f = os.fdopen(fdr)  # use the file descriptor directly
+        sys.stdin = f
+
+        return self
+
+    def __exit__(self, *args, **kwargs):
+        sys.stdin = sys.__stdin__
