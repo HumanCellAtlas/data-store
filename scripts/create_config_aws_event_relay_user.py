f70a7a72b89166e8af2a3babbdaabedb673350dc
Charles Reid <chmreid@ucsc.edu>
2019-09-30 14:59:01 -0700

    update scripts with set secrets usage

--- scripts/create_config_aws_event_relay_user.py
+++ scripts/create_config_aws_event_relay_user.py
@@ -70,7 +70,6 @@ subprocess.run(
         os.path.join(os.path.dirname(__file__), "dss-ops.py"),
         "secrets",
         "set",
-        "--secret-name",
         secret_name,
     ],
     input=json.dumps(secret_info).encode("utf-8"),
