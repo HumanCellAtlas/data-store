23d38f7ded20f0581ee822cecaeb0a69006093a2
Brian Hannafious <32105697+xbrianh@users.noreply.github.com>
2019-10-03 11:03:40 -0700

    Operation scripting for recording and journaling events (#2481)

--- dss/operations/util.py
+++ dss/operations/util.py
@@ -1,11 +1,18 @@
 import os
+import sys
 import traceback
+import argparse
 import logging
 import typing
+import time
+from functools import wraps
+from datetime import datetime, timedelta
 
 from cloud_blobstore import BlobStore
+from dcplib.aws.sqs import SQSMessenger
 
 from dss.util.aws.clients import sts  # type: ignore
+from dss.config import Config, Replica
 from concurrent.futures import ThreadPoolExecutor, as_completed
 
 
@@ -39,3 +46,20 @@ def map_bucket_results(func: typing.Callable, handle: BlobStore, bucket: str, ba
 def map_bucket(*args, **kwargs):
     for _ in map_bucket_results(*args, **kwargs):
         pass
+
+LOG_MONITOR_SLEEP_DURATION = 10
+
+def monitor_logs(logs_client, job_id: str, start_time: datetime):
+    start = new_start = int(1000 * (datetime.timestamp(datetime.utcnow())))
+    log_group = f"/aws/lambda/dss-operations-{os.environ['DSS_DEPLOYMENT_STAGE']}"
+    paginator = logs_client.get_paginator('filter_log_events')
+    while True:
+        for info in paginator.paginate(logGroupName=log_group, startTime=start, filterPattern=f'"{job_id}"'):
+            for e in info['events']:
+                print(e['message'])
+                new_start = e['timestamp'] + 1
+        if start == new_start:
+            sys.stderr.write(f"no new CloudWatch log messages, sleeping {LOG_MONITOR_SLEEP_DURATION}s" + os.linesep)
+            time.sleep(LOG_MONITOR_SLEEP_DURATION)
+        else:
+            start = new_start
