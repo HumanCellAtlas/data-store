f6366cc002a59f2d3ea7c69646d8ddd2a40c5a3a
Natan Lao <natanlao@users.noreply.github.com>
2019-10-15 12:27:41 -0700

    Refactor tombstone helper function to dss.storage
    We'll need to add support for tombstoning files, so it makes sense
    to move the tombstone helper function to dss.storage from
    dss.api.bundles.

---
+++ dss/storage/__init__.py
@@ -0,0 +1,11 @@
+from typing import NamedTuple, Optional
+
+
+class Tombstone(NamedTuple):
+    """
+    Tombstone object compliant with RFC #4 (Deletion of data in the DCP).
+    See HumanCellAtlas/dcp-community.
+    """
+    email: str
+    reason: str
+    details: Optional[dict] = {}
