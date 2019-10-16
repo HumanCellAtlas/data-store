4b43dc0a63cd74572a6faaceaff2c2f116fbdef5
amar jandu <ajandu@ucsc.edu>
2019-10-16 08:41:47 -0700

    removing mypy stuff

--- dss/events/handlers/notify_v2.py
+++ dss/events/handlers/notify_v2.py
@@ -96,7 +96,8 @@ def notify(subscription: dict, metadata_document: dict, key: str) -> bool:
     if bundle_version.endswith(sfx):
         bundle_version = bundle_version[:-len(sfx)]
     api_domain_name = f'https://{os.environ.get("API_DOMAIN_NAME")}'
-    payload: typing.Dict[typing.Union[str, SubscriptionData], typing.Any] = {
+    # payload: typing.Dict[typing.Union[str, SubscriptionData], typing.Any] = {
+    payload = {
         'bundle_url': api_domain_name + f'/v1/bundles/{bundle_uuid}?version={bundle_version}',
         'dss_api': api_domain_name,
         'subscription_id': subscription[SubscriptionData.UUID],
@@ -111,7 +112,7 @@ def notify(subscription: dict, metadata_document: dict, key: str) -> bool:
 
     jmespath_query = subscription.get(SubscriptionData.JMESPATH_QUERY)
     if jmespath_query is not None:
-        payload[str(SubscriptionData.JMESPATH_QUERY)] = jmespath_query
+        payload[SubscriptionData.JMESPATH_QUERY] = jmespath_query
 
     if "CREATE" == metadata_document['event_type']:
         attachments_defs = subscription.get(SubscriptionData.ATTACHMENTS)
