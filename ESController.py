32d48f4a9ea215a5e07becdeb797a74dde4868e0
amar jandu <ajandu@ucsc.edu>
2020-04-23 17:04:05 +0000

    working in ec2

--- ESController.py
+++ ESController.py
@@ -2,10 +2,12 @@ import deepdiff
 import sys
 import pathlib
 import pprint
+import copy
+from total_size import total_size
+from functools import reduce
 from dss.index.es import ElasticsearchClient
 from dss import Config, Replica, ESIndexType, dss_handler, DSSException
 from dss import ESDocType
-
 
 def _format_request_body(page: dict, es_query: dict, replica: Replica, output_format: str) -> dict:
     result_list = []  # type: typing.List[dict]
@@ -24,25 +26,12 @@ def _format_request_body(page: dict, es_query: dict, replica: Replica, output_fo
         'total_hits': page['hits']['total']
     }
 
+def _deep_get(src_dict: dict, keys: list):
+    """Performs deep retrieval of value from dictionary object"""
+    return reduce(lambda d, key: d.get(key) if d else None, keys, src_dict)
 
 
 data_dir = pathlib.Path.joinpath(pathlib.Path(__file__,), '../data')
-
-# swagger_url = 'https://dss.data.humancellatlas.org/v1/swagger.json'
-
-# dss_client = hca.dss.DSSClient(swagger_url=swagger_url)
-
-#
-# for result in dss_client.post_search.iterator(**es_args):
-#     # need to be able to get to search_after information, need to figure out how to pull from the headers.
-#     pprint.pprint(result)
-#     bundle_data = dict()
-#     bundle_data['bundle_fqid'] = result['bundle_fqid']
-#     bundle_data['bundle_url'] = result['bundle_url']
-#     bundle_data['links_json'] = result['metadata']['files']['links_json']
-
-# stash metadata into files.
-
 
 es_client = ElasticsearchClient.get()
 
@@ -80,32 +69,43 @@ def search(search_after: str = None):
 search_after = None
 total_hits = 0
 current_page = 0
-max_pages = 50
+max_pages = 300
 processing_lookup = dict()
 largest_link_json = None
 largest_link_json_size = 0
+largest_bundle = None
+def print_stats():
+    print(f'total_hits: {total_hits}')
+    print(len(processing_lookup))
+    pprint.pprint(largest_link_json)
+    print(largest_link_json_size)
+    print(largest_bundle)
+    #pprint.pprint(processing_lookup)
 
 while True:
     page = search(search_after)
-    if page['hits']['hits'][-1]['sort'] is None:
+    try:
+        next_page = page['hits']['hits'][-1]['sort']
+    except IndexError:
         print('i think we got everything')
+        print_stats()
         break
-    else:
-        search_after = ','.join(page['hits']['hits'][-1]['sort'])
-        current_page += 1
+    search_after = ','.join(page['hits']['hits'][-1]['sort'])
+    current_page += 1
     fmt_page = _format_request_body(page,es_query,replica,output_format)
     for bundles in fmt_page['results']:
         # sizing stuff
-        size = sys.getsizeof(bundles['metadata']['files']['links_json'])
+        size = total_size(bundles['metadata']['files']['links_json'])
         if largest_link_json_size < size:
             largest_link_json_size = size
             largest_link_json = bundles['metadata']['files']['links_json']
+            largest_bundle_fqid = bundles
         # comparing links_json obj
         for link in bundles['metadata']['files']['links_json'][0]['links']:
             total_hits += 1
             processes_id = link['process']
             if processes_id not in processing_lookup:
-                processing_lookup[processes_id] = link
+                processing_lookup[processes_id] = copy.deepcopy(link)
             else:
                 difference = deepdiff.DeepDiff(link, processing_lookup[processes_id])
                 if len(difference.keys()) == 0:
@@ -113,9 +113,5 @@ while True:
                 else:
                     print(f'WARNING:: process metadata DOES NOT match for collision: {processing_lookup} {link}')
     if max_pages <= current_page:
-        print(f'total_hits: {total_hits}')
-        print(len(processing_lookup))
-        pprint.pprint(largest_link_json)
-        print(largest_link_json_size)
-        #pprint.pprint(processing_lookup)
-        break
+        print_stats()
+        exit()
